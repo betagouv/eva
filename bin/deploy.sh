333b54d343c71495f4143a5af6d3b61930939f71
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2018-11-13 11:41:19 +0100

    Permets un déploiement sur une machine distante (production)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,6 +5,10 @@ nom_image_conteneur=betagouv/${nom_conteneur}
 horodatage=$(date +%Y%m%d%H%M%S)
 dossier_distribution=dist/${horodatage}
 
+if [[ -z ${port_ecoute} ]]
+then port_ecoute=80
+fi
+
 mkdir -p ${dossier_distribution}
 cp -R package-lock.json package.json server.js src webpack.config.js ${dossier_distribution}
 
@@ -15,4 +19,4 @@ then
   docker rm -f ${nom_conteneur}
 fi
 
-docker run --name ${nom_conteneur} --detach -p 80:3000 ${nom_image_conteneur}
+docker run --name ${nom_conteneur} --detach -p ${port_ecoute}:3000 ${nom_image_conteneur}
