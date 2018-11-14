dee3395239155699ebcce6dd9fa91b8e509b7fa5
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2018-11-14 11:37:36 +0100

    Rend possible l'execution des tests avec webpack
    source : https://www.threatstack.com/blog/unit-testing-with-webpack-mocha

---
+++ tests/simple-test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('simple tests', function() {
+  it('should pass', function() {
+    expect(false).to.be.true;
+  });
+});
---
+++ webpack-test.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+
+
+var config = {
+  entry: './all-tests.js',
+  output: {
+    path: path.resolve(__dirname, 'testdist'),
+    filename: 'testBundle.js'
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  node: {
+    fs: 'empty'
+  },
+
+
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildExit: "mocha --colors testdist/testBundle.js"
+    })
+  ]
+};
+
+
+module.exports = config;
