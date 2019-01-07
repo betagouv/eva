4900e21cc8bfe7820bccfd457f9b5c9495ee62de
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2019-01-07 18:12:47 +0100

    Ajout des tests dans circle-ci

---
+++ bin/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+BUILD_CMD="npx webpack --progress --color --config webpack.config-test.js"
+MOCHA_OPTION="--colors";
+if [ "$1" = "--watch" ]
+then
+  $BUILD_CMD --mode development --watch &
+  MOCHA_OPTION="$MOCHA_OPTION --watch";
+else
+  $BUILD_CMD --mode production
+fi
+
+npx mocha $MOCHA_OPTION tests_build/testBundle.js
