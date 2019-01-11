2d9d4677bbabb9090427be78ce83d5df2a5b5918
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2019-01-11 11:09:05 +0100

    [NETTOYAGE] donne à index.js de construire le point d'insertion `#magasin`

--- src/app/index.js
+++ src/app/index.js
@@ -3,5 +3,10 @@ import '../styles/app.scss';
 import { afficheMagasin } from './init.js';
 
 jQuery(function () {
+  jQuery('body').append(`
+    <div id="magasin">
+      <h1>Stock de boissons</h1>
+    </div>
+    `);
   afficheMagasin('#magasin', jQuery);
 });
