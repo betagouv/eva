e12ef731253fe946d436907a32cc71e68cb8705d
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2019-02-17 19:21:26 +0100

    Glisse et déplace les pièces

--- src/situations/controle/modeles/piece.js
+++ src/situations/controle/modeles/piece.js
@@ -2,6 +2,8 @@ export class Piece {
   constructor ({ x, y }) {
     this.x = x;
     this.y = y;
+    this.selectionnee = false;
+    this.abonnes = [];
   }
 
   position () {
@@ -11,5 +13,30 @@ export class Piece {
   changePosition ({ x, y }) {
     this.x = x;
     this.y = y;
+
+    this.abonnes.forEach(a => a({ x: x, y: y }));
+  }
+
+  quandChangementPosition (nouvelAbonne) {
+    this.abonnes.push(nouvelAbonne);
+  };
+
+  estSelectionnee () {
+    return this.selectionnee;
+  }
+
+  selectionne ({ x, y }) {
+    this.decalageSelection = { dx: x - this.x, dy: y - this.y };
+    this.selectionnee = true;
+  }
+
+  deselectionne () {
+    this.selectionnee = false;
+  }
+
+  deplaceSiSelectionnee ({ x, y }) {
+    if (this.estSelectionnee()) {
+      this.changePosition({ x: x - this.decalageSelection.dx, y: y - this.decalageSelection.dy });
+    }
   }
 }
