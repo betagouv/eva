fd25ad1c44ca47aedcff133a276d0490ecb0fdc6
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2019-02-22 10:16:30 +0100

    Ajoute la fenêtre modale de confirmation.

---
+++ tests/situations/commun/modale.js
@@ -0,0 +1,34 @@
+import jsdom from 'jsdom-global';
+import { afficheFenetreModale } from 'commun/vues/modale.js';
+
+describe('fenetre modale', function () {
+  let $;
+
+  beforeEach(function () {
+    jsdom('<div id="point-insertion"></div>');
+    $ = jQuery(window);
+  });
+
+  it("sait s'ajouter dans une page web", function () {
+    afficheFenetreModale($('#point-insertion'), $, 'message', () => {});
+
+    const $fenetre = $('#fenetre-modale');
+    expect($fenetre.length).to.equal(1);
+    expect($fenetre.find('label').text()).to.equal('message');
+  });
+
+  it("execute l'action quand on clique le bouton Ok", function (done) {
+    afficheFenetreModale($('#point-insertion'), $, 'message', () => {
+      done();
+    });
+
+    $('#OK-modale').click();
+  });
+
+  it('supprime la fenêtre modal quand on clique sur annuler', () => {
+    afficheFenetreModale($('#point-insertion'), $, 'message', () => {});
+
+    $('#annuler-modale').click();
+    expect($('#fenetre-modale').length).to.equal(0);
+  });
+});
--- tests/situations/commun/vues/stop.js
+++ tests/situations/commun/vues/stop.js
@@ -4,15 +4,34 @@ import { VueStop } from 'commun/vues/stop.js';
 describe('vue Stop', function () {
   let vue;
   let $;
+  let retourAccueil = false;
 
   beforeEach(function () {
     jsdom('<div id="magasin"></div>');
     $ = jQuery(window);
-    vue = new VueStop('#magasin', $);
+    vue = new VueStop('#magasin', $, () => {
+      retourAccueil = true;
+    });
   });
 
   it("sait s'insérer dans une page web", function () {
     vue.afficher();
-    expect($('#magasin #stop').attr('href')).to.equal('/');
+    expect(document.querySelector('#magasin #stop').classList).to.not.contain('invisible');
+  });
+
+  it('ouvre une fenêtre de confirmation avant de stopper', function () {
+    vue.afficher();
+
+    $('#magasin #stop').click();
+    expect($('#fenetre-modale').length).to.equal(1);
+    expect($('label').text()).to.equal('Voulez vous vraiment quitter la mission ?');
+  });
+
+  it("Redirige vers l'accueil quand on confirme la modale", function () {
+    vue.afficher();
+    $('#magasin #stop').click();
+    $('#OK-modale').click();
+
+    expect(retourAccueil).to.equal(true);
   });
 });
