102b1e3f39016b9ed48f71e56cf59e8fd10b0e85
Clément MORISSET <cmorisset@captive.fr>
2019-03-01 15:36:41 +0100

    ajoute méthode pour communiquer avec le serveur

--- src/situations/inventaire/infra/depot_journal.js
+++ src/situations/inventaire/infra/depot_journal.js
@@ -1,18 +1,46 @@
+
 export class DepotJournal {
-  constructor () {
+  constructor ($ = require('jquery')) {
     this.lignes = JSON.parse(window.localStorage.getItem('journal'));
     if (!this.lignes) {
       this.lignes = [];
       window.localStorage.setItem('journal', JSON.stringify(this.lignes));
     }
+    this.$ = $;
   }
 
   enregistre (ligne) {
     this.lignes.push(ligne);
     window.localStorage.setItem('journal', JSON.stringify(this.lignes));
+    this.envoiEvenementAuServeur(ligne);
   }
 
   evenements () {
     return this.lignes;
+  }
+
+  envoiEvenementAuServeur (ligne) {
+    this.$.ajax({
+      type: 'POST',
+      url: 'http://localhost:3000/api/evenements',
+      data: JSON.stringify(this.recupereDonnees(ligne)),
+      contentType: 'application/json; charset=utf-8',
+      success: function (data, status, jqXHR) {
+        console.log('Succès');
+      },
+      error: function (jqXHR, status) {
+        console.log('Echec ' + status.code);
+      }
+    });
+  }
+
+  recupereDonnees (ligne) {
+    var [date, type, description] = [ligne['date'], ligne['type'], ligne['description'].toString()];
+    var data = { date: date,
+      type_evenement: type,
+      description: description,
+      session_id: 'fake session_id',
+      situation: 'inventaire' };
+    return data;
   }
 }
