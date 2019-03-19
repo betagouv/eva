d9b5b369c7072d90deb88ba9ce90bee3b7367573
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2019-03-19 12:39:10 +0100

    Injecte la vue de la situation « Contrôle » dans le cadre
    Co-authored-by: Laurent Bossavit <laurent.bossavit@beta.gouv.fr>

--- src/situations/commun/vues/cadre.js
+++ src/situations/commun/vues/cadre.js
@@ -1,6 +1,13 @@
+import 'commun/styles/cadre.scss';
+
 export class VueCadre {
+  constructor (vueSituation) {
+    this.vueSituation = vueSituation;
+  }
+
   affiche (pointInsertion, $) {
     const $cadre = $('<div class="cadre"></div>');
     $(pointInsertion).append($cadre);
+    this.vueSituation.affiche('.cadre', $);
   }
 }
