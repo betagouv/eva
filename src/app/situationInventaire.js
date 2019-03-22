6bec4a35fa2bbe9ac1d421f49d7efb63afe55cc0
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2019-03-22 17:27:08 +0100

    Donne à la vue cadre la responsabilité de créer la vue action
    La vue cadre à la responsabilité de créer l'élément `<scene>` (pour accueillir la
    vue situation) et la barre d'action.
    
    Ces deux éléments doivent se partage l'espace verticale de l'écran et c'est la
    responsabilité de cette classe VueCadre de porter ce partage de ressource.
    
    Cela diminue aussi la duplication de la création de la barre d'action.

--- src/app/situationInventaire.js
+++ src/app/situationInventaire.js
@@ -4,7 +4,6 @@ import 'inventaire/styles/app.scss';
 
 import { DepotJournal } from 'commun/infra/depot_journal.js';
 import { Journal } from 'commun/modeles/journal.js';
-import { VueActions } from 'commun/vues/actions.js';
 import { VueCadre } from 'commun/vues/cadre.js';
 import { VueConsigne } from 'commun/vues/consigne.js';
 import { VueGo } from 'commun/vues/go.js';
@@ -17,14 +16,12 @@ function afficheSituation (pointInsertion, $) {
   const session = uuidv4();
   const journal = new Journal(Date.now, session, 'inventaire', new DepotJournal());
   const vueSituationInventaire = new VueSituation(journal);
-  const vueCadre = new VueCadre(vueSituationInventaire);
-  const vueActions = new VueActions(journal);
+  const vueCadre = new VueCadre(vueSituationInventaire, journal);
   const vueConsigne = new VueConsigne(sonConsigneDemarrage);
   const vueGo = new VueGo(vueConsigne, journal);
 
   vueConsigne.affiche(pointInsertion);
   vueCadre.affiche(pointInsertion, $);
-  vueActions.affiche(pointInsertion, $);
   vueGo.affiche(pointInsertion);
 }
 
--- tests/situations/commun/vues/cadre.js
+++ tests/situations/commun/vues/cadre.js
@@ -14,22 +14,30 @@ describe('Une vue du cadre', function () {
     $ = jQuery(window);
   });
 
-  it("s'affiche", function () {
+  it("Affiche une scene comme point d'insertion de la vue situation", function () {
     const vueSituation = uneVue();
     const vueCadre = new VueCadre(vueSituation);
-    expect($('#une-situation .cadre').length).to.equal(0);
+    expect($('#une-situation .scene').length).to.equal(0);
 
     vueCadre.affiche('#une-situation', $);
-    expect($('#une-situation .cadre').length).to.equal(1);
+    expect($('#une-situation .scene').length).to.equal(1);
   });
 
   it('affiche une situation donnée', function (done) {
     const vueSituation = uneVue(function (pointInsertion, jQuery) {
-      expect(pointInsertion).to.equal('.cadre');
+      expect(pointInsertion).to.equal('.scene');
       expect(jQuery).to.equal($);
       done();
     });
     const vueCadre = new VueCadre(vueSituation);
     vueCadre.affiche('#une-situation', $);
+  });
+
+  it("affiche la barre d'action", function () {
+    const vueSituation = uneVue();
+    const vueCadre = new VueCadre(vueSituation);
+    vueCadre.affiche('#une-situation', $);
+
+    expect($('.actions').length).to.equal(1);
   });
 });
