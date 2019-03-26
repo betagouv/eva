307cf52bdf0479e3cc184ca36ae0406953b1fde2
Stéphane Hanser <shanser@captive.fr>
2019-03-26 14:32:10 +0100

    remanie la VueGo pour utiliser jQuery

--- src/situations/commun/vues/go.js
+++ src/situations/commun/vues/go.js
@@ -4,41 +4,34 @@ import play from 'commun/assets/play.svg';
 
 export class VueGo {
   constructor (vueConsigne, journal) {
-    this.overlay = document.createElement('div');
-    this.overlay.id = 'overlay-go';
-    this.overlay.classList.add('overlay');
-    this.overlay.addEventListener('click', (event) => {
-      event.stopPropagation();
-    });
-
-    this.boutonGo = document.createElement('div');
-    this.boutonGo.id = 'go';
-    const imageGo = document.createElement('img');
-    imageGo.src = go;
-    this.boutonGo.appendChild(imageGo);
-    this.boutonGo.classList.add('invisible', 'bouton-centre', 'bouton-go');
-    this.boutonGo.addEventListener('click', () => {
-      this.overlay.classList.add('invisible');
-      journal.enregistreDemarrage();
-    });
-    this.overlay.appendChild(this.boutonGo);
-
-    this.boutonDemarrerConsigne = document.createElement('div');
-    this.boutonDemarrerConsigne.id = 'demarrer-consigne';
-    const imagePlay = document.createElement('img');
-    imagePlay.src = play;
-    this.boutonDemarrerConsigne.appendChild(imagePlay);
-    this.boutonDemarrerConsigne.classList.add('bouton-centre', 'bouton-lire-consigne-demarrage');
-    this.boutonDemarrerConsigne.addEventListener('click', () => {
-      this.boutonDemarrerConsigne.classList.add('invisible');
-      vueConsigne.jouerConsigneDemarrage(() => {
-        this.boutonGo.classList.remove('invisible');
-      });
-    });
-    this.overlay.appendChild(this.boutonDemarrerConsigne);
+    this.vueConsigne = vueConsigne;
+    this.journal = journal;
   }
 
-  affiche (pointInsertion) {
-    document.querySelector(pointInsertion).appendChild(this.overlay);
+  affiche (pointInsertion, $) {
+    this.overlay = $('<div id="overlay-go" class="overlay"></div>');
+
+    this.boutonGo = $('<div id="go" class="invisible bouton-centre bouton-lire-consigne-demarrage"></div>');
+    this.boutonGo.append(`<img src='${go}'>`);
+
+
+    this.boutonGo.on('click', () => {
+      this.overlay.addClass('invisible');
+      this.journal.enregistreDemarrage();
+    });
+    this.overlay.append(this.boutonGo);
+
+    this.boutonDemarrerConsigne = $('<div id="demarrer-consigne" class="bouton-centre bouton-lire-consigne-demarrage"></div>');
+    this.boutonDemarrerConsigne.append(`<img src='${play}'>`);
+
+    this.boutonDemarrerConsigne.on('click', () => {
+      this.boutonDemarrerConsigne.addClass('invisible');
+      this.vueConsigne.jouerConsigneDemarrage(() => {
+        this.boutonGo.removeClass('invisible');
+      });
+    });
+
+    this.overlay.append(this.boutonDemarrerConsigne);
+    $(pointInsertion).append(this.overlay);
   }
 }
