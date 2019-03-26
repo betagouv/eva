0ae6cb9a1f341e827778b9411a0cf6fc16b81172
Stéphane Hanser <shanser@captive.fr>
2019-03-26 20:29:42 +0100

    préfixe les variables jQuery avec $

--- src/situations/commun/vues/go.js
+++ src/situations/commun/vues/go.js
@@ -13,33 +13,33 @@ export class VueGo {
   affiche (pointInsertion, $) {
     this.$consigneTexte = $("<div class='consigne-texte'></div>");
 
-    this.overlay = $('<div id="overlay-go" class="overlay"></div>');
+    this.$overlay = $('<div id="overlay-go" class="overlay"></div>');
 
-    this.boutonGo = $('<div id="go" class="invisible bouton-centre bouton-lire-consigne-demarrage"></div>');
-    this.boutonGo.append(`<img src='${go}'>`);
+    this.$boutonGo = $('<div id="go" class="invisible bouton-centre bouton-lire-consigne-demarrage"></div>');
+    this.$boutonGo.append(`<img src='${go}'>`);
 
-    this.boutonGo.on('click', () => {
-      this.overlay.addClass('invisible');
+    this.$boutonGo.on('click', () => {
+      this.$overlay.addClass('invisible');
       this.journal.enregistreDemarrage();
     });
-    this.overlay.append(this.boutonGo);
+    this.$overlay.append(this.$boutonGo);
 
-    this.boutonDemarrerConsigne = $('<div id="demarrer-consigne" class="bouton-centre bouton-lire-consigne-demarrage"></div>');
-    this.boutonDemarrerConsigne.append(`<img src='${play}'>`);
+    this.$boutonDemarrerConsigne = $('<div id="demarrer-consigne" class="bouton-centre bouton-lire-consigne-demarrage"></div>');
+    this.$boutonDemarrerConsigne.append(`<img src='${play}'>`);
 
-    this.boutonDemarrerConsigne.on('click', () => {
-      this.boutonDemarrerConsigne.addClass('invisible');
+    this.$boutonDemarrerConsigne.on('click', () => {
+      this.$boutonDemarrerConsigne.addClass('invisible');
       this.$consigneTexte.text('');
 
       this.vueConsigne.jouerConsigneDemarrage(() => {
-        this.boutonGo.removeClass('invisible');
+        this.$boutonGo.removeClass('invisible');
         this.$consigneTexte.text(traduction('situation.go'));
       });
     });
 
     this.$consigneTexte.text(traduction('situation.ecouter-consigne'));
-    this.overlay.append(this.boutonDemarrerConsigne);
-    this.overlay.append(this.$consigneTexte);
-    $(pointInsertion).append(this.overlay);
+    this.$overlay.append(this.$boutonDemarrerConsigne);
+    this.$overlay.append(this.$consigneTexte);
+    $(pointInsertion).append(this.$overlay);
   }
 }
