a4a6fbfa59b00b55705447c2bbe13639c9359d82
Stéphane Hanser <shanser@captive.fr>
2019-03-28 11:49:39 +0100

    affiche un seul état à la fois dans la vue Go

--- src/situations/commun/vues/go.js
+++ src/situations/commun/vues/go.js
@@ -10,54 +10,57 @@ export class VueGo {
   constructor (vueConsigne, journal) {
     this.vueConsigne = vueConsigne;
     this.journal = journal;
+
+    this.etats = {
+      aDemarrer: {
+        image: play,
+        texte: traduction('situation.ecouter-consigne'),
+        classe: 'bouton-lire-consigne-demarrage',
+        click: () => {
+          this.afficheEtat(this.etats.lectureEnCours);
+
+          this.vueConsigne.jouerConsigneDemarrage(() => {
+            this.afficheEtat(this.etats.go);
+          });
+        }
+      },
+
+      lectureEnCours: {
+        image: lectureEnCours,
+        texte: '',
+        classe: 'bouton-lecture-en-cours'
+      },
+
+      go: {
+        image: go,
+        texte: traduction('situation.go'),
+        classe: 'bouton-go',
+        click: () => {
+          this.$overlay.addClass('invisible');
+          this.journal.enregistre(new EvenementDemarrage());
+        }
+      }
+    };
   }
 
   affiche (pointInsertion, $) {
-    this.$ = $;
     this.$message = $("<div class='message'></div>");
+    this.$bouton = $(`<div class="bouton-centre"></div>`);
     this.$overlay = $('<div id="overlay-go" class="overlay"></div>');
 
-    this.$boutonGo = this.$bouton({
-      id: 'go', classe: 'bouton-go', image: go, visibilite: 'invisible',
-      click: () => {
-        this.$overlay.addClass('invisible');
-        this.journal.enregistre(new EvenementDemarrage());
-      }
-    });
+    this.afficheEtat(this.etats.aDemarrer);
 
-    this.$boutonLectureEnCours = this.$bouton({
-      id: 'lecture-en-cours', classe: 'bouton-lecture-en-cours',
-      image: lectureEnCours, visibilite: 'invisible'
-    });
-
-    this.$boutonDemarrerConsigne = this.$bouton({
-      id: 'demarrer-consigne', classe: 'bouton-lire-consigne-demarrage',
-      image: play, visibilite: 'visible',
-      click: () => {
-        this.$boutonDemarrerConsigne.addClass('invisible');
-        this.$boutonLectureEnCours.removeClass('invisible');
-        this.$message.text('');
-
-        this.vueConsigne.jouerConsigneDemarrage(() => {
-          this.$boutonGo.removeClass('invisible');
-          this.$message.text(traduction('situation.go'));
-        });
-      }
-    });
-
-    this.$message.text(traduction('situation.ecouter-consigne'));
-    this.$overlay.append(this.$boutonDemarrerConsigne);
-    this.$overlay.append(this.$boutonLectureEnCours);
-    this.$overlay.append(this.$boutonGo);
+    this.$overlay.append(this.$bouton);
     this.$overlay.append(this.$message);
+
     $(pointInsertion).append(this.$overlay);
   }
 
-  $bouton (parametres) {
-    const $ = this.$;
-    const $bouton = $(`<div id="${parametres.id}" class="${parametres.visibilite} bouton-centre ${parametres.classe}"></div>`);
-    $bouton.append(`<img src='${parametres.image}'>`);
-    $bouton.on('click', parametres.click);
-    return $bouton;
+  afficheEtat (parametres) {
+    this.$bouton.attr('class', `bouton-centre ${parametres.classe}`);
+    this.$bouton.html(`<img src='${parametres.image}'>`);
+    this.$bouton.off('click');
+    this.$bouton.on('click', parametres.click);
+    this.$message.text(parametres.texte);
   }
 }
