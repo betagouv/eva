00f94bc47f375726c7eaca69cd4bbae16a39c7be
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2019-03-29 13:48:41 +0100

    Integre la VueConsigne et la VueGo dans la VueCadre

--- src/app/situationInventaire.js
+++ src/app/situationInventaire.js
@@ -8,8 +8,6 @@ import { Situation } from 'inventaire/modeles/situation';
 import { DepotJournal } from 'commun/infra/depot_journal';
 import { Journal } from 'commun/modeles/journal';
 import { VueCadre } from 'commun/vues/cadre';
-import { VueConsigne } from 'commun/vues/consigne';
-import { VueGo } from 'commun/vues/go';
 import { VueSituation } from 'inventaire/vues/situation';
 import { initialise as initialiseInternationalisation, traduction } from 'commun/infra/internationalisation';
 
@@ -18,15 +16,12 @@ import sonConsigneDemarrage from 'inventaire/assets/consigne_demarrage.mp3';
 function afficheSituation (pointInsertion, $) {
   const session = uuidv4();
   const journal = new Journal(Date.now, session, 'inventaire', new DepotJournal());
-  const situation = new Situation({ contenants, contenus });
-  const vueSituationInventaire = new VueSituation(situation, journal);
-  const vueCadre = new VueCadre(vueSituationInventaire, journal);
-  const vueConsigne = new VueConsigne(sonConsigneDemarrage);
-  const vueGo = new VueGo(vueConsigne, situation, journal);
+  const situation = new Situation({ contenants, contenus }, sonConsigneDemarrage);
 
-  vueConsigne.affiche(pointInsertion);
+  const vueSituationInventaire = new VueSituation(situation, journal);
+  const vueCadre = new VueCadre(vueSituationInventaire, situation, journal);
+
   vueCadre.affiche(pointInsertion, $);
-  vueGo.affiche(pointInsertion, $);
 }
 
 initialiseInternationalisation().then(function () {
