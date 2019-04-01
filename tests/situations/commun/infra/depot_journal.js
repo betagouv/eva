6e330a207af0034c3b38c6f37896ac6be2cb395d
François de Metz <francois.de_metz@beta.gouv.fr>
2019-04-01 21:38:57 +0200

    Supprime l'enregistrement du journal dans localStorage et la page de restitution.

--- tests/situations/commun/infra/depot_journal.js
+++ tests/situations/commun/infra/depot_journal.js
@@ -11,26 +11,6 @@ describe('le depot du journal', function () {
     journal = new DepotJournal({ ajax (params) { requetes.push(params); } });
   });
 
-  it('enregistre les lignes du journal dans le localStorage', function () {
-    journal.enregistre({ cle: 'valeur', description: { cle: 'valeur' } });
-    journal.enregistre({ autreCle: 'valeur2', description: { cle: 'valeur2' } });
-
-    const lignes = JSON.parse(window.localStorage.getItem('journal'));
-    expect(lignes.length).to.equal(2);
-    expect(lignes).to.eql([
-      { cle: 'valeur',
-        description: {
-          cle: 'valeur'
-        }
-      },
-      { autreCle: 'valeur2',
-        description: {
-          cle: 'valeur2'
-        }
-      }
-    ]);
-  });
-
   it('fait un POST des lignes du journal vers le serveur', function () {
     journal.enregistre({ autreCle: 'valeur2', description: { cle: 'valeur2' } });
 
@@ -45,34 +25,5 @@ describe('le depot du journal', function () {
     expect(payload['donnees']).to.equal(donnees);
     expect(payload['session_id']).to.equal('ma session id');
     expect(payload['situation']).to.equal('controle');
-  });
-
-  it("vérifie s'il n'existe pas un journal au démarrage et le charge", function () {
-    journal.enregistre({ nom: 'NomEvenement', description: { cle: 'valeur' } });
-
-    journal = new DepotJournal({ ajax (params) { requetes.push(params); } });
-    journal.enregistre({ nom: 'NomEvenement', description: { cle: 'valeur' } });
-
-    const lignes = JSON.parse(window.localStorage.getItem('journal'));
-    expect(lignes.length).to.equal(2);
-  });
-
-  it('retourne les lignes du journal', function () {
-    journal.enregistre({ cle: 'valeur', description: { cle: 'valeur' } });
-    journal.enregistre({ cle: 'valeur2', description: { cle: 'valeur2' } });
-
-    expect(journal.evenements()).to.eql([
-      { 'cle': 'valeur',
-        'description': {
-          'cle': 'valeur'
-        }
-      },
-      { 'cle': 'valeur2',
-        'description': {
-          'cle': 'valeur2'
-        }
-      }
-    ]
-    );
   });
 });
