51dad306edc35640bc87d77a53bb0b7ed5690bb5
Stéphane Hanser <shanser@captive.fr>
2019-04-01 11:35:08 +0200

    attribue une forme au contenu pour gérer le zoom de l'affichage

--- tests/situations/inventaire/modeles/magasin.js
+++ tests/situations/inventaire/modeles/magasin.js
@@ -4,8 +4,8 @@ import { unMagasin } from '../aides/magasin.js';
 describe('Le magasin', function () {
   it('connaît les produits en stock', function () {
     let magasin = unMagasin().avecCommeReferences(
-      { idProduit: '0', nom: 'Nova Sky', image: 'chemin image Nova Sky' },
-      { idProduit: '1', nom: 'Premium Terra', image: 'chemin image Premium Terra' }
+      { idProduit: '0', nom: 'Nova Sky', image: 'chemin image Nova Sky', forme: 'petiteBouteille' },
+      { idProduit: '1', nom: 'Premium Terra', image: 'chemin image Premium Terra', forme: 'grandeBouteille' }
     ).avecEnStock(
       new Contenant({ idContenu: '0', quantite: 12 }),
       new Contenant({ idContenu: '1', quantite: 3 })
@@ -13,14 +13,14 @@ describe('Le magasin', function () {
 
     let produits = magasin.produitsEnStock();
     expect(Array.from(produits)).to.eql([
-      ['0', { nom: 'Nova Sky', image: 'chemin image Nova Sky', quantite: 12 }],
-      ['1', { nom: 'Premium Terra', image: 'chemin image Premium Terra', quantite: 3 }]
+      ['0', { nom: 'Nova Sky', image: 'chemin image Nova Sky', quantite: 12, forme: 'petiteBouteille' }],
+      ['1', { nom: 'Premium Terra', image: 'chemin image Premium Terra', quantite: 3, forme: 'grandeBouteille' }]
     ]);
   });
 
   it('additionne les quantités', function () {
     let magasin = unMagasin().avecCommeReferences(
-      { idProduit: '0', nom: 'Nova Sky', image: 'chemin image Nova Sky' }
+      { idProduit: '0', nom: 'Nova Sky', image: 'chemin image Nova Sky', forme: 'petiteBouteille' }
     ).avecEnStock(
       new Contenant({ idContenu: '0', quantite: 12 }),
       new Contenant({ idContenu: '0', quantite: 3 })
@@ -28,7 +28,7 @@ describe('Le magasin', function () {
 
     let produits = magasin.produitsEnStock();
     expect(Array.from(produits)).to.eql([
-      ['0', { nom: 'Nova Sky', image: 'chemin image Nova Sky', quantite: 15 }]
+      ['0', { nom: 'Nova Sky', image: 'chemin image Nova Sky', quantite: 15, forme: 'petiteBouteille' }]
     ]);
   });
 });
