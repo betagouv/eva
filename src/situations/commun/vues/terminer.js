9d414d573baecc05e30dfe2f42599eb37fb5c49b
François de Metz <francois.de_metz@beta.gouv.fr>
2019-04-05 20:15:42 +0200

    Affiche une confirmation avant de quitter si la situation a démarré.

--- src/situations/commun/vues/terminer.js
+++ src/situations/commun/vues/terminer.js
@@ -10,4 +10,6 @@ export default class VueTerminer {
     $actions.append(`<div class='message-succes'>${traduction('situation.reussite')}</div>`);
     $(pointInsertion).append($actions);
   }
+
+  cache () {}
 }
