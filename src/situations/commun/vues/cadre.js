026c3254c1a6563664d86d217dec451b3c41b0d3
François de Metz <francois.de_metz@beta.gouv.fr>
2019-04-05 15:13:09 +0200

    Affiche la vue terminer a la réussite de l'inventaire.

--- src/situations/commun/vues/cadre.js
+++ src/situations/commun/vues/cadre.js
@@ -1,14 +1,18 @@
 import 'commun/styles/cadre.scss';
+import EvenementFin from 'commun/modeles/evenement_fin';
 import { VueActions } from 'commun/vues/actions';
 import { VueConsigne } from 'commun/vues/consigne';
 import { VueGo } from 'commun/vues/go';
+import VueTerminer from 'commun/vues/terminer';
 
 export class VueCadre {
   constructor (vueSituation, situation, journal) {
     this.vueSituation = vueSituation;
+    this.situation = situation;
     this.vueActions = new VueActions(journal);
     this.vueConsigne = new VueConsigne(situation.consigneAudio);
     this.vueGo = new VueGo(this.vueConsigne, situation, journal);
+    this.vueTerminer = new VueTerminer();
   }
 
   affiche (pointInsertion, $) {
@@ -22,5 +26,9 @@ export class VueCadre {
     this.vueActions.affiche(selecteurCadre, $);
     this.vueConsigne.affiche(selecteurCadre);
     this.vueGo.affiche(selecteurCadre, $);
+    this.situation.observe(EvenementFin, () => {
+      this.vueActions.cache($);
+      this.vueTerminer.affiche(selecteurCadre, $);
+    });
   }
 }
