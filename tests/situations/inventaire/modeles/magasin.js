8f0ac36fc17a5e59ff418f4c4b5ffa06b0011eba
Stéphane Hanser <shanser@captive.fr>
2019-04-08 11:35:28 +0200

    affiche les produits triés dans le formulaire de saisie de l'inventaire
    This reverts commit 13c27ab5cdb751dec844c2e7b60bb6a70735ce98.

--- tests/situations/inventaire/modeles/magasin.js
+++ tests/situations/inventaire/modeles/magasin.js
@@ -2,10 +2,10 @@ import { Contenant } from 'inventaire/modeles/contenant';
 import { unMagasin } from '../aides/magasin';
 
 describe('Le magasin', function () {
-  it('connaît les produits en stock', function () {
+  it('connaît les produits en stock triés', function () {
     let magasin = unMagasin().avecCommeReferences(
-      { idProduit: '0', nom: 'Nova Sky', image: 'chemin image Nova Sky', forme: 'petiteBouteille' },
-      { idProduit: '1', nom: 'Premium Terra', image: 'chemin image Premium Terra', forme: 'grandeBouteille' }
+      { idProduit: '0', nom: 'Nova Sky', image: 'chemin image Nova Sky', forme: 'petiteBouteille', position: 2 },
+      { idProduit: '1', nom: 'Premium Terra', image: 'chemin image Premium Terra', forme: 'grandeBouteille', position: 1 }
     ).avecEnStock(
       new Contenant({ idContenu: '0', quantite: 12 }),
       new Contenant({ idContenu: '1', quantite: 3 })
@@ -13,14 +13,14 @@ describe('Le magasin', function () {
 
     let produits = magasin.produitsEnStock();
     expect(Array.from(produits)).to.eql([
-      ['0', { nom: 'Nova Sky', image: 'chemin image Nova Sky', quantite: 12, forme: 'petiteBouteille' }],
-      ['1', { nom: 'Premium Terra', image: 'chemin image Premium Terra', quantite: 3, forme: 'grandeBouteille' }]
+      ['1', { nom: 'Premium Terra', image: 'chemin image Premium Terra', quantite: 3, forme: 'grandeBouteille', position: 1 }],
+      ['0', { nom: 'Nova Sky', image: 'chemin image Nova Sky', quantite: 12, forme: 'petiteBouteille', position: 2 }]
     ]);
   });
 
   it('additionne les quantités', function () {
     let magasin = unMagasin().avecCommeReferences(
-      { idProduit: '0', nom: 'Nova Sky', image: 'chemin image Nova Sky', forme: 'petiteBouteille' }
+      { idProduit: '0', nom: 'Nova Sky', image: 'chemin image Nova Sky', forme: 'petiteBouteille', position: 1 }
     ).avecEnStock(
       new Contenant({ idContenu: '0', quantite: 12 }),
       new Contenant({ idContenu: '0', quantite: 3 })
@@ -28,7 +28,7 @@ describe('Le magasin', function () {
 
     let produits = magasin.produitsEnStock();
     expect(Array.from(produits)).to.eql([
-      ['0', { nom: 'Nova Sky', image: 'chemin image Nova Sky', quantite: 15, forme: 'petiteBouteille' }]
+      ['0', { nom: 'Nova Sky', image: 'chemin image Nova Sky', quantite: 15, forme: 'petiteBouteille', position: 1 }]
     ]);
   });
 });
