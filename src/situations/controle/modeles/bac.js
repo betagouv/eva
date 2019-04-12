f0e784230b1c0adf36af407d6b9cd164cdec2474
Clément MORISSET <cmorisset@captive.fr>
2019-04-12 11:52:19 +0200

    ajoute etat survolé au modele Bac
    Co-authored-by: François de Metz <francois.de_metz@beta.gouv.fr>

--- src/situations/controle/modeles/bac.js
+++ src/situations/controle/modeles/bac.js
@@ -1,8 +1,13 @@
-export class Bac {
+import EventEmitter from 'events';
+export const CHANGEMENT_ETAT_SURVOLE = 'changementEtatSurvole';
+
+export class Bac extends EventEmitter {
   constructor ({ categorie, x, y, largeur, hauteur }) {
+    super();
     this._categorie = categorie;
     this._dimensions = { largeur, hauteur };
     this._position = { x, y };
+    this._survole = false;
   }
 
   dimensions () {
@@ -15,5 +20,22 @@ export class Bac {
 
   categorie () {
     return this._categorie;
+  }
+
+  passeEnEtatSurvole () {
+    this.changeEtatSurvole(true);
+  }
+
+  reinitialiseEtatSurvole () {
+    this.changeEtatSurvole(false);
+  }
+
+  changeEtatSurvole (etat) {
+    this._survole = etat;
+    this.emit(CHANGEMENT_ETAT_SURVOLE, this._survole);
+  }
+
+  etatSurvole () {
+    return this._survole;
   }
 }
