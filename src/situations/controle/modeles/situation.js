dbfa3e28c702c046e937115196b1bcc8c3b090b8
François de Metz <francois.de_metz@beta.gouv.fr>
2019-04-12 14:30:32 +0200

    Change l'état survolé des bacs au déplacement des pieces.

--- src/situations/controle/modeles/situation.js
+++ src/situations/controle/modeles/situation.js
@@ -1,4 +1,4 @@
-import { Piece } from 'controle/modeles/piece';
+import { Piece, CHANGEMENT_POSITION, CHANGEMENT_SELECTION } from 'controle/modeles/piece';
 import SituationCommune, { FINI } from 'commun/modeles/situation';
 
 import sonFondSonore from 'controle/assets/fond_sonore.mp3';
@@ -80,6 +80,8 @@ export class Situation extends SituationCommune {
   ajoutePiece (piece) {
     this._piecesAffichees.push(piece);
     this.emit(NOUVELLE_PIECE, piece);
+    piece.on(CHANGEMENT_POSITION, () => this.detecteSurvol(piece));
+    piece.on(CHANGEMENT_SELECTION, () => this.reinitialiseBacs(piece));
   }
 
   faisDisparaitrePiece (piece) {
@@ -87,6 +89,22 @@ export class Situation extends SituationCommune {
     piece.emit(DISPARITION_PIECE);
     if (this.nAPlusRienAFaire()) {
       this.modifieEtat(FINI);
+    }
+  }
+
+  detecteSurvol (piece) {
+    this.bacs().forEach((bac) => {
+      if (bac.contient(piece)) {
+        bac.passeEnEtatSurvole();
+      } else {
+        bac.reinitialiseEtatSurvole();
+      }
+    });
+  }
+
+  reinitialiseBacs (piece) {
+    if (!piece.estSelectionnee()) {
+      this.bacs().forEach(bac => bac.reinitialiseEtatSurvole());
     }
   }
 }
