04c7375a6e2ae2b3089ff0acc18f01b128111025
Clément MORISSET <cmorisset@captive.fr>
2019-04-16 14:02:57 +0200

    ajoute la nouvelle fenetre stop

--- src/situations/commun/vues/modale.js
+++ src/situations/commun/vues/modale.js
@@ -4,13 +4,11 @@ import 'commun/styles/overlay.scss';
 import 'commun/styles/modale.scss';
 
 export function afficheFenetreModale ($pointInsertion, $, message, actionOk) {
-  const $modale = $(`<div id="fenetre-modale" class="overlay">
-    <div class="fenetre">
+  const $modale = $(`<div id="fenetre-modale" class="overlay modale-stop">
     <label>${message}</label>
     <div class="buttons">
-    <button id="OK-modale">${traduction('situation.modale.ok')}</button>
-    <button id="annuler-modale">${traduction('situation.modale.annuler')}</button>
-    </div>
+    <button id="annuler-modale" class='modal-annuler'>${traduction('situation.modale.annuler')}</button>
+    <button id="OK-modale" class='modal-ok'>${traduction('situation.modale.ok')}</button>
     </div>
     </div>`);
   $pointInsertion.append($modale);
