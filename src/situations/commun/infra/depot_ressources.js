bcf5e9fa8cfbdbf5288192fba2522525a25b6b7c
François de Metz <francois.de_metz@beta.gouv.fr>
2019-04-19 18:45:06 +0200

    Ajoute la promesse de chargement.
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- src/situations/commun/infra/depot_ressources.js
+++ src/situations/commun/infra/depot_ressources.js
@@ -1,14 +1,24 @@
 export default class DepotRessources {
   constructor () {
     this.images = {};
+    this.promesses = [];
   }
 
   charge (contexte, identifiant) {
     contexte.keys().forEach((key) => {
       const img = new window.Image();
+      const promesse = new Promise((resolve, reject) => {
+        img.onload = resolve;
+        img.onerror = reject;
+      });
+      this.promesses.push(promesse);
       img.src = contexte(key);
       this.images[`${identifiant}/${key.substring(2)}`] = img;
     });
+  }
+
+  chargement () {
+    return Promise.all(this.promesses);
   }
 
   image (nom) {
