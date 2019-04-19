96a814677466f4b03ec81626005a4b8799e7de0c
François de Metz <francois.de_metz@beta.gouv.fr>
2019-04-19 11:02:43 +0200

    Utilise l'export par défaut de classe pour la vue Etageres.

--- src/situations/inventaire/vues/situation.js
+++ src/situations/inventaire/vues/situation.js
@@ -1,6 +1,6 @@
 import { CHANGEMENT_ETAT, DEMARRE } from 'commun/modeles/situation';
 
-import { VueEtageres } from 'inventaire/vues/etageres';
+import VueEtageres from 'inventaire/vues/etageres';
 import { initialiseFormulaireSaisieInventaire } from 'inventaire/vues/formulaireSaisieInventaire';
 import EvenementDemarrage from 'commun/modeles/evenement_demarrage';
 
