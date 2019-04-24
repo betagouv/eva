f0b4e7fa3aa5de18925a3a840ad84def161e420f
Clément MORISSET <cmorisset@captive.fr>
2019-04-24 17:02:42 +0200

    Rejoue la consigne quand on appuie sur le bouton de relecture

--- src/situations/commun/vues/rejoue_consigne.js
+++ src/situations/commun/vues/rejoue_consigne.js
@@ -1,13 +1,33 @@
 import 'commun/styles/rejoue_consigne.scss';
 import play from 'commun/assets/play.svg';
+import lectureEnCours from 'commun/assets/lecture-en-cours.svg';
 
 export default class VueRejoueConsigne {
-  constructor (pointInsertion, $) {
-    this.$pointInsertion = $(pointInsertion);
-    this.$boutonRejoueConsigne = $(`<a id="rejoue-consigne" class="bouton-relecture-consigne"><img src="${play}"></a>`);
+  constructor (consigne) {
+    this.consigne = consigne;
   }
 
-  affiche () {
-    this.$pointInsertion.append(this.$boutonRejoueConsigne);
+  affiche (pointInsertion, $) {
+    this.$boutonRejoueConsigne = $(`<a id="rejoue-consigne" class="bouton-relecture-consigne"><img src="${play}"></a>`);
+    this.$boutonRejoueConsigne.on('click', () => this.click($));
+
+    $(pointInsertion).append(this.$boutonRejoueConsigne);
+  }
+
+  click ($) {
+    this.joueConsigne($);
+    this.$boutonRejoueConsigne.children().attr('src', lectureEnCours);
+  }
+
+  joueConsigne ($) {
+    $(this.consigne).on('ended', this.lectureTermine.bind(this));
+    return Promise.resolve(this.consigne.play())
+      .catch(e => {
+        this.lectureTermine();
+      });
+  }
+
+  lectureTermine () {
+    this.$boutonRejoueConsigne.children().attr('src', play);
   }
 }
