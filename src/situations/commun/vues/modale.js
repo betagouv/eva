5134b9890c4a2cc361319a4abad6e9e6e2101e1a
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2019-04-26 10:52:05 +0200

    fixup! Permet l'utilisation du bouton stop, même si l'enregistrement de l'evenement ne fonctionne pas
    Corrige modale.js pour qu'elle ne fasse plus référence à la notion de stop

--- src/situations/commun/vues/modale.js
+++ src/situations/commun/vues/modale.js
@@ -4,7 +4,7 @@ import 'commun/styles/overlay.scss';
 import 'commun/styles/modale.scss';
 
 export function afficheFenetreModale ($pointInsertion, $, message, actionOk) {
-  const $modale = $(`<div id="fenetre-modale" class="overlay modale-stop">
+  const $modale = $(`<div id="fenetre-modale" class="overlay modale">
     <label>${message}</label>
     <div class="buttons">
     <button id="annuler-modale" class='modal-annuler'>${traduction('situation.modale.annuler')}</button>
