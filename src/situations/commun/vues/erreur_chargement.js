56ac73d7853ec108406a841c98dad0b97419ba19
François de Metz <francois.de_metz@beta.gouv.fr>
2019-04-26 15:25:06 +0200

    Change le style utilisé dans l'écran d'erreur de chargement.
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- src/situations/commun/vues/erreur_chargement.js
+++ src/situations/commun/vues/erreur_chargement.js
@@ -1,11 +1,11 @@
 import VueActionOverlay from 'commun/vues/action_overlay';
 
-import go from 'commun/assets/lecture-en-cours.svg';
+import recharge from 'commun/assets/recharge.svg';
 import { traduction } from 'commun/infra/internationalisation';
 
 export default class VueErreurChargement extends VueActionOverlay {
   constructor () {
-    super(go, traduction('situation.erreur_chargement'), 'bouton-chargement');
+    super(recharge, traduction('situation.erreur_chargement'), 'bouton-erreur-chargement');
   }
 
   affiche (pointInsertion, $) {
