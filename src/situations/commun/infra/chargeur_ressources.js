b6df9b313fa5d9366646d79637e1ff3c9a50ef17
François de Metz <francois.de_metz@beta.gouv.fr>
2019-04-29 11:42:18 +0200

    Pré-charge les mp3 dans la classe ChargeurRessources.
    Co-authored-by: Enzo Cranford <enzo@ut7.fr>

--- src/situations/commun/infra/chargeur_ressources.js
+++ src/situations/commun/infra/chargeur_ressources.js
@@ -1,17 +1,36 @@
+export function chargeurAudio (src) {
+  const audio = new window.Audio(src);
+  return new Promise((resolve, reject) => {
+    audio.addEventListener('canplaythrough', resolve);
+    audio.addEventListener('error', reject);
+  });
+}
+
+export function chargeurImage (src) {
+  const img = new window.Image();
+  const promesse = new Promise((resolve, reject) => {
+    img.onload = resolve;
+    img.onerror = reject;
+  });
+  img.src = src;
+  return promesse;
+}
+
+export const CHARGEURS = {
+  'mp3': chargeurAudio,
+  'png': chargeurImage
+};
+
 export default class ChargeurRessources {
-  constructor () {
+  constructor (chargeurs = CHARGEURS) {
+    this.chargeurs = chargeurs;
     this.promesses = [];
   }
 
   charge (contexte) {
     contexte.keys().forEach((key) => {
-      const img = new window.Image();
-      const promesse = new Promise((resolve, reject) => {
-        img.onload = resolve;
-        img.onerror = reject;
-      });
+      const promesse = this.chargeurs[key.match(/\.([^.]+)$/)[1]](contexte(key));
       this.promesses.push(promesse);
-      img.src = contexte(key);
     });
   }
 
