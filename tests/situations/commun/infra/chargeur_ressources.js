1b431ca0d3950e9d4de4b25847140f665dafc46e
François de Metz <francois.de_metz@beta.gouv.fr>
2019-04-29 11:42:18 +0200

    Pré-charge les mp3 dans la classe ChargeurRessources.
    Co-authored-by: Enzo Cranford <enzo@ut7.fr>

--- tests/situations/commun/infra/chargeur_ressources.js
+++ tests/situations/commun/infra/chargeur_ressources.js
@@ -1,41 +1,89 @@
-import ChargeurRessources from 'commun/infra/chargeur_ressources';
+import ChargeurRessources, { chargeurAudio, chargeurImage } from 'commun/infra/chargeur_ressources';
 import jsdom from 'jsdom-global';
 
 describe('le chargeur de ressources', function () {
-  let chargeur;
-
   beforeEach(function () {
     jsdom('', { url: 'http://localhost' });
-    chargeur = new ChargeurRessources();
   });
 
   it("permet de charger toutes les ressources d'un répertoire", function () {
+    const chargeur = new ChargeurRessources();
     chargeur.charge(require.context('./ressources'));
-    expect(chargeur.promesses.length).to.equal(2);
+    expect(chargeur.promesses.length).to.equal(3);
   });
 
   it('résout la promesse lorsque toutes les ressources sont chargées', function (done) {
-    const images = [];
-    window.Image = class {
-      constructor () {
-        images.push(this);
-      }
-    };
+    const chargeur = new ChargeurRessources({
+      'mp3': (src) => Promise.resolve(),
+      'png': (src) => Promise.resolve()
+    });
     chargeur.charge(require.context('./ressources'));
-    images[0].onload();
-    images[1].onload();
     chargeur.chargement().then(() => done());
   });
 
   it('rejette la promesse lorsque une ressource est en erreur', function (done) {
+    const chargeur = new ChargeurRessources({
+      'mp3': (src) => Promise.resolve(),
+      'png': (src) => Promise.reject(new Error('test'))
+    });
+    chargeur.charge(require.context('./ressources'));
+    chargeur.chargement().catch(() => done());
+  });
+});
+
+describe('le chargeur audio', function () {
+  it('résout la promesse', function (done) {
+    const sons = [];
+    window.Audio = class {
+      constructor (src) {
+        expect(src).to.equal('test.mp3');
+      }
+
+      addEventListener (nomEvenement, callback) {
+        if (nomEvenement === 'canplaythrough') {
+          sons.push(callback);
+        }
+      }
+    };
+    chargeurAudio('test.mp3').then(() => done());
+    sons[0]();
+  });
+
+  it('rejette la promesse', function (done) {
+    const sons = [];
+    window.Audio = class {
+      addEventListener (nomEvenement, callback) {
+        if (nomEvenement === 'error') {
+          sons.push(callback);
+        }
+      }
+    };
+    chargeurAudio('test.mp3').catch(() => done());
+    sons[0]();
+  });
+});
+
+describe("le chargeur d'image", function () {
+  it('résout la promesse', function (done) {
     const images = [];
     window.Image = class {
       constructor () {
         images.push(this);
       }
     };
-    chargeur.charge(require.context('./ressources'));
+    chargeurImage('test.png').then(() => done());
+    images[0].onload();
+    expect(images[0].src).to.equal('test.png');
+  });
+
+  it('rejette la promesse', function (done) {
+    const images = [];
+    window.Image = class {
+      constructor () {
+        images.push(this);
+      }
+    };
+    chargeurImage('test.png').catch(() => done());
     images[0].onerror();
-    chargeur.chargement().catch(done);
   });
 });
