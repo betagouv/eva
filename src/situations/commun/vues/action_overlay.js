59181cb9d490db2807bda1ae7e24c41464733ae2
Stéphane Hanser <shanser@captive.fr>
2019-04-29 14:53:59 +0200

    retire la mixin  pour utiliser uniquement la mixin

--- src/situations/commun/vues/action_overlay.js
+++ src/situations/commun/vues/action_overlay.js
@@ -9,10 +9,12 @@ export default class VueActionOverlay {
 
   affiche (pointInsertion, $) {
     this.$message = $(`<div class='message'>${this.message}</div>`);
-    this.$bouton = $(`<div class="bouton-centre ${this.classe}"><img src="${this.image}"></div>`);
+    const $conteneurBouton = $('<div class="bouton-centre"></div>');
+    this.$bouton = $(`<div class="${this.classe}"><img src="${this.image}"></div>`);
+    $conteneurBouton.append(this.$bouton);
     this.$overlay = $('<div class="overlay"></div>');
 
-    this.$overlay.append(this.$bouton);
+    this.$overlay.append($conteneurBouton);
     this.$overlay.append(this.$message);
 
     this.$bouton.on('click', () => this.click());
