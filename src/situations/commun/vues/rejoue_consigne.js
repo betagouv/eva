dfb4a7173994ed9e5f045941bf0c9173e11bc6ad
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-02 17:42:38 +0200

    Journalise l'événement RejoueConsigne.

--- src/situations/commun/vues/rejoue_consigne.js
+++ src/situations/commun/vues/rejoue_consigne.js
@@ -1,12 +1,14 @@
 import VueBouton from './bouton';
+import EvenementRejoueConsigne from '../modeles/evenement_rejoue_consigne';
 
 import play from 'commun/assets/play.svg';
 import lectureEnCours from 'commun/assets/lecture-en-cours.svg';
 import 'commun/styles/bouton.scss';
 
 export default class VueRejoueConsigne {
-  constructor (consigne) {
+  constructor (consigne, journal) {
     this.consigne = consigne;
+    this.journal = journal;
     this.vueBoutonLire = new VueBouton('bouton-lire-consigne', play, () => this.click(this.$));
     this.vueBoutonLectureEnCours = new VueBouton('bouton-lecture-en-cours', lectureEnCours);
   }
@@ -18,6 +20,7 @@ export default class VueRejoueConsigne {
   }
 
   click ($) {
+    this.journal.enregistre(new EvenementRejoueConsigne());
     this.joueConsigne($);
     this.vueBoutonLire.cache();
     this.vueBoutonLectureEnCours.affiche(this.pointInsertion, $);
--- tests/situations/commun/vues/rejoue_consigne.js
+++ tests/situations/commun/vues/rejoue_consigne.js
@@ -1,15 +1,18 @@
 import jsdom from 'jsdom-global';
 import VueRejoueConsigne from 'commun/vues/rejoue_consigne';
 import Situation from 'commun/modeles/situation';
+import EvenementRejoueConsigne from 'commun/modeles/evenement_rejoue_consigne';
 
 describe('vue Rejoue Consigne', function () {
   let situation;
   let vue;
   let $;
+  let journal;
 
   beforeEach(function () {
     jsdom('<div id="pointInsertion"></div>');
     $ = jQuery(window);
+    journal = {};
     situation = new class extends Situation {
       constructor () {
         super();
@@ -19,11 +22,20 @@ describe('vue Rejoue Consigne', function () {
       }
     }();
 
-    vue = new VueRejoueConsigne(situation.audios.consigne);
+    vue = new VueRejoueConsigne(situation.audios.consigne, journal);
   });
 
   it("sait s'insérer dans une page web", function () {
     vue.affiche('#pointInsertion', $);
     expect($('#pointInsertion .bouton-lire-consigne').length).to.eql(1);
+  });
+
+  it('journalise un événement RejoueConsigne', function (done) {
+    journal.enregistre = (evenement) => {
+      expect(evenement).to.be.a(EvenementRejoueConsigne);
+      done();
+    };
+    vue.affiche('#pointInsertion', $);
+    vue.click($);
   });
 });
