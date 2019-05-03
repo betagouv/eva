2d36f3941959368423ab59de88c2e9b171dd8e08
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-03 12:27:00 +0200

    Enlève la consigne des modèles situation.
    Co-authored-by: Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>

--- src/app/situationControle.js
+++ src/app/situationControle.js
@@ -4,7 +4,6 @@ import { afficheSituation } from 'commun/vues/affiche_situation';
 import DepotRessourcesControle from 'controle/infra/depot_ressources_controle';
 import Situation from 'controle/modeles/situation';
 import VueSituation from 'controle/vues/situation';
-import sonConsigne from 'controle/assets/consigne_demarrage.mp3';
 
 import { scenario } from 'controle/data/pieces';
 
@@ -12,8 +11,7 @@ const situation = new Situation({
   scenario,
   cadence: 5000,
   positionApparitionPieces: { x: 100, y: 64.5 },
-  dureeViePiece: 12000,
-  sonConsigne
+  dureeViePiece: 12000
 });
 
 const depotRessources = new DepotRessourcesControle();
