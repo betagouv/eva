be4fe8ad800cb03adbc05d9a3a452a9266d2673f
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-03 11:18:46 +0200

    Injecte le DepotRessources de la situation dans la vue.
    Co-authored-by: Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>

--- src/app/situationInventaire.js
+++ src/app/situationInventaire.js
@@ -2,18 +2,20 @@ import 'inventaire/styles/app.scss';
 
 import { afficheSituation } from 'commun/vues/affiche_situation';
 
-import { contenants, contenus } from 'inventaire/data/stock';
+import DepotRessourcesInventaire from 'controle/infra/depot_ressources_inventaire';
 import Situation from 'inventaire/modeles/situation';
-
 import VueSituation from 'inventaire/vues/situation';
 
 import consigne from 'inventaire/assets/consigne_demarrage.mp3';
 import reussite from 'inventaire/assets/reussite.mp3';
 import echec from 'inventaire/assets/echec.mp3';
 
+import { contenants, contenus } from 'inventaire/data/stock';
+
 const situation = new Situation(
   { contenants, contenus },
   { consigne, reussite, echec }
 );
 
-afficheSituation('inventaire', situation, VueSituation, require.context('inventaire/assets'));
+const depotRessources = new DepotRessourcesInventaire();
+afficheSituation('inventaire', situation, VueSituation, depotRessources);
--- src/situations/commun/vues/affiche_situation.js
+++ src/situations/commun/vues/affiche_situation.js
@@ -1,25 +1,16 @@
 import uuidv4 from 'uuid/v4';
 
 import DepotJournal from 'commun/infra/depot_journal';
-import DepotRessources from 'commun/infra/depot_ressources';
 import Journal from 'commun/modeles/journal';
 import VueCadre from 'commun/vues/cadre';
 import { initialise as initialiseInternationalisation, traduction } from 'commun/infra/internationalisation';
 import RegistreUtilisateur from 'commun/infra/registre_utilisateur';
 
-export function afficheSituation (nomSituation, modeleSituation, VueSituation, contexte) {
+export function afficheSituation (nomSituation, modeleSituation, VueSituation, depotRessources) {
   function affiche (pointInsertion, $) {
     const session = uuidv4();
     const journal = new Journal(Date.now, session, nomSituation, new DepotJournal(), new RegistreUtilisateur());
     const vueSituation = new VueSituation(modeleSituation, journal);
-
-    const depotRessources = new DepotRessources();
-    const contexteRessourcesCommunes = require.context('commun/assets');
-    const ressourcesCommunes = contexteRessourcesCommunes.keys().map(contexteRessourcesCommunes);
-    depotRessources.charge(ressourcesCommunes);
-
-    const ressourcesSituation = contexte.keys().map(contexte);
-    depotRessources.charge(ressourcesSituation);
 
     const vueCadre = new VueCadre(vueSituation, modeleSituation, journal, depotRessources);
     vueCadre.affiche(pointInsertion, $);
