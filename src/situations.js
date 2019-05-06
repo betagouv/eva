c253cae73305b87f1dc9b9f4412488e8cc570e42
Clément MORISSET <cmorisset@captive.fr>
2019-05-06 14:44:21 +0200

    affiche le lien Magasin de friandise dans Accueil en mode developpement

--- src/situations.js
+++ src/situations.js
@@ -3,6 +3,14 @@ import { traduction } from 'commun/infra/internationalisation';
 export function situations () {
   return [
     { nom: traduction('controle.titre'), chemin: 'controle.html' },
-    { nom: traduction('inventaire.titre'), chemin: 'inventaire.html' }
+    { nom: traduction('inventaire.titre'), chemin: 'inventaire.html' },
+    ...afficheSituationTri()
   ];
+
+  function afficheSituationTri () {
+    if (process.env.NODE_ENV !== 'production') {
+      return [{ nom: traduction('tri.titre'), chemin: 'tri.html' }];
+    }
+    return [];
+  }
 }
