830dac11061f77623976e7edcbcc646dd31dac91
Clément MORISSET <cmorisset@captive.fr>
2019-05-06 14:59:38 +0200

    affiche la situation Tri vide

--- src/app/situationTri.js
+++ src/app/situationTri.js
@@ -1,1 +1,12 @@
 import 'inventaire/styles/app.scss';
+
+import { afficheSituation } from 'commun/vues/affiche_situation';
+
+import DepotRessourcesTri from 'tri/infra/depot_ressources_tri';
+import Situation from 'commun/modeles/situation';
+import VueSituation from 'tri/vues/situation';
+
+const situation = new Situation();
+
+const depotRessources = new DepotRessourcesTri();
+afficheSituation('tri', situation, VueSituation, depotRessources);
