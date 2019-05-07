0b1a022658e950215e7446906a7fe933c4627bfb
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2019-05-07 22:34:53 +0200

    Ajoute une étiquette aux boutons de la barre d'action
    Co-authored-by: François de Metz <francois.de_metz@beta.gouv.fr>

--- tests/situations/commun/vues/bouton.js
+++ tests/situations/commun/vues/bouton.js
@@ -15,4 +15,20 @@ describe('vue Bouton', function () {
     vue.affiche('#point-insertion', $);
     expect($('.bouton-lire-consigne').length).to.eql(1);
   });
+
+  it('sait aussi afficher un libellé', function () {
+    vue.ajouteUneEtiquette('Un texte');
+    vue.affiche('#point-insertion', $);
+
+    expect($('.bouton-et-etiquette').length).to.eql(1);
+    expect($('.bouton-et-etiquette .bouton-lire-consigne').length).to.eql(1);
+    expect($('.bouton-et-etiquette span').text()).to.equal('Un texte');
+  });
+
+  it('sait afficher le libellé à gauche du bouton', function () {
+    vue.ajouteUneEtiquette('Un texte', true);
+    vue.affiche('#point-insertion', $);
+
+    expect($('.bouton-et-etiquette.gauche').length).to.eql(1);
+  });
 });
