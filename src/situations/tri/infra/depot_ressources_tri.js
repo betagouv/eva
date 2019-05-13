0878ee1b3a8d1385bee1b99b8804f48f5ced9454
Stéphane Hanser <shanser@captive.fr>
2019-05-13 10:12:44 +0200

    intègre la consigne du tri

--- src/situations/tri/infra/depot_ressources_tri.js
+++ src/situations/tri/infra/depot_ressources_tri.js
@@ -1,6 +1,6 @@
 import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
 
-import sonConsigne from 'inventaire/assets/consigne_demarrage.mp3';
+import sonConsigne from 'tri/assets/consigne_demarrage.mp3';
 import fondSituation from 'tri/assets/fond-situation.jpg';
 
 export default class DepotRessourcesTri extends DepotRessourcesCommunes {
