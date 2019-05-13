bbfc01f58dc5c2272ce2a42e5efe014ed35b5bd9
Clément MORISSET <cmorisset@captive.fr>
2019-05-13 16:04:14 +0200

    ajoute la notion de position pour les pièces

--- tests/situations/tri/vues/piece.js
+++ tests/situations/tri/vues/piece.js
@@ -9,10 +9,13 @@ describe('Une pièce', function () {
     jsdom('<div id="point-insertion"></div>');
     $ = jQuery(window);
   });
-  it("affiche l'image de la piece en fonction de son type", function () {
-    const piece = new Piece({ image: 'url-image' });
+
+  it("sait s'afficher et se positionner", function () {
+    const piece = new Piece({ image: 'url-image', posY: 200, posX: 30 });
     const vuePiece = new VuePiece(piece);
     vuePiece.affiche('#point-insertion', $);
     expect($('.piece').attr('src')).to.eql('url-image');
+    expect($('.piece').css('top')).to.eql('200px');
+    expect($('.piece').css('left')).to.eql('30px');
   });
 });
