e001be0e7cc4082a30014f341104d2c3c2073314
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-16 16:12:02 +0200

    Retourne les pièces dans le dépot ressources de tri.

--- src/situations/tri/infra/depot_ressources_tri.js
+++ src/situations/tri/infra/depot_ressources_tri.js
@@ -3,13 +3,24 @@ import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
 import sonConsigne from 'tri/assets/consigne_demarrage.mp3';
 import fondSituation from 'tri/assets/fond-situation.jpg';
 
+const bonbons = require.context('tri/assets', false, /bonbon[0-9]+\.png$/);
+
 export default class DepotRessourcesTri extends DepotRessourcesCommunes {
   constructor (chargeurs) {
     super(sonConsigne, chargeurs);
     this.charge([fondSituation]);
+    this.chargeContexte(bonbons);
+    this.bonbons = bonbons.keys().reduce((memo, fichier) => {
+      memo[fichier.match(/(bonbon[0-9]+).png/)[1]] = bonbons(fichier);
+      return memo;
+    }, {});
   }
 
   fondSituation () {
     return this.ressource(fondSituation);
+  }
+
+  piece (type) {
+    return this.bonbons[type];
   }
 }
