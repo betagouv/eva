ee5f3b8ce7f9725a25957e6f98732ed911db5383
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-16 16:25:17 +0200

    Retourne les pièces dans le dépot ressource contrôle.

--- src/situations/controle/infra/depot_ressources_controle.js
+++ src/situations/controle/infra/depot_ressources_controle.js
@@ -2,9 +2,20 @@ import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
 
 import sonConsigne from 'controle/assets/consigne_demarrage.mp3';
 
+const biscuits = require.context('controle/assets', false, /(def[0-9]+|biscuit-normal)\.png$/);
+
 export default class DepotRessourcesControle extends DepotRessourcesCommunes {
   constructor (chargeurs) {
     super(sonConsigne, chargeurs);
     this.chargeContexte(require.context('controle/assets'));
+
+    this.biscuits = biscuits.keys().reduce((memo, fichier) => {
+      memo[fichier.match(/(def[0-9]+|biscuit-normal).png/)[1]] = biscuits(fichier);
+      return memo;
+    }, {});
+  }
+
+  piece (type) {
+    return this.biscuits[type];
   }
 }
