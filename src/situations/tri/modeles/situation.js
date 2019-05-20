903237fb6233f2fb7409543da7cd7160009dec05
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-20 17:02:18 +0200

    Permet à la situation tri d'avoir des bacs.

--- src/situations/tri/modeles/situation.js
+++ src/situations/tri/modeles/situation.js
@@ -1,13 +1,19 @@
 import SituationCommune from 'commun/modeles/situation';
 import Piece from 'commun/modeles/piece';
+import Bac from 'commun/modeles/bac';
 
 export default class Situation extends SituationCommune {
-  constructor ({ pieces }) {
+  constructor ({ pieces, bacs }) {
     super();
     this.pieces = pieces.map((piece) => new Piece({ ...piece, largeur: 7.44, hauteur: 11.3 }));
+    this._bacs = bacs.map((bac) => new Bac({ ...bac, largeur: 7.44, hauteur: 11.3 }));
   }
 
   piecesAffichees () {
     return this.pieces;
+  }
+
+  bacs () {
+    return this._bacs;
   }
 }
--- tests/situations/tri/modeles/situation.js
+++ tests/situations/tri/modeles/situation.js
@@ -1,10 +1,17 @@
+import Bac from 'commun/modeles/bac';
 import Piece from 'commun/modeles/piece';
 import Situation from 'tri/modeles/situation';
 
 describe('La situation « Tri »', function () {
   it('répond que toutes ses pièces sont affichées', function () {
     const pieces = [new Piece({ x: 1, y: 2, largeur: 7.44, hauteur: 11.3 })];
-    let situation = new Situation({ pieces: pieces });
+    const situation = new Situation({ pieces, bacs: [] });
     expect(situation.piecesAffichees()).to.eql(pieces);
+  });
+
+  it('retourne ses bacs', function () {
+    const situation = new Situation({ pieces: [], bacs: [{ categorie: 1, x: 1, y: 2 }] });
+    const bac = new Bac({ x: 1, y: 2, largeur: 7.44, hauteur: 11.3, categorie: 1 });
+    expect(situation.bacs()).to.eql([bac]);
   });
 });
