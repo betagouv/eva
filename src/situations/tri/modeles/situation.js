d96e85cebebca7ee9f70f69488a04bb25c5e1c45
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-21 12:40:35 +0200

    Déclenche l'événement disparation pièce lorsque la pièce est placé sur le bon bac.

--- src/situations/tri/modeles/situation.js
+++ src/situations/tri/modeles/situation.js
@@ -1,12 +1,24 @@
 import SituationCommune from 'commun/modeles/situation';
-import Piece from 'commun/modeles/piece';
+import Piece, { CHANGEMENT_SELECTION } from 'commun/modeles/piece';
 import Bac from 'commun/modeles/bac';
+
+export const DISPARITION_PIECE = 'disparitionPiece';
 
 export default class Situation extends SituationCommune {
   constructor ({ pieces, bacs }) {
     super();
     this.pieces = pieces.map((piece) => new Piece({ ...piece, categorie: piece.type, largeur: 7.44, hauteur: 11.3 }));
     this._bacs = bacs.map((bac) => new Bac({ ...bac, largeur: 15, hauteur: 22.5 }));
+    this.pieces.forEach((piece) => {
+      piece.on(CHANGEMENT_SELECTION, (selectionnee) => {
+        if (!selectionnee) {
+          const bac = this.bacs().find((bac) => bac.contient(piece));
+          if (bac.correspondALaCategorie(piece)) {
+            piece.emit(DISPARITION_PIECE);
+          }
+        }
+      });
+    });
   }
 
   piecesAffichees () {
