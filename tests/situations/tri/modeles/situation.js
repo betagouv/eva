0cdce20839dc86d684b4b8b06610d04b36082fdb
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-21 12:40:35 +0200

    Déclenche l'événement disparation pièce lorsque la pièce est placé sur le bon bac.

--- tests/situations/tri/modeles/situation.js
+++ tests/situations/tri/modeles/situation.js
@@ -1,12 +1,11 @@
 import Bac from 'commun/modeles/bac';
-import Piece from 'commun/modeles/piece';
-import Situation from 'tri/modeles/situation';
+import Situation, { DISPARITION_PIECE } from 'tri/modeles/situation';
 
 describe('La situation « Tri »', function () {
   it('répond que toutes ses pièces sont affichées', function () {
-    const pieces = [new Piece({ x: 1, y: 2, largeur: 7.44, hauteur: 11.3 })];
+    const pieces = [{}];
     const situation = new Situation({ pieces, bacs: [] });
-    expect(situation.piecesAffichees()).to.eql(pieces);
+    expect(situation.piecesAffichees().length).to.eql(1);
   });
 
   it('spécifie que les pièces ont la categorie de leur type', function () {
@@ -19,5 +18,18 @@ describe('La situation « Tri »', function () {
     const situation = new Situation({ pieces: [], bacs: [{ categorie: 1, x: 1, y: 2 }] });
     const bac = new Bac({ x: 1, y: 2, largeur: 15, hauteur: 22.5, categorie: 1 });
     expect(situation.bacs()).to.eql([bac]);
+  });
+
+  it('fait disparaitre la pièce lorsque la pièce est bien placée', function (done) {
+    const situation = new Situation({ pieces: [{ categorie: 1 }], bacs: [{ categorie: 1, x: 1, y: 2 }] });
+    const bac = situation.bacs()[0];
+    const piece = situation.piecesAffichees()[0];
+    bac.contient = () => true;
+    bac.correspondALaCategorie = () => true;
+    piece.on(DISPARITION_PIECE, () => {
+      done();
+    });
+    piece.selectionne({});
+    piece.deselectionne();
   });
 });
