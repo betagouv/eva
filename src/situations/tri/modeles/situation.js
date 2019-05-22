b2490bd49906c624e1fa9f61d13caa5c9dd2c5a5
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-22 16:45:47 +0200

    Stocke dans le résultat le temps passé à trier.
    Co-authored-by: Clément Morisset <clement.morisset@beta.gouv.fr>

--- src/situations/tri/modeles/situation.js
+++ src/situations/tri/modeles/situation.js
@@ -1,13 +1,21 @@
-import SituationCommune, { FINI } from 'commun/modeles/situation';
+import SituationCommune, { CHANGEMENT_ETAT, DEMARRE, FINI } from 'commun/modeles/situation';
 import Piece, { CHANGEMENT_SELECTION, DISPARITION_PIECE } from 'tri/modeles/piece';
 import Bac from 'commun/modeles/bac';
 
 export default class Situation extends SituationCommune {
-  constructor ({ pieces, bacs }) {
+  constructor ({ pieces, bacs }, maintenant = Date.now) {
     super();
+    this.maintenant = maintenant;
+    this.date_debut = 0;
     this.resultat = {
+      temps_passe: 0,
       erreurs: 0
     };
+    this.on(CHANGEMENT_ETAT, (etat) => {
+      if (etat === DEMARRE) {
+        this.date_debut = maintenant();
+      }
+    });
     this._pieces = pieces.map((piece) => new Piece({ ...piece, categorie: piece.type, largeur: 7.44, hauteur: 11.3 }));
     this._bacs = bacs.map((bac) => new Bac({ ...bac, largeur: 15, hauteur: 22.5 }));
     this._pieces.forEach((piece) => {
@@ -30,6 +38,7 @@ export default class Situation extends SituationCommune {
     piece.emit(DISPARITION_PIECE);
 
     if (this.piecesAffichees().length === 0) {
+      this.resultat.temps_passe = this.maintenant() - this.date_debut;
       this.modifieEtat(FINI);
     }
   }
