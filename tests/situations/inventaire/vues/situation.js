2566b83580c5d9aad4b7e788db0bdf47f5b33a50
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-24 16:33:40 +0200

    Envoi l'événement de démarrage de la même manière pour toutes les situations.
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- tests/situations/inventaire/vues/situation.js
+++ tests/situations/inventaire/vues/situation.js
@@ -1,8 +1,6 @@
 import jsdom from 'jsdom-global';
 import { unMagasinVide } from '../aides/magasin';
 import VueSituation from 'inventaire/vues/situation';
-import { CHANGEMENT_ETAT, DEMARRE } from 'commun/modeles/situation';
-import EvenementDemarrage from 'commun/modeles/evenement_demarrage';
 
 describe('La situation « Inventaire »', function () {
   let $;
@@ -26,14 +24,5 @@ describe('La situation « Inventaire »', function () {
     vue.affiche('#point-insertion', $);
 
     expect($('#point-insertion .etageres').length).to.equal(1);
-  });
-
-  it("incrit le journal à l'événement démarrage", function (done) {
-    mockJournal.enregistre = (evenement) => {
-      expect(evenement).to.be.a(EvenementDemarrage);
-      done();
-    };
-
-    situation.emit(CHANGEMENT_ETAT, DEMARRE);
   });
 });
