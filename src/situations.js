c809790bd2a859732302046fdb5955863ba786a5
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-26 10:11:10 +0200

    Affiche le tri par défaut.

--- src/situations.js
+++ src/situations.js
@@ -4,13 +4,6 @@ export function situations () {
   return [
     { nom: traduction('controle.titre'), chemin: 'controle.html' },
     { nom: traduction('inventaire.titre'), chemin: 'inventaire.html' },
-    ...afficheSituationTri()
+    { nom: traduction('tri.titre'), chemin: 'tri.html' }
   ];
-
-  function afficheSituationTri () {
-    if (process.env.NODE_ENV !== 'production') {
-      return [{ nom: traduction('tri.titre'), chemin: 'tri.html' }];
-    }
-    return [];
-  }
 }
