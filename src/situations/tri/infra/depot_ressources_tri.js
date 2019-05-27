0e6316b452050e7b069298e9f0172bb2277f3d0b
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-27 14:44:12 +0200

    Ajoute les sons de bon ou de mauvais placement de bonbon dans un bac.

--- src/situations/tri/infra/depot_ressources_tri.js
+++ src/situations/tri/infra/depot_ressources_tri.js
@@ -2,13 +2,15 @@ import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
 
 import sonConsigne from 'tri/assets/consigne_demarrage.wav';
 import fondSituation from 'tri/assets/fond-situation.jpg';
+import sonBonBac from 'tri/assets/bon-bac.wav';
+import sonMauvaisBac from 'tri/assets/mauvais-bac.wav';
 
 const bonbons = require.context('tri/assets', false, /bonbon[0-9]+\.png$/);
 
 export default class DepotRessourcesTri extends DepotRessourcesCommunes {
   constructor (chargeurs) {
     super(sonConsigne, chargeurs);
-    this.charge([fondSituation]);
+    this.charge([fondSituation, sonBonBac, sonMauvaisBac]);
     this.chargeContexte(bonbons);
     this.bonbons = bonbons.keys().reduce((memo, fichier) => {
       memo[fichier.match(/(bonbon[0-9]+).png/)[1]] = bonbons(fichier);
@@ -22,5 +24,13 @@ export default class DepotRessourcesTri extends DepotRessourcesCommunes {
 
   piece (type) {
     return this.bonbons[type];
+  }
+
+  sonBonBac () {
+    return this.ressource(sonBonBac);
+  }
+
+  sonMauvaisBac () {
+    return this.ressource(sonMauvaisBac);
   }
 }
