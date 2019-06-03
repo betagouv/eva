9bf79914de12d5ea1bebb97c2f298171dbad248e
Stéphane Hanser <shanser@captive.fr>
2019-06-03 15:52:00 +0200

    intègre l'illustration d'accueil avec un effet de survol

--- src/situations.js
+++ src/situations.js
@@ -2,8 +2,8 @@ import { traduction } from 'commun/infra/internationalisation';
 
 export function situations () {
   return [
-    { nom: traduction('controle.titre'), chemin: 'controle.html' },
-    { nom: traduction('inventaire.titre'), chemin: 'inventaire.html' },
-    { nom: traduction('tri.titre'), chemin: 'tri.html' }
+    { nom: traduction('controle.titre'), chemin: 'controle.html', identifiant: 'controle' },
+    { nom: traduction('inventaire.titre'), chemin: 'inventaire.html', identifiant: 'inventaire' },
+    { nom: traduction('tri.titre'), chemin: 'tri.html', identifiant: 'tri' }
   ];
 }
