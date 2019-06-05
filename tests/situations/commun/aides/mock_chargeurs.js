a95e62d4b31d112b86d03afec00d0d141ceee4ff
Clément MORISSET <cmorisset@captive.fr>
2019-06-05 17:43:32 +0200

    modifie le mock par défaut pour qu'il renvoit un src
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- tests/situations/commun/aides/mock_chargeurs.js
+++ tests/situations/commun/aides/mock_chargeurs.js
@@ -1,5 +1,5 @@
 import MockAudioNode from '../aides/mock_audio_node';
-const chargeurDefaut = () => Promise.resolve(() => {});
+const chargeurDefaut = (src) => Promise.resolve(() => { return src; });
 
 export default function (chargeurs = {}) {
   return {
