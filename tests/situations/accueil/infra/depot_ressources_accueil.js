cbab70a0aaab8f6bba5417b3aaed3fb6e7b7edec
Clément MORISSET <cmorisset@captive.fr>
2019-06-05 17:39:20 +0200

    ajoute depotRessourceAccueil
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

---
+++ tests/situations/accueil/infra/depot_ressources_accueil.js
@@ -0,0 +1,22 @@
+import DepotRessources from 'commun/infra/depot_ressources';
+import DepotRessourcesAccueil from 'accueil/infra/depot_ressources_accueil';
+import chargeurs from '../../commun/aides/mock_chargeurs';
+
+describe('Le dépôt de ressources de la situation accueil', function () {
+  let depot;
+
+  beforeEach(function () {
+    depot = new DepotRessourcesAccueil(chargeurs());
+    return depot.chargement();
+  });
+
+  it('étend DepotRessources', function () {
+    expect(depot).to.be.a(DepotRessources);
+  });
+
+  it('retourne les bâtiments', function () {
+    expect(depot.batimentSituation('tri')).to.eql('assets/tri.png');
+    expect(depot.batimentSituation('inventaire')).to.eql('assets/inventaire.png');
+    expect(depot.batimentSituation('controle')).to.eql('assets/controle.png');
+  });
+});
