f6f10ba8a2080e6b6b89655591a6e98d3e221456
Clément MORISSET <cmorisset@captive.fr>
2019-06-21 16:51:09 +0200

    Envoie le compte-rendu au serveur
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>
    Co-authored-by: Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>

--- src/situations/commun/modeles/journal.js
+++ src/situations/commun/modeles/journal.js
@@ -1,4 +1,9 @@
-export default class Journal {
+import uuidv4 from 'uuid/v4';
+
+import DepotJournal from 'commun/infra/depot_journal';
+import RegistreUtilisateur from 'commun/infra/registre_utilisateur';
+
+export class Journal {
   constructor (maintenant, session, situation, depot, registre) {
     this.maintenant = maintenant;
     this.depot = depot;
@@ -18,4 +23,11 @@ export default class Journal {
     };
     return this.depot.enregistre(payLoad, timeout);
   }
+}
+
+export default function creeJournalPourSituation (identifiantSituation) {
+  const session = uuidv4();
+  const depotJournal = new DepotJournal();
+  const registreUtilisateur = new RegistreUtilisateur();
+  return new Journal(Date.now, session, identifiantSituation, depotJournal, registreUtilisateur);
 }
