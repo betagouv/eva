28f4bef4bbad9b0327f05bc5594b7e562801f3d8
Clément MORISSET <cmorisset@captive.fr>
2019-06-26 15:05:24 +0200

    affiche la date et l'heure courante dans les informations de l'email réceptionné

--- src/situations/compte_rendu/vues/situation.js
+++ src/situations/compte_rendu/vues/situation.js
@@ -11,13 +11,16 @@ export default class VueSituation {
   }
 
   affiche (pointInsertion, $) {
+    const dateCourante = new Date().toLocaleDateString();
+    const heureSansSecondes = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
     const $vue = $(`
       <div class="situation">
         <div class="scene-compte-rendu">
           <img class="illustration" src=${this.depotRessources.accidentCarine().src}></img>
           <div class="messagerie">
             <p class="messagerie-sujet">${traduction('compte_rendu.objet_message_catherine')}</p>
-            <p class="messagerie-expediteur">${traduction('compte_rendu.informations_expediteur')}</p>
+            <p class="messagerie-expediteur">${traduction('compte_rendu.informations_expediteur', { date_courante: dateCourante, heure_sans_secondes: heureSansSecondes })}</p>
             <p class='message-catherine-jean'>
               ${traduction('compte_rendu.message_catherine_jean')}
             </p>
