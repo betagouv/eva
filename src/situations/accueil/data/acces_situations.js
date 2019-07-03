3a2b78fb03cc7557757b22e179f30e88939d1c3c
Clément MORISSET <cmorisset@captive.fr>
2019-07-03 16:59:08 +0200

    retire la situation Compte-rendu du fichier data/acces_situations

--- src/situations/accueil/data/acces_situations.js
+++ src/situations/accueil/data/acces_situations.js
@@ -7,7 +7,6 @@ export function accesSituations () {
   return [
     new AccesSituation({ nom: traduction('tri.titre'), chemin: 'tri.html', identifiant: 'tri', niveauMinimum: 1 }),
     new AccesSituation({ nom: traduction('controle.titre'), chemin: 'controle.html', identifiant: 'controle', niveauMinimum: 2 }),
-    new AccesSituation({ nom: traduction('inventaire.titre'), chemin: 'inventaire.html', identifiant: 'inventaire', niveauMinimum: 3 }),
-    new AccesSituation({ nom: traduction('compte_rendu.titre'), chemin: 'compte_rendu.html', identifiant: IDENTIFIANT_SITUATION_COMPTE_RENDU })
+    new AccesSituation({ nom: traduction('inventaire.titre'), chemin: 'inventaire.html', identifiant: 'inventaire', niveauMinimum: 3 })
   ];
 }
