cd411f1f4c18f69b8caec5cdd0e8fc216353ad8b
Clément MORISSET <cmorisset@captive.fr>
2019-07-10 17:50:38 +0200

    supprime le chargement des assets pour la situation Questions dans le depotRessourcesAccueil

--- src/situations/accueil/infra/depot_ressources_accueil.js
+++ src/situations/accueil/infra/depot_ressources_accueil.js
@@ -4,21 +4,19 @@ import personnages from 'accueil/assets/personnages.png';
 import tri from 'accueil/assets/tri.png';
 import inventaire from 'accueil/assets/inventaire.png';
 import controle from 'accueil/assets/controle.png';
-import questions from 'accueil/assets/1px-transparent.png';
 
 const progression = require.context('accueil/assets', false, /progression[1-4]\.png$/);
 
 const batiments = {
   'tri': tri,
   'inventaire': inventaire,
-  'controle': controle,
-  'questions': questions
+  'controle': controle
 };
 
 export default class DepotRessourcesAccueil extends DepotRessources {
   constructor (chargeurs) {
     super(chargeurs);
-    this.charge([fondAccueil, personnages, tri, inventaire, controle, questions]);
+    this.charge([fondAccueil, personnages, tri, inventaire, controle]);
     this.chargeContexte(progression);
     this._progression = progression.keys().reduce((memo, fichier) => {
       memo[fichier.match(/progression([1-4]).png/)[1]] = progression(fichier);
