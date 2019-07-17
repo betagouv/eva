23fc6df44d30a0fb59ce66739314d9b77c460491
François de Metz <francois.de_metz@beta.gouv.fr>
2019-07-17 09:45:31 +0200

    Utilise la nouvelle consigne dans la situation questions.

--- src/situations/questions/infra/depot_ressources_questions.js
+++ src/situations/questions/infra/depot_ressources_questions.js
@@ -3,7 +3,7 @@ import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
 import accidentCarine from 'questions/assets/accident_carine.png';
 import palette from 'questions/assets/palette.png';
 
-import sonConsigne from 'controle/assets/consigne_demarrage.wav';
+import sonConsigne from 'questions/assets/consigne_demarrage.wav';
 
 export default class DepotRessourcesQuestions extends DepotRessourcesCommunes {
   constructor (chargeurs) {
