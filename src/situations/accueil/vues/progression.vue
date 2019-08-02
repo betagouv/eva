df256fc16e4f6489dede3701f5c89071001edf2d
François de Metz <francois.de_metz@beta.gouv.fr>
2019-08-02 11:33:48 +0200

    Réécrit la progression avec vue.
    Co-authored-by: Stéphane Hanser <stephane.hanser@beta.gouv.fr>
    Co-authored-by: Clément Morisset <clement.morisset@beta.gouv.fr>

---
+++ src/situations/accueil/vues/progression.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    :style="{ 'background-image': backgroundImage }"
+    class="progression"
+  ></div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['niveauActuel']),
+
+    backgroundImage() {
+      return `url('${this.depotRessources.progression(this.niveauActuel).src}')`;
+    }
+  }
+}
+</script>
