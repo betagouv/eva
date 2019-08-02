d781fda3c72fe71720564f03f58374515c0bf369
François de Metz <francois.de_metz@beta.gouv.fr>
2019-08-02 12:17:34 +0200

    Restaure l'animation sur le formulaire d'identification.
    Co-authored-by: Stéphane Hanser <stephane.hanser@beta.gouv.fr>
    Co-authored-by: Clément Morisset <clement.morisset@beta.gouv.fr>

--- src/situations/accueil/vues/formulaire_identification.vue
+++ src/situations/accueil/vues/formulaire_identification.vue
@@ -1,41 +1,43 @@
 <template>
-  <form
-    v-if="!estConnecte"
-    id="formulaire-identification"
-    class="formulaire-identification"
-    @submit.prevent="envoieFormulaire">
-    <h2>{{ traduction('accueil.identification.titre') }}</h2>
-    <label for="formulaire-identification-input-nom">
-      {{ traduction('accueil.identification.label') }}
-    </label>
-    <div class="element-formulaire">
-      <input
-        v-model.trim="nom"
-        type="text"
-        class="input-accueil"
-        autofocus>
-      <span
-        v-if="erreurs.nom"
-        class="erreur">{{ erreurs.nom[0] }}</span>
-    </div>
-    <label for="formulaire-identification-input-campagne">
-      {{ traduction('accueil.identification.campagne') }}
-    </label>
-    <div class="element-formulaire">
-      <input
-        v-model.trim="campagne"
-        type="text"
-        class="input-accueil">
-      <span
-        v-if="erreurs.campagne"
-        class="erreur">{{ erreurs.campagne[0] }}</span>
-    </div>
-    <div class="element-formulaire">
-      <button
-        :disabled="estDesactive"
-        class="bouton-arrondi">{{ traduction('accueil.identification.boutton') }}</button>
-    </div>
-  </form>
+  <transition name="fade">
+    <form
+      v-if="!estConnecte"
+      id="formulaire-identification"
+      class="formulaire-identification"
+      @submit.prevent="envoieFormulaire">
+      <h2>{{ traduction('accueil.identification.titre') }}</h2>
+      <label for="formulaire-identification-input-nom">
+        {{ traduction('accueil.identification.label') }}
+      </label>
+      <div class="element-formulaire">
+        <input
+          v-model.trim="nom"
+          type="text"
+          class="input-accueil"
+          autofocus>
+        <span
+          v-if="erreurs.nom"
+          class="erreur">{{ erreurs.nom[0] }}</span>
+      </div>
+      <label for="formulaire-identification-input-campagne">
+        {{ traduction('accueil.identification.campagne') }}
+      </label>
+      <div class="element-formulaire">
+        <input
+          v-model.trim="campagne"
+          type="text"
+          class="input-accueil">
+        <span
+          v-if="erreurs.campagne"
+          class="erreur">{{ erreurs.campagne[0] }}</span>
+      </div>
+      <div class="element-formulaire">
+        <button
+          :disabled="estDesactive"
+          class="bouton-arrondi">{{ traduction('accueil.identification.boutton') }}</button>
+      </div>
+    </form>
+  </transition>
 </template>
 
 <script>
@@ -80,3 +82,12 @@ export default {
   }
 }
 </script>
+
+<style>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .3s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>
