31cdd8d78e8da409316eaef650380f47950559cf
Stéphane Hanser <shanser@captive.fr>
2019-08-05 11:00:10 +0200

    supprime le fichier de données des situations
    elles proviennent désormais du serveur
    
    Co-authored-by: François de Metz <francois.de_metz@beta.gouv.fr>

--- src/app/situation_questions.js
+++ src/app/situation_questions.js
@@ -6,9 +6,8 @@ import { afficheSituation } from 'commun/vues/affiche_situation';
 import DepotRessourcesQuestions from 'questions/infra/depot_ressources_questions';
 import Situation from 'questions/modeles/situation';
 import VueSituation from 'questions/vues/situation';
-import { IDENTIFIANT_SITUATION_QUESTIONS } from 'accueil/data/acces_situations';
 
 const situation = new Situation();
 
 const depotRessources = new DepotRessourcesQuestions();
-afficheSituation(IDENTIFIANT_SITUATION_QUESTIONS, situation, VueSituation, depotRessources);
+afficheSituation('questions', situation, VueSituation, depotRessources);
