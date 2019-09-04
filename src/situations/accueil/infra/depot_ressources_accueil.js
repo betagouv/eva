4cf180fda08367f335d92e66c2c7a68849e77393
François de Metz <francois.de_metz@beta.gouv.fr>
2019-09-04 09:23:34 +0200

    Supprime les images de progression.

--- src/situations/accueil/infra/depot_ressources_accueil.js
+++ src/situations/accueil/infra/depot_ressources_accueil.js
@@ -6,8 +6,6 @@ import inventaire from 'accueil/assets/inventaire.png';
 import controle from 'accueil/assets/controle.png';
 import questions from 'accueil/assets/bureau.png';
 
-const progression = require.context('accueil/assets', false, /progression[1-5]-sur-[1-5]\.png$/);
-
 const batiments = {
   tri,
   inventaire,
@@ -19,12 +17,6 @@ export default class DepotRessourcesAccueil extends DepotRessources {
   constructor (chargeurs) {
     super(chargeurs);
     this.charge([fondAccueil, personnage, tri, inventaire, controle, questions]);
-    this.chargeContexte(progression);
-    this._progression = progression.keys().reduce((memo, fichier) => {
-      const match = fichier.match(/progression([1-5])-sur-([1-5])\.png$/);
-      memo[match[1] + '-' + match[2]] = progression(fichier);
-      return memo;
-    }, {});
   }
 
   fondAccueil () {
@@ -37,12 +29,5 @@ export default class DepotRessourcesAccueil extends DepotRessources {
 
   batimentSituation (situation) {
     return this.ressource(batiments[situation]);
-  }
-
-  progression (niveau, dernierNiveau) {
-    if (!dernierNiveau) {
-      return this.ressource(Object.values(this._progression)[0]);
-    }
-    return this.ressource(this._progression[niveau + '-' + dernierNiveau]);
   }
 }
