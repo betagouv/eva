d3df9c7df046d5ab8ad42fe6593c2febed698ab4
François de Metz <francois.de_metz@beta.gouv.fr>
2019-09-13 18:04:10 +0200

    Affiche les batiments accueil et fin.

--- src/situations/accueil/vues/accueil.vue
+++ src/situations/accueil/vues/accueil.vue
@@ -24,11 +24,11 @@
           :style="{ left: `${-decalageGaucheVue(indexBatiment)}px`}"
           class="acces-situations">
           <acces-situation
-            v-for="(situation, index) in situations"
+            v-for="(situation, index) in batiments"
             :key="situation.identifiant"
             :situation="situation"
             :style="{ left: `${decalageGaucheBatiment(index)}px`}"
-            :desactivee="indexBatiment - 1 !== index"
+            :desactivee="indexBatiment !== index"
            />
         </div>
       </div>
@@ -76,6 +76,12 @@
       </div>
       <formulaire-identification :force-campagne="forceCampagne" />
       <transition-fade>
+        <overlay-intro
+          v-if="estConnecte && indexBatiment === 0"
+          @passe="passeIntro"
+        />
+      </transition-fade>
+      <transition-fade>
         <fin v-if="ecranFinAfficher" />
       </transition-fade>
     </div>
@@ -83,7 +89,7 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex';
+import { mapState } from 'vuex';
 import 'accueil/styles/accueil.scss';
 import 'commun/styles/cadre.scss';
 import 'commun/styles/actions.scss';
@@ -94,6 +100,7 @@ import FormulaireIdentification from './formulaire_identification';
 import AccesSituation from 'accueil/vues/acces_situation';
 import BoiteUtilisateur from 'commun/vues/boite_utilisateur';
 import Fin from 'accueil/vues/fin';
+import OverlayIntro from 'accueil/vues/overlay_intro';
 import TransitionFade from 'commun/vues/transition_fade';
 
 const LARGEUR_SCENE = 1008;
@@ -101,13 +108,14 @@ const LARGEUR_BATIMENT = 411;
 const DECALAGE_INITIAL = LARGEUR_SCENE / 2 - LARGEUR_BATIMENT / 2;
 const ESPACEMENT_BATIMENT = (LARGEUR_SCENE - 1.5 * LARGEUR_BATIMENT) / 2;
 
-export const CLE_NIVEAU_PRECEDENT = 'niveauPrecedent';
+export const CLE_ETAT_ACCUEIL = 'etatAccueil';
 
 export default {
-  components: { FormulaireIdentification, AccesSituation, BoiteUtilisateur, Fin, TransitionFade },
+  components: { FormulaireIdentification, AccesSituation, BoiteUtilisateur, OverlayIntro, Fin, TransitionFade },
 
   data() {
     const parsedUrl = new URL(window.location.href);
+    const { indexPrecedent } = this.recupereEtatDuPrecedentChargement();
     return {
       fondAccueil: `url(${this.depotRessources.fondAccueil().src})`,
       personnage: this.depotRessources.personnage().src,
@@ -115,59 +123,88 @@ export default {
       suivant: this.depotRessources.suivant().src,
       punaise: this.depotRessources.punaise().src,
       forceCampagne: parsedUrl.searchParams.get('code') || '',
-      indexBatiment: this.recupereNiveauDuPrecedentChargement(),
+      indexBatiment: indexPrecedent,
       ecranFinAfficher: false
     };
   },
 
   computed: {
-    ...mapState(['situations', 'estConnecte']),
-    ...mapGetters(['niveauActuel']),
+    ...mapState(['situations', 'estConnecte', 'situationsFaites']),
 
     positionFond () {
-      return (this.indexBatiment - 1) * 80;
+      return (this.indexBatiment) * 80;
     },
 
     precedentDesactivee () {
-      return this.indexBatiment === 1;
+      return this.indexBatiment === 0;
     },
 
     suivantDesactivee () {
-      return this.indexBatiment === this.niveauActuel;
+      return this.indexBatiment === this.niveauMax;
     },
 
     situationActuelle () {
-      if (this.situations.length === 0 || this.termine)
+      if (this.situations.length === 0 || this.termine || this.indexBatiment < 1)
         return null;
       return this.situations[this.indexBatiment - 1];
     },
 
     termine () {
-      return this.indexBatiment > this.situations.length;
+      return this.indexBatiment === this.batiments.length - 1;
+    },
+
+    batiments () {
+      return [
+        {
+          identifiant: 'bienvenue',
+          nom: 'Bienvenue'
+        },
+        ...this.situations,
+        {
+          identifiant: 'fin',
+          nom: 'Terminer'
+        }
+      ]
+    },
+
+    niveauMax () {
+      return this.situationsFaites.length + 1;
     }
   },
 
   mounted () {
     this.synchroniseSituations();
-    this.sauvegardeNiveauPourProchainChargement();
   },
 
   watch: {
     estConnecte () {
-      this.synchroniseSituations();
+      this.synchroniseSituations(false);
       if (!this.estConnecte) {
+        this.reinitialiseDonnees();
+      } else {
         this.indexBatiment = 0;
-        this.ecranFinAfficher = false;
-        this.sauvegardeNiveauPourProchainChargement();
       }
+    },
+
+    indexBatiment () {
+      this.sauvegardeEtatPourProchainChargement();
     }
   },
 
   methods: {
-    synchroniseSituations () {
+    synchroniseSituations (sync = true) {
       if (!this.estConnecte) return;
       this.$store.dispatch('synchroniseSituations')
-        .then(() => this.indexBatiment = this.niveauActuel );
+        .then(() => {
+          if (sync) {
+            this.indexBatiment = this.niveauMax;
+          }
+        });
+    },
+
+    reinitialiseDonnees () {
+      this.indexBatiment = -0.5;
+      this.ecranFinAfficher = false;
     },
 
     decalageGaucheBatiment (index) {
@@ -175,16 +212,23 @@ export default {
     },
 
     decalageGaucheVue (niveau) {
-      return (niveau - 1) * (LARGEUR_BATIMENT + ESPACEMENT_BATIMENT);
+      return (niveau) * (LARGEUR_BATIMENT + ESPACEMENT_BATIMENT);
     },
 
-    sauvegardeNiveauPourProchainChargement () {
-      window.localStorage.setItem(CLE_NIVEAU_PRECEDENT, this.niveauActuel);
+    sauvegardeEtatPourProchainChargement () {
+      window.localStorage.setItem(CLE_ETAT_ACCUEIL, JSON.stringify({
+        indexPrecedent: this.indexBatiment
+      }));
     },
 
-    recupereNiveauDuPrecedentChargement () {
-      const precedentNiveau = window.localStorage.getItem(CLE_NIVEAU_PRECEDENT);
-      return parseInt(precedentNiveau);
+    recupereEtatDuPrecedentChargement () {
+      const etatPrecedent = window.localStorage.getItem(CLE_ETAT_ACCUEIL);
+      if (!etatPrecedent) return { indexPrecedent: -0.5 };
+      return JSON.parse(etatPrecedent);
+    },
+
+    passeIntro () {
+      this.indexBatiment++;
     },
 
     afficheEcranFin () {
---
+++ src/situations/accueil/vues/overlay_intro.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="overlay modale">
+    <div>
+      <h2>{{ traduction('accueil.intro.titre') }}</h2>
+      <p>{{ traduction('accueil.intro.message') }}</p>
+      <button
+        class="bouton-arrondi"
+        @click="passe"
+      >{{ traduction('accueil.intro.bouton') }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  methods: {
+    passe () {
+      this.$emit('passe');
+    }
+  }
+};
+</script>
