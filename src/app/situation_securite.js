906143104440cf10c9c8065d387f6f2b7cd3ee75
François de Metz <francois.de_metz@beta.gouv.fr>
2019-09-24 16:15:54 +0200

    Affiche le fond de la situation.
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- src/app/situation_securite.js
+++ src/app/situation_securite.js
@@ -2,12 +2,11 @@ import 'commun/infra/report_erreurs';
 
 import { afficheSituation } from 'commun/vues/affiche_situation';
 
-import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
+import DepotRessourcesSecurite from 'securite/infra/depot_ressources_securite';
 import Situation from 'commun/modeles/situation';
 import VueSituation from 'securite/vues/situation';
-import sonConsigne from 'commun/assets/consigne_demarrage_blanche.wav';
 
 const situation = new Situation();
 
-const depotRessources = new DepotRessourcesCommunes(sonConsigne);
+const depotRessources = new DepotRessourcesSecurite();
 afficheSituation('securite', situation, VueSituation, depotRessources);
