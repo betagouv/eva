5ada06ebd17d7516b4674236b6e8a70aa60d5637
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2019-09-30 17:50:48 +0200

    Generalise identification_danger en formulaire_radio

---
+++ src/situations/securite/vues/formulaire_radio.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h3>{{ question.titre }}</h3>
+    <div
+      v-for="option in question.options"
+      class="identification-danger-input">
+      <label>
+        <input v-model="choix" type="radio" name="option" :value="option.valeur" />
+        {{ option.libelle }}
+      </label>
+    </div>
+    <button
+      :disabled="desactivee"
+      @click="question.click"
+      class="bouton-arrondi bouton-arrondi--petit">{{ traduction('securite.danger.bouton') }}
+    </button>
+  </div>
+</template>
+
+<script>
+import 'securite/styles/identification_danger.scss';
+
+export default {
+  props: {
+    question: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      choix: ""
+    };
+  },
+
+  computed: {
+    desactivee () {
+      return this.choix === "";
+    },
+  }
+}
+
+</script>
+
