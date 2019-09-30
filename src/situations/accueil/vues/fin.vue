ce4cc6130130e7e882a67c4777a75702c5860dbb
Clément MORISSET <cmorisset@captive.fr>
2019-09-30 15:17:08 +0200

    n'affiche le message d'identification des compétences si aucune compétences n'est renvoyé par l'api

--- src/situations/accueil/vues/fin.vue
+++ src/situations/accueil/vues/fin.vue
@@ -2,10 +2,13 @@
   <div class="overlay modale">
     <div class= "modale-interieur">
       <h2>{{ traduction('accueil.fin.titre') }}</h2>
-      <p class="message-fin">{{ traduction('accueil.fin.message') }}</p>
-      <li class="competences" v-for="(competence, index) in competencesFortes">
-        {{ traduction(`accueil.fin.${Object.keys(competence).join()}`) }}
-      </li>
+      <p class="message-base">{{ traduction('accueil.fin.message') }}</p>
+      <div class='competences-fortes-conteneur' v-if="this.competencesFortes.length != 0" >
+        <p class="message-competence">{{ traduction('accueil.fin.competences') }}</p>
+        <li class="competences" v-for="(competence, index) in competencesFortes">
+          {{ traduction(`accueil.fin.${Object.keys(competence).join()}`) }}
+        </li>
+      </div>
       <button
         class="bouton-arrondi"
         @click="deconnecte"
