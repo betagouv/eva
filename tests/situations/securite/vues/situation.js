3f269811990a245fc9ef3ecde8dfc1b8d0924a00
François de Metz <francois.de_metz@beta.gouv.fr>
2019-10-01 15:31:41 +0200

    Passe la situation en FINI une fois tout les dangers qualifiés.
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- tests/situations/securite/vues/situation.js
+++ tests/situations/securite/vues/situation.js
@@ -1,6 +1,6 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Situation from 'securite/vues/situation.vue';
-import { creeStore } from 'securite/store/store';
+import { creeStore, CHARGEMENT, FINI } from 'securite/store/store';
 
 describe('La vue de la situation Sécurité', function () {
   let wrapper;
@@ -35,5 +35,16 @@ describe('La vue de la situation Sécurité', function () {
     expect(wrapper.findAll('.zone-selectionnee').length).to.eql(0);
     wrapper.find('.zone').trigger('click');
     expect(wrapper.findAll('.zone-selectionnee').length).to.eql(1);
+  });
+
+  it('passe la situation en FINI lorsque tout les dangers ont été identifiés', function () {
+    store.commit('chargeZonesEtDangers', {
+      zones: [{ x: 1, y: 2, r: 3, danger: 'danger1' }, { x: 4, y: 5, r: 6, danger: 'danger2' }],
+      dangers: { danger1: {}, danger2: {} }
+    });
+    store.commit('ajouteDangerQualifie', 'danger1');
+    expect(store.state.etat).to.equal(CHARGEMENT);
+    store.commit('ajouteDangerQualifie', 'danger2');
+    expect(store.state.etat).to.equal(FINI);
   });
 });
