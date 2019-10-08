13d9ef8730246508802656751d6daec013bc78bb
François de Metz <francois.de_metz@beta.gouv.fr>
2019-10-08 16:20:21 +0200

    Ajoute des zones de non dangers.

--- src/situations/securite/vues/fenetre_zone.vue
+++ src/situations/securite/vues/fenetre_zone.vue
@@ -54,11 +54,11 @@ export default {
       return this.formatePourcentage((this.zone.y + Math.sin(Math.PI / 4) * this.zone.r).toFixed(1));
     },
     left () {
-      if (this.zone.x >= 80) return undefined;
+      if (this.zone.x >= 70) return undefined;
       return this.formatePourcentage((this.zone.x + Math.cos(Math.PI / 4) * this.zone.r).toFixed(1));
     },
     right () {
-      if (this.zone.x < 80) return undefined;
+      if (this.zone.x < 70) return undefined;
       return this.formatePourcentage((100 - (this.zone.x + Math.cos(3 * Math.PI / 4) * this.zone.r)).toFixed(1));
     },
     qualificationDanger () {
