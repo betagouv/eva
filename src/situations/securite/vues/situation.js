565f1d0179e057c4585be5748db703671f072e81
François de Metz <francois.de_metz@beta.gouv.fr>
2019-10-09 13:11:42 +0200

    Enregistre l'événement de qualification de danger.
    Co-authored-by: Stéphane Hanser <stephane.hanser@beta.gouv.fr>

--- src/situations/securite/vues/situation.js
+++ src/situations/securite/vues/situation.js
@@ -3,12 +3,14 @@ import Vue from 'vue';
 import { traduction } from 'commun/infra/internationalisation';
 import { creeStore, synchroniseStoreEtModeleSituation } from '../store/store';
 import { zones, dangers } from '../data/zones';
+import rapporteAuJournal from '../modeles/rapporteur';
 import Situation from './situation.vue';
 
 export default class VueSituation {
   constructor (situation, journal, depotRessources, registreUtilisateur) {
     this.situation = situation;
     this.depotRessources = depotRessources;
+    this.journal = journal;
 
     Vue.prototype.depotRessources = depotRessources;
     Vue.prototype.traduction = traduction;
@@ -20,6 +22,7 @@ export default class VueSituation {
     const store = creeStore();
     store.commit('chargeZonesEtDangers', { zones, dangers });
     synchroniseStoreEtModeleSituation(this.situation, store);
+    rapporteAuJournal(store, this.journal);
     new Vue({
       store,
       render: createEle => createEle(Situation)
