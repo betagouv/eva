442e3c7b84f2f5dd555479cefacf8d115d0f51f1
Stéphane Hanser <shanser@captive.fr>
2019-10-11 12:09:09 +0200

    Émet un évenement à l'activation de l'aide dans la situation Sécurité
    Co-authored-by: François de Metz <francois.de_metz@beta.gouv.fr>

---
+++ tests/situations/securite/modeles/evenement_activation_aide.js
@@ -0,0 +1,7 @@
+import EvenementActivationAide from 'securite/modeles/evenement_activation_aide';
+
+describe("l'événement de l'activation de l'aide", function () {
+  it('retourne son nom', function () {
+    expect(new EvenementActivationAide().nom()).to.eql('activationAide');
+  });
+});
--- tests/situations/securite/vues/bouton_aide.js
+++ tests/situations/securite/vues/bouton_aide.js
@@ -1,15 +1,20 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import BoutonAide from 'securite/vues/bouton_aide';
 import { creeStore } from 'securite/store/store';
+import EvenementActivationAide from 'securite/modeles/evenement_activation_aide';
 
 describe("Le bouton d'aide", function () {
   let wrapper;
   let store;
+  let localVue;
 
   beforeEach(function () {
     store = creeStore();
+    localVue = createLocalVue();
+    localVue.prototype.journal = { enregistre () {} };
     wrapper = shallowMount(BoutonAide, {
-      store
+      store,
+      localVue
     });
   });
 
@@ -24,5 +29,13 @@ describe("Le bouton d'aide", function () {
   it("le bouton est désactivé si l'aide est activé", function () {
     store.commit('activeAide');
     expect(wrapper.attributes('disabled')).to.equal('disabled');
+  });
+
+  it("rapporte l'activation de l'aide au journal", function (done) {
+    localVue.prototype.journal.enregistre = (evenement) => {
+      expect(evenement).to.be.a(EvenementActivationAide);
+      done();
+    };
+    wrapper.trigger('click');
   });
 });
