3a780f668dba02159d16e791b1a303fa7340a1d1
Stéphane Hanser <shanser@captive.fr>
2019-10-11 11:18:21 +0200

    Introduis l'événement Ouverture Zone pour la situation Sécurité
    Co-authored-by: François de Metz <francois.de_metz@beta.gouv.fr>

---
+++ tests/situations/securite/modeles/evenement_ouverture_zone.js
@@ -0,0 +1,12 @@
+import EvenementOuvertureZone from 'securite/modeles/evenement_ouverture_zone';
+
+describe("l'événement de l'ouverture de la zone", function () {
+  it('retourne son nom', function () {
+    expect(new EvenementOuvertureZone().nom()).to.eql('ouvertureZone');
+  });
+
+  it('retourne ses donnees', function () {
+    const donnees = { zone: 'La zone' };
+    expect(new EvenementOuvertureZone(donnees).donnees()).to.eql(donnees);
+  });
+});
