232dfd38425f95f0f965018f38909c8171d80927
François de Metz <francois.de_metz@beta.gouv.fr>
2019-10-15 14:13:53 +0200

    Affiche un écran spécifique une fois l'entrainement terminé.

--- src/situations/commun/vues/go.js
+++ src/situations/commun/vues/go.js
@@ -1,19 +1,33 @@
-import { ENTRAINEMENT_DEMARRE, DEMARRE } from 'commun/modeles/situation';
+import { ENTRAINEMENT_DEMARRE, ENTRAINEMENT_FINI, DEMARRE } from 'commun/modeles/situation';
 import VueActionOverlay from 'commun/vues/action_overlay';
 
 import go from 'commun/assets/go.svg';
 import { traduction } from 'commun/infra/internationalisation';
 
+function messageVueGo (situation) {
+  if (situation.etat() === ENTRAINEMENT_FINI) {
+    return 'situation.entrainement_fini';
+  }
+  return situation.entrainementDisponible() ? 'situation.entrainement_go' : 'situation.go';
+}
+
 export default class VueGo extends VueActionOverlay {
   constructor (situation) {
-    const message = situation.entrainementDisponible() ? 'situation.entrainement_go' : 'situation.go';
+    const message = messageVueGo(situation);
     super(go, traduction(message), 'bouton-go', 'bouton-centre-visible', 'hors-actions');
 
     this.situation = situation;
   }
 
   click () {
-    const prochainEtat = this.situation.entrainementDisponible() ? ENTRAINEMENT_DEMARRE : DEMARRE;
-    this.situation.modifieEtat(prochainEtat);
+    this.situation.modifieEtat(this.prochainEtat());
+  }
+
+  prochainEtat () {
+    if (this.situation.etat() === ENTRAINEMENT_FINI ||
+        !this.situation.entrainementDisponible()) {
+      return DEMARRE;
+    }
+    return ENTRAINEMENT_DEMARRE;
   }
 }
--- tests/situations/commun/vues/go.js
+++ tests/situations/commun/vues/go.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 import VueGo from 'commun/vues/go';
-import Situation, { ENTRAINEMENT_DEMARRE, DEMARRE } from 'commun/modeles/situation';
+import Situation, { ENTRAINEMENT_DEMARRE, ENTRAINEMENT_FINI, DEMARRE } from 'commun/modeles/situation';
 import { traduction } from 'commun/infra/internationalisation';
 
 describe('vue Go', function () {
@@ -29,6 +29,15 @@ describe('vue Go', function () {
     expect($('#pointInsertion .message').text()).to.eql(traduction('situation.entrainement_go'));
   });
 
+  it('affiche des informations différentes lorsque le mode entrainement est disponible mais que celui ci est terminé', () => {
+    situation = new Situation(true);
+    situation.modifieEtat(ENTRAINEMENT_FINI);
+    vue = new VueGo(situation);
+    vue.affiche('#pointInsertion', $);
+    expect($('#pointInsertion .bouton-go').length).to.eql(1);
+    expect($('#pointInsertion .message').text()).to.eql(traduction('situation.entrainement_fini'));
+  });
+
   it("au click, change l'état à DEMARRE", () => {
     vue.affiche('#pointInsertion', $);
     vue.click();
@@ -41,5 +50,14 @@ describe('vue Go', function () {
     vue.affiche('#pointInsertion', $);
     vue.click();
     expect(situation.etat()).to.eql(ENTRAINEMENT_DEMARRE);
+  });
+
+  it("au click, change l'état à DEMARRE lorsque le mode entrainement est fini", () => {
+    situation = new Situation(true);
+    situation.modifieEtat(ENTRAINEMENT_FINI);
+    vue = new VueGo(situation);
+    vue.affiche('#pointInsertion', $);
+    vue.click();
+    expect(situation.etat()).to.eql(DEMARRE);
   });
 });
