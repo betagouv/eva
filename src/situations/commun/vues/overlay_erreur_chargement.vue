75a823ffcb7dd462ebae387804cb95353e9548bf
François de Metz <francois.de_metz@beta.gouv.fr>
2019-10-28 18:30:38 +0100

    Refactor le style des overlay d'actions.

--- src/situations/commun/vues/overlay_erreur_chargement.vue
+++ src/situations/commun/vues/overlay_erreur_chargement.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="overlay opaque">
+  <div class="overlay opaque overlay-action">
     <div class="bouton-centre-invisible">
       <a class="bouton-erreur-chargement" @click="reload">
         <img :src="recharge">
       </a>
     </div>
-    <div class="message">{{ $traduction('situation.erreur_chargement') }}</div>
+    <div class="overlay-action-message">{{ $traduction('situation.erreur_chargement') }}</div>
   </div>
 </template>
 
 <script>
+import 'commun/styles/overlay.scss';
 import 'commun/styles/action_overlay.scss';
+import 'commun/styles/erreur_chargement.scss';
 
 import recharge from 'commun/assets/recharge.svg';
 
