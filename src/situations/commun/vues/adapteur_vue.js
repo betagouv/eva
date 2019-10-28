2510ace722be9a61a87155077fc14c50b0651ecf
François de Metz <francois.de_metz@beta.gouv.fr>
2019-10-28 18:11:57 +0100

    Utilise les version vuejs de chargement et erreur_chargement dans le cadre.

---
+++ src/situations/commun/vues/adapteur_vue.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+
+import { traduction } from 'commun/infra/internationalisation';
+
+export function creeAdapteur (component) {
+  return class AdaptateurVue {
+    constructor (situation, depotRessources) {
+      Vue.prototype.$depotRessources = depotRessources;
+      Vue.prototype.$traduction = traduction;
+    }
+
+    affiche (pointInsertion, $) {
+      const div = document.createElement('div');
+      $(pointInsertion).append(div);
+      this.vm = new Vue({
+        render: createEle => createEle(component)
+      }).$mount(div);
+    }
+
+    cache () {
+      this.vm.$el.remove();
+      this.vm.$destroy();
+    }
+  };
+}
---
+++ src/situations/commun/vues/overlay_chargement.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="overlay opaque">
+    <div class="bouton-centre-invisible">
+      <a class="bouton-chargement">
+        <img :src="chargement">
+      </a>
+    </div>
+    <div class="message">{{ $traduction('situation.chargement') }}</div>
+  </div>
+</template>
+
+<script>
+import 'commun/styles/action_overlay.scss';
+
+import chargement from 'commun/assets/chargement.svg';
+
+export default {
+  data () {
+    return {
+      chargement
+    };
+  }
+};
+</script>
