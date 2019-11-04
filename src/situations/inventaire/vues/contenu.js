beef4dec03a2fb0326ac3e304b00f9dbb26ea2d6
François de Metz <francois.de_metz@beta.gouv.fr>
2019-11-04 11:19:22 +0100

    Refactor la vue contenu pour n'insérer le contenu seulement à l'affichage.

--- src/situations/inventaire/vues/contenu.js
+++ src/situations/inventaire/vues/contenu.js
@@ -15,13 +15,12 @@ export default class VueContenu {
       setTimeout(() => {
         this.calque.classList.add('invisible');
         this.element.classList.add('invisible');
+        this.element.remove();
+        this.calque.remove();
       }, delaiFermeture);
       event.stopPropagation();
     });
-    this.element = document.createElement('img');
-    this.element.classList.add('contenu', 'fermer', 'invisible');
-    pointInsertion.appendChild(this.element);
-    pointInsertion.appendChild(this.calque);
+    this.pointInsertion = pointInsertion;
   }
 
   position (position, dimensionFermee, dimensionOuvert) {
@@ -29,11 +28,16 @@ export default class VueContenu {
   }
 
   affiche (contenant) {
+    this.element = document.createElement('img');
     this.element.src = contenant.imageOuvert;
+    this.element.classList.add('contenu', 'fermer', 'invisible');
     this.element.style.top = this.position(contenant.posY - contenant.hauteur, contenant.hauteur, contenant.dimensionsOuvert.hauteur) + '%';
     this.element.style.left = this.position(contenant.posX, contenant.largeur, contenant.dimensionsOuvert.largeur) + '%';
     this.element.style.height = contenant.dimensionsOuvert.hauteur + '%';
     this.element.style.width = contenant.dimensionsOuvert.largeur + '%';
+
+    this.pointInsertion.appendChild(this.element);
+    this.pointInsertion.appendChild(this.calque);
 
     this.calque.classList.remove('invisible');
     this.element.classList.remove('invisible');
