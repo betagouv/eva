40b4b4f4425e899e9d9b2159d7fe860cfdcb9743
François de Metz <francois.de_metz@beta.gouv.fr>
2019-11-05 19:12:21 +0100

    Active l'aide au click sur le bouton.

--- tests/situations/commun/vues/aide.js
+++ tests/situations/commun/vues/aide.js
@@ -1,16 +1,25 @@
 import $ from 'jquery';
+import Situation from 'commun/modeles/situation';
 import VueAide from 'commun/vues/aide';
 
 describe('vue bouton aide', function () {
   let vue;
+  let situation;
 
   beforeEach(function () {
     $('body').append('<div id="point-insertion"></div>');
-    vue = new VueAide();
+    situation = new Situation();
+    vue = new VueAide(situation);
   });
 
   it("sait s'insérer dans une page web", function () {
     vue.affiche('#point-insertion', $);
     expect($('#point-insertion .bouton-aide').length).to.eql(1);
+  });
+
+  it("au click, active l'aide", function () {
+    vue.affiche('#point-insertion', $);
+    $('#point-insertion .bouton-aide').click();
+    expect(situation.aideActivee).to.be(true);
   });
 });
