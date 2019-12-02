d7a52e94bca723b916beeb55571b2d1965418f54
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2019-12-02 18:05:20 +0100

    Nouvel écran de fin en deux parties pour aider à comprendre que c'est fini mais qu'il en reste
    fix #606

--- src/situations/accueil/vues/fin.vue
+++ src/situations/accueil/vues/fin.vue
@@ -1,49 +1,78 @@
 <template>
   <div class="overlay modale">
-    <div class="modale-interieur">
-      <h2>{{ $traduction('accueil.fin.titre') }}</h2>
+    <div
+      v-if="ecran == 'bravo'"
+      class="modale-interieur bravo">
+      <img class="avatar-fin" :src="avatarFin"/>
+      <div>
+        <h2>{{ $traduction('accueil.fin.bravo.titre') }}</h2>
+        <div v-if="this.competencesFortes.length != 0">
+          <div class="contenu">
+            <p class="message-fin">
+            {{ $traduction('accueil.fin.bravo.message') }}
+            </p>
+          </div>
+          <button
+            class="bouton-arrondi"
+            @click="suivant"
+            >{{ $traduction('accueil.fin.bravo.bouton') }}</button>
+        </div>
+      </div>
+    </div>
+    <div
+      v-else
+      class="modale-interieur resultat">
+      <h2>{{ $traduction('accueil.fin.resultat.titre') }}</h2>
+      <div
+        class="message-competences-fortes"
+        v-html="$traduction('accueil.fin.resultat.competences')">
+      </div>
       <div class="contenu">
-        <p class="message-fin">
-        {{ $traduction('accueil.fin.message') }}
-        <span
-          v-if="this.competencesFortes.length != 0"
-          class="message-competences-fortes"
-          >{{ $traduction('accueil.fin.competences') }}</span>
-        </p>
         <div
           v-for="competence in competencesFortes"
           :key="competence.id"
           class="competences-fortes"
           >
           <img :src="competence.picto" />
-          <div>
-            <p class="competences-fortes-nom">{{ competence.nom }}</p>
-            <p class="competences-fortes-description">{{ competence.description }}
-              <a class="competences-fortes-lien" :href="lienSiteVitrine(competence.id)">{{ $traduction('accueil.fin.en-savoir-plus') }}</a>
-            </p>
-          </div>
+          <p class="competences-fortes-nom">{{ competence.nom }}</p>
+          <p class="competences-fortes-description">{{ competence.description }}</p>
         </div>
       </div>
-      <button
-        class="bouton-arrondi"
-        @click="deconnecte"
-      >{{ $traduction('accueil.fin.bouton') }}</button>
+      <div class="contenu">
+        <div
+          v-for="competence in competencesFortes"
+          :key="competence.id"
+          >
+            <a target="_blank" class="bouton-arrondi bouton-arrondi--petit" :href="lienSiteVitrine(competence.id)">{{ $traduction('accueil.fin.resultat.en-savoir-plus') }}</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex';
-import 'commun/styles/fin.scss';
+import 'accueil/styles/fin.scss';
 
 export default {
   computed: mapState(['competencesFortes']),
+
+  data () {
+    return {
+      ecran: 'bravo',
+      avatarFin: this.$depotRessources.avatarFin().src
+    };
+  },
 
   methods: {
     ...mapActions(['deconnecte']),
 
     lienSiteVitrine (competence) {
       return `https://eva.beta.gouv.fr/competences/${competence}`;
+    },
+
+    suivant () {
+      this.ecran = 'resultat';
     }
   }
 };
