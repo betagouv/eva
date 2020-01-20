b9a54e5eb890d864f227ce8fd454ae2bda7d3ea9
François de Metz <francois.de_metz@beta.gouv.fr>
2020-01-20 15:52:30 +0100

    Réutilise les transitions css pour l'illustration et le cercle.

--- src/situations/prevention/vues/acte.vue
+++ src/situations/prevention/vues/acte.vue
@@ -68,8 +68,7 @@
       v-if="zoneActive"
       :xlink:href="fondSituation"
       :style="[ { 'transform-origin': zoneActiveTransformOrigin },
-                   zoneEvaluee ? { transform: transformZone(2) } : '',
-                   zonePrevention ? { transform: transformZone(1) } : '' ]"
+                   zoneEvaluee || zonePrevention ? { transform: transformZone(2) } : '' ]"
       clip-path="url(#cercle-illustration-clip)"
       height="100%"
       width="100%"
@@ -77,12 +76,15 @@
     />
     <circle
       v-if="zoneActive"
-      :cx="`${cercleBlanc.cx}%`"
-      :cy="`${cercleBlanc.cy}%`"
-      :r="`${cercleBlanc.r}%`"
-      :class="{ 'zone-agrandi': zoneEvaluee || zonePrevention,
-                'zone-clickable': zoneSurvolee }"
-      class="zone"
+      :cx="`${zoneActive.x}%`"
+      :cy="`${zoneActive.y}%`"
+      :r="`${zoneActive.r}%`"
+      :class="{ 'zone-agrandi': zoneEvaluee,
+                'zone-clickable': zoneSurvolee,
+                'zone-reduite':  zonePrevention }"
+      :style="[{ 'transform-origin': zoneActiveTransformOrigin },
+                 zoneEvaluee || zonePrevention ? { transform: transformZone(3) } : '' ]"
+      class="zone transition-transform"
       @mouseout="deSurvoleZone"
       @click="evalueZone(zoneActive)"
     />
@@ -99,7 +101,6 @@ import ActionEvaluation from './action_evaluation';
 import ActionPrevention from './action_prevention';
 
 const POSITION_CERCLE_EVALUATION = { x: 50, y: 40 };
-const POSITION_CERCLE_PREVENTION = { x: 50, y: 35 };
 
 const RECTANGLE_EVALUATION = { x: 32, y: 73, width: 35, height: 22, rx: 64 };
 const RECTANGLE_PREVENTION = { x: 21.5, y: 45, width: 57, height: 44.85, rx: 30 };
@@ -116,8 +117,7 @@ export default {
       zoneSurvolee: null,
       zoneEvaluee: null,
       zonePrevention: null,
-      rectActions: {},
-      cercleBlanc: {}
+      rectActions: {}
     };
   },
 
@@ -131,19 +131,16 @@ export default {
     },
     zoneActiveTransformOrigin () {
       return `${this.zoneActive.x}% ${this.zoneActive.y}%`;
-    },
-    positionCercle () {
-      if (this.zonePrevention) {
-        return POSITION_CERCLE_PREVENTION;
-      }
-      return POSITION_CERCLE_EVALUATION;
     }
   },
 
   methods: {
     transformZone (scale) {
-      const scaleInstruction = scale === 1 ? '' : `scale(${scale})`;
-      return `${scaleInstruction} translate(${(this.positionCercle.x - this.zoneActive.x) / scale}%, ${(this.positionCercle.y - this.zoneActive.y) / scale}%)`;
+      let transform = '';
+      if (this.zonePrevention) {
+        transform = `scale(0.5) translate(0, ${-5 / scale * 2}%)`;
+      }
+      return `scale(${scale}) translate(${(POSITION_CERCLE_EVALUATION.x - this.zoneActive.x) / scale}%, ${(POSITION_CERCLE_EVALUATION.y - this.zoneActive.y) / scale}%) ${transform}`;
     },
 
     survoleZone (zone) {
@@ -178,16 +175,10 @@ export default {
     zoneSurvolee (zone) {
       if (!zone) return;
       this.rectActions = { ...RECTANGLE_EVALUATION };
-      this.cercleBlanc = { cx: this.zoneActive.x, cy: this.zoneActive.y, r: this.zoneActive.r };
-    },
-    zoneEvaluee (zone) {
-      if (!zone) return;
-      this.anime(this.cercleBlanc, { r: this.zoneActive.r * 3, cx: this.positionCercle.x, cy: this.positionCercle.y });
     },
     zonePrevention (zone) {
       if (!zone) return;
       this.anime(this.rectActions, RECTANGLE_PREVENTION);
-      this.anime(this.cercleBlanc, { r: this.zoneActive.r * 1.5, cx: this.positionCercle.x, cy: this.positionCercle.y });
     }
   }
 };
