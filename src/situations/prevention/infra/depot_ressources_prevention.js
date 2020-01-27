ad8d4719a2b895c2bda929f2c851830d78c04bdf
François de Metz <francois.de_metz@beta.gouv.fr>
2020-01-27 10:43:47 +0100

    Déplace les panneaux dans des fichiers.

--- src/situations/prevention/infra/depot_ressources_prevention.js
+++ src/situations/prevention/infra/depot_ressources_prevention.js
@@ -4,15 +4,30 @@ import sonConsigneDemarrage from 'commun/assets/consigne_blanche.wav';
 import fondSituation from '../assets/fond-situation.jpg';
 import risque21 from '../assets/risque2-1.png';
 import risque22 from '../assets/risque2-2.png';
+import ok from '../assets/ok.svg';
+import danger from '../assets/danger.svg';
+import urgence from '../assets/urgence.svg';
 
 export default class DepotRessourcesPrevention extends DepotRessourcesCommunes {
   constructor (chargeurs) {
     super(chargeurs, sonConsigneDemarrage);
-    this.charge([fondSituation, risque21, risque22]);
+    this.charge([fondSituation, ok, danger, urgence, risque21, risque22]);
   }
 
   fondSituation () {
     return this.ressource(fondSituation);
+  }
+
+  ok () {
+    return this.ressource(ok);
+  }
+
+  danger () {
+    return this.ressource(danger);
+  }
+
+  urgence () {
+    return this.ressource(urgence);
   }
 
   risque21 () {
