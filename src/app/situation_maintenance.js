ccc92f9842029c3e272527df33affa9d496e3454
Clément MORISSET <cmorisset@captive.fr>
2020-01-28 10:53:21 +0100

    Ajoute test sur l'acte de la situation Maintenance
    Co-authored-by: François de Metz <francois.de_metz@beta.gouv.fr>

--- src/app/situation_maintenance.js
+++ src/app/situation_maintenance.js
@@ -3,7 +3,7 @@ import 'commun/infra/report_erreurs';
 import { afficheSituation } from 'commun/vues/affiche_situation';
 
 import DepotRessourcesMaintenance from 'maintenance/infra/depot_ressources_maintenance';
-import Situation from 'maintenance/modeles/situation';
+import Situation from 'commun/modeles/situation';
 import VueSituation from 'maintenance/vues/situation';
 
 const situation = new Situation();
