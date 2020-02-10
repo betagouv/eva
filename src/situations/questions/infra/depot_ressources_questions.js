2549e99ce0ba411bfdaf7693865aa754e1d53b4d
François de Metz <francois.de_metz@beta.gouv.fr>
2020-02-10 13:51:49 +0100

    Charge le questionnaire de l'entrainement dans le dépot ressources.

--- src/situations/questions/infra/depot_ressources_questions.js
+++ src/situations/questions/infra/depot_ressources_questions.js
@@ -22,19 +22,41 @@ export default class DepotRessourcesQuestions extends DepotRessourcesCommunes {
     }
   }
 
+  questionsEntrainement () {
+    if (this.questionnaireEntrainementUrl) {
+      return this.ressource(this.questionnaireEntrainementUrl);
+    }
+    return [];
+  }
+
   chargement () {
     return super.chargement()
-      .then(() => {
-        const evaluationJSON = this.ressource(this.evaluationUrl);
-        const situation = evaluationJSON.situations.find(situation => situation.nom_technique === this.nomSituation);
-        if (situation && situation.questionnaire_id) {
-          this.questionnaireUrl = `${this.urlServeur}/api/questionnaires/${situation.questionnaire_id}.json`;
-          return this.promesseRessource(this.questionnaireUrl);
-        }
-      }).then(() => {
-        const images = this.questions().map(question => question.illustration).filter(illustration => illustration);
-        this.charge(images);
-        return super.chargement();
-      });
+      .then(() => this.chargeQuestionnaires())
+      .then(() => this.chargeIllustrations());
+  }
+
+  chargeQuestionnaires () {
+    const evaluationJSON = this.ressource(this.evaluationUrl);
+    const situation = evaluationJSON.situations.find(situation => situation.nom_technique === this.nomSituation);
+    if (!situation) return;
+
+    const promesses = [];
+    if (situation.questionnaire_id) {
+      this.questionnaireUrl = `${this.urlServeur}/api/questionnaires/${situation.questionnaire_id}.json`;
+      promesses.push(this.promesseRessource(this.questionnaireUrl));
+    }
+    if (situation.questionnaire_entrainement_id) {
+      this.questionnaireEntrainementUrl = `${this.urlServeur}/api/questionnaires/${situation.questionnaire_entrainement_id}.json`;
+      promesses.push(this.promesseRessource(this.questionnaireEntrainementUrl));
+    }
+    return Promise.all(promesses);
+  }
+
+  chargeIllustrations () {
+    const images = [...this.questions(), ...this.questionsEntrainement()]
+      .map(question => question.illustration)
+      .filter(illustration => illustration);
+    this.charge(images);
+    return super.chargement();
   }
 }
