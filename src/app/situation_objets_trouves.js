fcfbd8f6f4075b065ab49da9f1ab69850ac8adfb
Marine Dominé <marine@captive.fr>
2020-02-24 16:59:39 +0100

    Créé le squelette de la situation

---
+++ src/app/situation_objets_trouves.js
@@ -0,0 +1,12 @@
+import 'commun/infra/report_erreurs';
+
+import { afficheSituation } from 'commun/vues/affiche_situation';
+
+import DepotRessourcesObjetsTrouves from 'objets_trouves/infra/depot_ressources_objets_trouves';
+import Situation from 'commun/modeles/situation';
+import VueSituation from 'objets_trouves/vues/situation';
+
+const situation = new Situation();
+
+const depotRessources = new DepotRessourcesObjetsTrouves();
+afficheSituation('objets_trouves', situation, VueSituation, depotRessources);
