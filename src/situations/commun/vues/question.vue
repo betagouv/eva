f93ea6b887030e4284f4019954ac5935827051ef
François de Metz <francois.de_metz@beta.gouv.fr>
2020-03-04 17:47:45 +0100

    Déplace l'affichage de la progression dans l'acte de questions.
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- src/situations/commun/vues/question.vue
+++ src/situations/commun/vues/question.vue
@@ -5,26 +5,18 @@
       class="question-illustration"
     />
     <div class="question-barre">
-      <div class="question-progression">
-        {{ numeroQuestionCourante }}/{{ nombreQuestions }}
-      </div>
       <slot />
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import 'commun/styles/question.scss';
-
 export default {
   props: {
     question: {
       type: Object,
       required: true
     }
-  },
-
-  computed: mapGetters(['nombreQuestions', 'numeroQuestionCourante'])
+  }
 };
 </script>
