aa70eedaa5993ba72fcafbd8865fdcaff12dd2d5
François de Metz <francois.de_metz@beta.gouv.fr>
2020-03-04 16:33:07 +0100

    Rajoute un composant ActeObjetsTrouves.
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

---
+++ src/situations/objets_trouves/vues/acte.vue
@@ -0,0 +1,11 @@
+<template>
+  <app-accueil />
+</template>
+
+<script>
+import AppAccueil from './accueil';
+
+export default {
+  components: { AppAccueil }
+};
+</script>
--- src/situations/objets_trouves/vues/situation.js
+++ src/situations/objets_trouves/vues/situation.js
@@ -1,20 +1,10 @@
 import AdaptateurCommunVueSituation from 'commun/vues/adaptateur_situation';
 import Vue from 'vue';
 import { creeStore } from '../modeles/store';
-import vueAccueil from './accueil';
+import ActeObjetsTrouves from './acte';
 
 export default class AdaptateurVueSituation extends AdaptateurCommunVueSituation {
   constructor (situation, journal, depotRessources) {
-    super(journal, situation, depotRessources, creeStore);
-  }
-
-  affiche (pointInsertion, $) {
-    const div = document.createElement('div');
-    $(pointInsertion).append(div);
-    new Vue({
-      render: function (createElement) {
-        return createElement(vueAccueil);
-      }
-    }).$mount(div);
+    super(situation, journal, depotRessources, creeStore, ActeObjetsTrouves, {}, {});
   }
 }
