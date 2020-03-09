4a39abc0a94bf0faf27c6331823797cd567433b6
Étienne Charignon <etienne.charignon@gmail.com>
2020-03-09 10:49:46 +0000

    Utilise l'heure courante et la met à jour.

--- src/situations/objets_trouves/vues/heure.vue
+++ src/situations/objets_trouves/vues/heure.vue
@@ -1,3 +1,31 @@
 <template>
-  <span class="heure">17:49</span>
+  <span class="heure">{{ heure }}:{{ minute }}</span>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      heure: '00',
+      minute: '00'
+    };
+  },
+
+  mounted () {
+    this.metsAJour();
+    this.interval = setInterval(this.metsAJour, 60 * 1000);
+  },
+
+  beforeDestroy () {
+    clearInterval(this.interval);
+  },
+
+  methods: {
+    metsAJour () {
+      const date = new Date();
+      this.heure = date.getHours().toString().padStart(2, 0);
+      this.minute = date.getMinutes().toString().padStart(2, 0);
+    }
+  }
+};
+</script>
