711ea193b7c0bdb95fc141c30e237796b3b9d9b6
François de Metz <francois.de_metz@beta.gouv.fr>
2020-03-16 15:46:22 +0100

    Ajoute le texte de l'app photo.

--- src/situations/objets_trouves/data/apps.js
+++ src/situations/objets_trouves/data/apps.js
@@ -1,3 +1,33 @@
+import AppPhoto from '../assets/app-photo.png';
+
+const questionPhoto = {
+  id: 'photo',
+  illustration: AppPhoto,
+  intitule: "Qu'indique le badge de Denis ?",
+  choix: [
+    {
+      id: '1',
+      intitule: 'Il travaille dans le zoo'
+    },
+    {
+      id: '2',
+      intitule: 'Il déjeune dans le zoo'
+    },
+    {
+      id: '3',
+      intitule: "Il doit retrouver quelqu'un dans le zoo"
+    },
+    {
+      id: '4',
+      intitule: 'Il doit se déplacer au zoo'
+    },
+    {
+      id: '5',
+      intitule: 'Il est perdu dans le zoo'
+    }
+  ]
+};
+
 const question1 = {
   id: 1,
   choix: [
@@ -25,7 +55,7 @@ const configurationEntrainement = {
 };
 const configurationNormale = {
   apps: {
-    photos: question1,
+    photos: questionPhoto,
     repondeur: question1,
     rappels: question1,
     agenda: question1,
