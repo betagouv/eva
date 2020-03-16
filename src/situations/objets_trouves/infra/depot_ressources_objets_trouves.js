ac2f00ad835386f82d0f5dea438ded8b00475774
François de Metz <francois.de_metz@beta.gouv.fr>
2020-03-16 15:46:22 +0100

    Ajoute le texte de l'app photo.

--- src/situations/objets_trouves/infra/depot_ressources_objets_trouves.js
+++ src/situations/objets_trouves/infra/depot_ressources_objets_trouves.js
@@ -3,11 +3,12 @@ import sonConsigneDemarrage from 'maintenance/assets/consigne_demarrage.wav';
 import sonConsigneTransition from 'maintenance/assets/consigne_transition.wav';
 
 import fondSituation from '../assets/fond-situation.png';
+import appPhoto from '../assets/app-photo.png';
 
 export default class DepotRessourcesObjetsTrouves extends DepotRessourcesCommunes {
   constructor (chargeurs) {
     super(chargeurs, sonConsigneDemarrage, sonConsigneTransition);
-    this.charge([fondSituation]);
+    this.charge([fondSituation, appPhoto]);
   }
 
   fondSituation () {
