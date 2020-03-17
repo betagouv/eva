10312fd41ca861d24c4b9d6ebc02ef65c812e69c
François de Metz <francois.de_metz@beta.gouv.fr>
2020-03-17 14:30:02 +0100

    Ajoute la vrai question de l'entrainement.

--- src/situations/objets_trouves/data/apps.js
+++ src/situations/objets_trouves/data/apps.js
@@ -1,6 +1,31 @@
+import AppAgendaEntrainement from '../assets/app-agenda-entrainement.png';
 import AppPhoto from '../assets/app-photo.png';
 import AppAgenda from '../assets/app-agenda.png';
 import choix1 from 'maintenance/assets/consigne_demarrage.wav';
+
+const questionAgendaEntrainement = {
+  id: 'agenda-entrainement',
+  illustration: AppAgendaEntrainement,
+  intitule: 'Voici le téléphone de Sophie, que faisait Sophie ce matin ?',
+  choix: [
+    {
+      id: '1',
+      intitule: 'Elle distribuait le courrier'
+    },
+    {
+      id: '2',
+      intitule: "Elle s'occupait du zoo"
+    },
+    {
+      id: '3',
+      intitule: 'Elle réalisait un travail administratif'
+    },
+    {
+      id: '4',
+      intitule: 'Elle distribuait les colis'
+    }
+  ]
+};
 
 const questionPhoto = {
   id: 'photo',
@@ -84,7 +109,7 @@ const question2 = {
 
 const configurationEntrainement = {
   apps: {
-    photos: question1
+    agenda: questionAgendaEntrainement
   }
 };
 const configurationNormale = {
