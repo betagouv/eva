53c61a2d3c588ab35cc167e63d111bee6ea898ee
Clément MORISSET <cmorisset@captive.fr>
2020-03-30 16:51:28 +0200

    Ajoute les icônes des applications dans les questions
    Co-authored-by: Marine Dominé <marine@captive.fr>

--- src/situations/commun/vues/question.vue
+++ src/situations/commun/vues/question.vue
@@ -4,6 +4,19 @@
       :src="question.illustration"
       class="question-illustration"
     />
+
+    <div
+      class="icone-conteneur"
+      v-if="appActive"
+    >
+      <div
+        class="icone"
+        :class="icone()"
+        :style="{ 'background-image': `url(${appAttributes().icone})` }"
+      ></div>
+      <span class="label">{{ $traduction(`objets_trouves.accueil.${appActive}`) }}</span>
+    </div>
+
     <div class="question-barre">
       <slot />
     </div>
@@ -11,11 +24,25 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import 'objets_trouves/styles/acte.scss';
+
 export default {
   props: {
     question: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    ...mapState(['appActive', 'apps'])
+  },
+  methods: {
+    appAttributes () {
+      return Object.assign({}, this.apps)[this.appActive][0];
+    },
+    icone () {
+      return `icone--${this.appActive}`;
     }
   }
 };
--- tests/situations/commun/vues/question.js
+++ tests/situations/commun/vues/question.js
@@ -1,18 +1,45 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import VueQuestion from 'commun/vues/question';
+import { creeStore } from 'objets_trouves/modeles/store';
+import { traduction } from 'commun/infra/internationalisation';
 
 describe('La vue de la question', function () {
   let question;
+  let store;
+  let wrapper;
+  let localVue;
 
   beforeEach(function () {
+    localVue = createLocalVue();
+    localVue.prototype.$traduction = traduction;
     question = { choix: [] };
+    store = creeStore();
+    question.illustration = 'palette';
+    wrapper = shallowMount(VueQuestion, {
+      propsData: {
+        question
+      },
+      store,
+      localVue
+    });
   });
 
   it("affiche l'image", function () {
-    question.illustration = 'palette';
-    const vue = shallowMount(VueQuestion, { propsData: { question } });
+    expect(wrapper.contains('.question-illustration')).to.be(true);
+    expect(wrapper.find('.question-illustration').attributes('src')).to.equal('palette');
+  });
 
-    expect(vue.contains('.question-illustration')).to.be(true);
-    expect(vue.find('.question-illustration').attributes('src')).to.equal('palette');
+  it("n'affiche pas d'icône si il n'y a pas d'apps active", function () {
+    expect(wrapper.contains('.icone-conteneur')).to.be(false);
+  });
+
+  it('affiche une icône si il y a une app active', function () {
+    store.commit('configureActe', { apps: { agenda: { 0: { icone: 'icone-agenda' } } } });
+    store.commit('afficheApp', 'agenda');
+
+    expect(wrapper.contains('.icone-conteneur')).to.be(true);
+    const icone = wrapper.find('.icone');
+    expect(icone.classes('icone--agenda')).to.be(true);
+    expect(icone.attributes('style')).to.equal('background-image: url(icone-agenda);');
   });
 });
