ead9b5272ea7abde77190e1900f4bc70cc2e3067
Étienne Charignon <etienne.charignon@gmail.com>
2020-03-30 14:53:35 +0000

    Ajout les consignes audio de la situation objets trouvés
    Le texte des consignes a aussi été mis en conformité avec l'audio
    conformément au document des consignes simplifiés de l'issue #784.

--- src/situations/objets_trouves/infra/depot_ressources_objets_trouves.js
+++ src/situations/objets_trouves/infra/depot_ressources_objets_trouves.js
@@ -1,12 +1,13 @@
 import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
-import sonConsigneBlanche from 'commun/assets/consigne_blanche.wav';
+import sonConsigne from 'objets_trouves/assets/consigne_demarrage.wav';
+import sonConsigneTransition from 'objets_trouves/assets/consigne_transition.wav';
 
 import fondSituation from '../assets/accueil.png';
 import appPhoto from '../assets/app-photo.png';
 
 export default class DepotRessourcesObjetsTrouves extends DepotRessourcesCommunes {
   constructor (chargeurs) {
-    super(chargeurs, sonConsigneBlanche, sonConsigneBlanche);
+    super(chargeurs, sonConsigne, sonConsigneTransition);
     this.charge([fondSituation, appPhoto]);
   }
 
