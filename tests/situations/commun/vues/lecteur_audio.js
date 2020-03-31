63026d09a237cdb0dc9681166b675a0dfb718015
Étienne Charignon <etienne.charignon@gmail.com>
2020-03-31 19:36:48 +0000

    Objets trouvés : joue les sons de l'agenda au moment de la selection

--- tests/situations/commun/vues/lecteur_audio.js
+++ tests/situations/commun/vues/lecteur_audio.js
@@ -3,7 +3,7 @@ import LecteurAudio from 'commun/vues/lecteur_audio';
 
 describe('Le lecteur audio', function () {
   it('rend une balise audio', function () {
-    const wrapper = shallowMount(LecteurAudio, { propsData: { src: '1' } });
+    const wrapper = shallowMount(LecteurAudio, { propsData: { src: '1', joueSon: false } });
     expect(wrapper.findAll('audio').length).to.equal(1);
   });
 });
