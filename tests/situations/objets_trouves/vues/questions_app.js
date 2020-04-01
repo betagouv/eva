4f54e02ce1ea36c2a54d78f380da66e583a952c4
Stéphane Hanser <shanser@captive.fr>
2020-04-01 13:13:16 +0200

    Envoie les réponses dans la situation objets trouvés

--- tests/situations/objets_trouves/vues/questions_app.js
+++ tests/situations/objets_trouves/vues/questions_app.js
@@ -1,11 +1,20 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import QuestionsApp from 'objets_trouves/vues/questions_app';
+import EvenementReponse from 'questions/modeles/evenement_reponse';
 
 describe("Les questions d'une app", function () {
+  let localVue;
+
+  beforeEach(function () {
+    localVue = createLocalVue();
+    localVue.prototype.$journal = { enregistre () {} };
+  });
+
   it('affiche les questions les une après les autres', function () {
     const question1 = {};
     const question2 = {};
     const wrapper = shallowMount(QuestionsApp, {
+      localVue,
       propsData: {
         questions: [question1, question2]
       }
@@ -17,5 +26,17 @@ describe("Les questions d'une app", function () {
     wrapper.vm.reponseApp();
     expect(wrapper.emitted('finQuestions').length).to.eql(1);
     expect(wrapper.vm.question).to.eql(question2);
+  });
+
+  it('enregistre les réponses au journal', function (done) {
+    localVue.prototype.$journal.enregistre = (evenement) => {
+      expect(evenement).to.be.a(EvenementReponse);
+      expect(evenement.donnees()).to.eql({ question: 'mon-id', reponse: 'Ma super réponse' });
+      done();
+    };
+
+    const question = { id: 'mon-id' };
+    const wrapper = shallowMount(QuestionsApp, { localVue, propsData: { questions: [question] } });
+    wrapper.vm.reponseApp('Ma super réponse');
   });
 });
