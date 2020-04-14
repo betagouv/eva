947afafcfda226a8eb670a20282759fa568603d9
Étienne Charignon <etienne.charignon@gmail.com>
2020-04-14 14:54:06 +0000

    Objets trouvés : coupe la dépendance de qcm.vue vers objects trouvés

---
+++ src/situations/objets_trouves/vues/lecture-message.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="telephone-conteneur">
+    <bouton-lecture
+      :son-message="$depotRessources.messageAudio(question.id)"
+    />
+  </div>
+</template>
+
+<script>
+import BoutonLecture from 'objets_trouves/vues/bouton-lecture';
+
+export default {
+  components: { BoutonLecture },
+
+  props: {
+    question: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
