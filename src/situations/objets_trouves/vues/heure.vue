98117174c1bc8a544ea0203a5de1fd4035888a67
Étienne Charignon <etienne.charignon@gmail.com>
2020-04-22 15:15:10 +0000

    affiche une heure fixe : 13h00

--- src/situations/objets_trouves/vues/heure.vue
+++ src/situations/objets_trouves/vues/heure.vue
@@ -6,26 +6,9 @@
 export default {
   data () {
     return {
-      heure: '00',
+      heure: '13',
       minute: '00'
     };
-  },
-
-  mounted () {
-    this.metsAJour();
-    this.interval = setInterval(this.metsAJour, 60 * 1000);
-  },
-
-  beforeDestroy () {
-    clearInterval(this.interval);
-  },
-
-  methods: {
-    metsAJour () {
-      const date = new Date();
-      this.heure = date.getHours().toString().padStart(2, 0);
-      this.minute = date.getMinutes().toString().padStart(2, 0);
-    }
   }
 };
 </script>
