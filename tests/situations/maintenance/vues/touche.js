e20bd48b641ba17d084a064a3092b2972087a678
Étienne Charignon <etienne.charignon@gmail.com>
2021-05-12 13:44:02 +0000

    Ajoute les tests pour la vue touche

---
+++ tests/situations/maintenance/vues/touche.js
@@ -0,0 +1,47 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Touche from 'maintenance/vues/touche';
+
+describe('La vue touche de la Maintenance', function () {
+  let wrapper;
+  let localVue;
+
+  beforeEach(function () {
+    localVue = createLocalVue();
+  });
+
+  it('affiche un label à droite', function () {
+    wrapper = shallowMount(Touche, {
+      localVue, propsData: { label: 'texte' }
+    });
+    expect(wrapper.find('.label-droite').exists()).to.be(true);
+    expect(wrapper.find('.label-gauche').exists()).to.be(false);
+  });
+
+  it('affiche un label à gauche', function () {
+    wrapper = shallowMount(Touche, {
+      localVue, propsData: { labelGauche: 'texte' }
+    });
+    expect(wrapper.find('.label-droite').exists()).to.be(false);
+    expect(wrapper.find('.label-gauche').exists()).to.be(true);
+  });
+
+  it("n'affiche aucun label", function () {
+    wrapper = shallowMount(Touche, { localVue });
+    expect(wrapper.find('.label-droite').exists()).to.be(false);
+    expect(wrapper.find('.label-gauche').exists()).to.be(false);
+  });
+
+  it('fait tourner le svg', function () {
+    wrapper = shallowMount(Touche, {
+      localVue, propsData: { rotation: 90 }
+    });
+    expect(wrapper.find('svg').attributes('transform')).to.be('rotate(90)');
+  });
+
+  it('peut changer de couleur', function () {
+    wrapper = shallowMount(Touche, {
+      localVue, propsData: { couleur: 'verte' }
+    });
+    expect(wrapper.find('.touche.verte').exists()).to.be(true);
+  });
+});
