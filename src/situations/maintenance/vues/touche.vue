22eca8a3c8eaaaaa78e60a29326f0b996c763d46
Étienne Charignon <etienne.charignon@gmail.com>
2021-05-18 08:25:01 +0000

    touche.vue : applique la rotation aux elements du svg pour Safari
    Apparement Safari ne supporte pas la propriété `transform` directement
    sur l'élément svg

--- src/situations/maintenance/vues/touche.vue
+++ src/situations/maintenance/vues/touche.vue
@@ -9,10 +9,17 @@
       height="32"
       viewBox="0 0 28 28"
       fill="none"
-      :transform="transformation"
       xmlns="http://www.w3.org/2000/svg">
-      <rect x="1.5" y="2.14502" width="24.8325" height="23.58" rx="6.5" stroke="#1E416A" stroke-width="3"/>
-      <path d="M20.8736 13.9349H7.82715M7.82715 13.9349L13.4185 8.95117M7.82715 13.9349L13.4185 18.9187" stroke="#1E416A" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+      <rect
+      :transform="transformation"
+      width="24.8325" height="23.58"
+      x="1.5" y="2.14502"
+      rx="6.5"
+      stroke="#1E416A" stroke-width="3"/>
+      <path
+      :transform="transformation"
+      d="M20.8736 13.9349H7.82715M7.82715 13.9349L13.4185 8.95117M7.82715 13.9349L13.4185 18.9187"
+      stroke="#1E416A" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
     <span v-if="label" class="label-droite" >{{ label }}</span>
   </div>
@@ -43,7 +50,7 @@ export default {
 
   computed: {
     transformation () {
-      return `rotate(${this.rotation})`;
+      return `rotate(${this.rotation},14,14)`;
     }
   }
 };
