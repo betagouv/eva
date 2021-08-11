fe4d5c1054cd13cc6a9786786979bff21b1311f2
Clément Prod'homme <clement@captive.fr>
2021-08-11 15:53:18 +0200

    Évalué, je peux entendre les messages de la situation Objets trouvés sur Safari
    Sur Safari, il est obligatoire de préciser un lien absolue (avec l'host) pour lire un fichier audio.
    
    Co-authored-by: Marine Sourin <msourin@captive.fr>

--- src/situations/commun/infra/depot_ressources.js
+++ src/situations/commun/infra/depot_ressources.js
@@ -1,10 +1,12 @@
 let audioCtx;
 
+const HOST = `${window.location.protocol}//${window.location.host}`;
+
 function chargeurAudio (src, timeout = 2000) {
   audioCtx = audioCtx || new (window.AudioContext || window.webkitAudioContext)();
   const request = new window.XMLHttpRequest();
 
-  request.open('GET', src, true);
+  request.open('GET', `${HOST}/${src}`, true);
   request.responseType = 'arraybuffer';
 
   const promesse = new Promise((resolve, reject) => {
