9505f36919c306a3ab9a93d1fd62fa8eff9114f9
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2021-08-17 17:25:01 +0200

    Extrait la configuration Jest dans son fichier dédier et en javascript

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFiles: [
+    '<rootDir>/.jest/register-context.js'
+  ],
+  setupFilesAfterEnv: [
+    '<rootDir>/.jest/before-after.js'
+  ],
+  modulePaths: [
+    '/shared/vendor/modules'
+  ],
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'vue',
+    'test.js'
+  ],
+  moduleDirectories: [
+    'node_modules',
+    'bower_components',
+    'shared',
+    'src/situations'
+  ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.vue$': 'vue-jest'
+  },
+  moduleNameMapper: {
+    i18next: '<rootDir>/__mocks__/i18nextMock.js',
+    '\\.wav$': '<rootDir>/__mocks__/fileMock_wav.js',
+    '\\.png$': '<rootDir>/__mocks__/fileMock_png.js',
+    '\\.(jpg|jpeg)$': '<rootDir>/__mocks__/fileMock_jpg.js',
+    '\\.(gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '^(commun|controle|inventaire|maintenance|objets_trouves|prevention|tri)/(.*)$': '<rootDir>/src/situations/$1/$2'
+  }
+};
