f2fb2de49b1a1a442d63b4ce1e94e6cf6baf1990
Clément Prod'homme <clement@captive.fr>
2021-08-30 12:45:42 +0200

    Fix: execute les tests de la situation maintenance (touche.test.js)

--- tests/situations/maintenance/vues/touche.test.js
+++ tests/situations/maintenance/vues/touche.test.js
@@ -13,36 +13,36 @@ describe('La vue touche de la Maintenance', function () {
     wrapper = shallowMount(Touche, {
       localVue, propsData: { label: 'texte' }
     });
-    expect(wrapper.find('.label-droite').exists()).to.be(true);
-    expect(wrapper.find('.label-gauche').exists()).to.be(false);
+    expect(wrapper.find('.label-droite').exists()).toEqual(true);
+    expect(wrapper.find('.label-gauche').exists()).toEqual(false);
   });
 
   it('affiche un label à gauche', function () {
     wrapper = shallowMount(Touche, {
       localVue, propsData: { labelGauche: 'texte' }
     });
-    expect(wrapper.find('.label-droite').exists()).to.be(false);
-    expect(wrapper.find('.label-gauche').exists()).to.be(true);
+    expect(wrapper.find('.label-droite').exists()).toEqual(false);
+    expect(wrapper.find('.label-gauche').exists()).toEqual(true);
   });
 
   it("n'affiche aucun label", function () {
     wrapper = shallowMount(Touche, { localVue });
-    expect(wrapper.find('.label-droite').exists()).to.be(false);
-    expect(wrapper.find('.label-gauche').exists()).to.be(false);
+    expect(wrapper.find('.label-droite').exists()).toEqual(false);
+    expect(wrapper.find('.label-gauche').exists()).toEqual(false);
   });
 
   it('fait tourner le svg', function () {
     wrapper = shallowMount(Touche, {
       localVue, propsData: { rotation: 90 }
     });
-    expect(wrapper.find('rect').attributes('transform')).to.be('rotate(90,14,14)');
-    expect(wrapper.find('path').attributes('transform')).to.be('rotate(90,14,14)');
+    expect(wrapper.find('rect').attributes('transform')).toEqual('rotate(90,14,14)');
+    expect(wrapper.find('path').attributes('transform')).toEqual('rotate(90,14,14)');
   });
 
   it('peut changer de couleur', function () {
     wrapper = shallowMount(Touche, {
       localVue, propsData: { couleur: 'verte' }
     });
-    expect(wrapper.find('.touche.verte').exists()).to.be(true);
+    expect(wrapper.find('.touche.verte').exists()).toEqual(true);
   });
 });
