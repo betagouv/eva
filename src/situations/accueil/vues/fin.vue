c95aea7a7ff3e23884570e1ff1db0a5205147b71
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2021-09-06 14:20:28 +0200

    extrait dans une vue dédiée les boutons de déconnexion

--- src/situations/accueil/vues/fin.vue
+++ src/situations/accueil/vues/fin.vue
@@ -47,87 +47,25 @@
         </div>
       </div>
 
-      <transition-fade>
-        <div
-            v-if="afficheDonnerAvis"
-            key="mon-avis"
-            class='actions-fin'
-          >
-          <div class='mon-avis'>
-            <div class='information'>
-              <img class="avatar" :src="avatarAvis"/>
-              <span class="texte" v-html="$traduction('accueil.fin.avis.label')" />
-            </div>
-            <div class='actions-avis'>
-              <a :href="lienDonnerAvis"
-                target='_blank'
-                @click="fermeDonnerAvis">
-                <img style="height:40px;" :src="boutonAvis" alt="Je donne mon avis" title="Je donne mon avis sur cette démarche" />
-              </a>
-              <a class="bouton-arrondi bouton-arrondi--petit bouton-arrondi-orange"
-                 @click="fermeDonnerAvis"
-              >
-                {{ $traduction('accueil.fin.avis.fermer') }}
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div
-            v-else-if="confirmeDeconnexion"
-            key="confirme-deconnection"
-            class='actions-fin'
-        >
-            <div class="confirmation-deconnexion" >
-              <div class="information">
-                <span class="avatar">
-                  <img class :src="avatarDeconnexion" />
-                </span>
-                <span class="texte" v-html="$traduction('accueil.fin.confirmation_deconnexion.label')" />
-              </div>
-              <div class="actions-deconnexion">
-                <a
-                  class="bouton-arrondi bouton-arrondi--petit bouton-arrondi-orange"
-                  @click="deconnecte"
-                >{{ $traduction('accueil.fin.confirmation_deconnexion.bouton') }}</a>
-              </div>
-            </div>
-        </div>
-
-        <div v-else
-            key="buton-deconnexion"
-            class='actions-fin'
-        >
-          <a
-            class="bouton-deconnexion bouton-arrondi"
-            @click="afficheJeDonneMonAvis"
-          >{{ $traduction('deconnexion.titre') }}</a>
-        </div>
-      </transition-fade>
+      <bouton-deconnexion/>
     </div>
 
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapState } from 'vuex';
 import 'accueil/styles/fin.scss';
-import TransitionFade from 'commun/vues/transition_fade';
+import BoutonDeconnexion from 'accueil/vues/bouton_deconnexion';
 
 export default {
-  components: { TransitionFade },
+  components: { BoutonDeconnexion },
   computed: mapState(['competencesFortes', 'nom']),
 
   data () {
     return {
       ecran: 'bravo',
-      avatarFin: this.$depotRessources.avatarFin().src,
-      avatarAvis: this.$depotRessources.avatarAvis().src,
-      avatarDeconnexion: this.$depotRessources.avatarDeconnexion().src,
-      boutonAvis: this.$depotRessources.boutonAvis().src,
-      lienDonnerAvis: 'https://voxusagers.numerique.gouv.fr/Demarches/2381?&view-mode=formulaire-avis&nd_mode=en-ligne-enti%C3%A8rement&nd_source=button&key=e556745a003dc51aac047d6818d4e1d6',
-      afficheDonnerAvis: false,
-      confirmeDeconnexion: false
+      avatarFin: this.$depotRessources.avatarFin().src
     };
   },
 
@@ -136,23 +74,12 @@ export default {
   },
 
   methods: {
-    ...mapActions(['deconnecte']),
-
     lienSiteVitrine (competence) {
       return `https://eva.beta.gouv.fr/competences/${competence}`;
     },
 
     suivant () {
       this.ecran = 'resultat';
-    },
-
-    fermeDonnerAvis () {
-      this.afficheDonnerAvis = false;
-      this.confirmeDeconnexion = true;
-    },
-
-    afficheJeDonneMonAvis () {
-      this.afficheDonnerAvis = true;
     },
 
     termineEvaluation () {
