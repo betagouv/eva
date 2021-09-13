2e5329d2ca420f6f69cc31c4a6a6aad8eedcb332
Clément Prod'homme <clement@captive.fr>
2021-09-13 17:34:46 +0200

    Réorganise le fichier de test du dépot journal

--- tests/situations/commun/infra/depot_journal.test.js
+++ tests/situations/commun/infra/depot_journal.test.js
@@ -1,29 +1,33 @@
 import DepotJournal from 'commun/infra/depot_journal';
 
 describe('le depot du journal', function () {
-  it('fait un POST des lignes du journal vers le serveur', function () {
-    const requetes = [];
-    const depot = new DepotJournal({ ajax (params) { requetes.push(params); } });
-    depot.enregistre({ autreCle: 'valeur2', description: { cle: 'valeur2' } });
+  describe('#enregistre()', function () {
+    it('fait un POST des lignes du journal vers le serveur', function () {
+      const requetes = [];
+      const depot = new DepotJournal({ ajax (params) { requetes.push(params); } });
+      depot.enregistre({ autreCle: 'valeur2', description: { cle: 'valeur2' } });
 
-    expect(requetes.length).toBe(1);
-    expect(requetes[0].type).toBe('POST');
+      expect(requetes.length).toBe(1);
+      expect(requetes[0].type).toBe('POST');
+    });
+
+    it('retourne une promesse standard javascript', function () {
+      const depot = new DepotJournal({ ajax (params) {} });
+
+      expect(depot.enregistre({})).toBeInstanceOf(Promise);
+    });
   });
 
-  it('retourne une promesse standard javascript', function () {
-    const depot = new DepotJournal({ ajax (params) {} });
+  describe('#attendFinEnregistrement()', function () {
+    it('peut attendre la fin de tous les enregistrements', function (done) {
+      const requetes = [];
+      const depot = new DepotJournal({ ajax (params) { requetes.push(params); } });
+      depot.enregistre({});
+      depot.enregistre({});
 
-    expect(depot.enregistre({})).toBeInstanceOf(Promise);
-  });
+      depot.attendFinEnregistrement().finally(done);
 
-  it('peut attendre la fin de tous les enregistrements', function (done) {
-    const requetes = [];
-    const depot = new DepotJournal({ ajax (params) { requetes.push(params); } });
-    depot.enregistre({});
-    depot.enregistre({});
-
-    depot.attendFinEnregistrement().finally(done);
-
-    requetes.forEach((requete) => { requete.success(); });
+      requetes.forEach((requete) => { requete.success(); });
+    });
   });
 });
