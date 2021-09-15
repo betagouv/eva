4832da52f2a422ff3f0716dddd11934342dd4ad2
Clément Prod'homme <clement@captive.fr>
2021-09-15 10:20:51 +0200

    Hors ligne : permet au dépot_journal d'enregistrer les événements en local
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- tests/situations/commun/infra/depot_journal.test.js
+++ tests/situations/commun/infra/depot_journal.test.js
@@ -1,9 +1,15 @@
 import DepotJournal from 'commun/infra/depot_journal';
+import RegistreUtilisateur from 'commun/infra/registre_utilisateur';
 
 describe('le depot du journal', function () {
+  const requetes = [];
+
+  beforeEach(function () {
+    window.localStorage.clear();
+  });
+
   describe('#enregistre()', function () {
     it('fait un POST des lignes du journal vers le serveur', function () {
-      const requetes = [];
       const depot = new DepotJournal({ ajax (params) { requetes.push(params); } });
       depot.enregistre({ autreCle: 'valeur2', description: { cle: 'valeur2' } });
 
@@ -20,7 +26,6 @@ describe('le depot du journal', function () {
 
   describe('#attendFinEnregistrement()', function () {
     it('peut attendre la fin de tous les enregistrements', function (done) {
-      const requetes = [];
       const depot = new DepotJournal({ ajax (params) { requetes.push(params); } });
       depot.enregistre({});
       depot.enregistre({});
@@ -28,6 +33,48 @@ describe('le depot du journal', function () {
       depot.attendFinEnregistrement().finally(done);
 
       requetes.forEach((requete) => { requete.success(); });
+    });
+  });
+
+  describe('#enregistreEnLocale()', function () {
+    const registre = new RegistreUtilisateur();
+    const depot = new DepotJournal({ ajax (params) { requetes.push(params); } }, registre);
+
+    beforeEach(function () {
+      registre.idClient = function () { return 'id_client'; };
+    });
+
+    describe("quand il y a déjà des événements d'enregistrés", function () {
+      it('ajoute un nouvel évènement à la liste', function () {
+        window.localStorage.setItem('evenements_id_client', JSON.stringify([{ id: 1 }]));
+        const payload = { id: 2 };
+
+        depot.enregistreEnLocale(payload);
+
+        const resultat = [{ id: 1 }, { id: 2 }];
+        expect(window.localStorage.getItem('evenements_id_client')).toBe(JSON.stringify(resultat));
+      });
+    });
+
+    describe("quand il n'y a pas d'événements d'enregistrés", function () {
+      it("ajoute l'évènement dans le localStorage", function () {
+        const payload = { id: 1 };
+
+        depot.enregistreEnLocale(payload);
+
+        const resultat = [{ id: 1 }];
+        expect(window.localStorage.getItem('evenements_id_client')).toBe(JSON.stringify(resultat));
+      });
+    });
+  });
+
+  describe('#cleEvenementsPourLocalStorage()', function () {
+    it("retourne la clé pour les événements d'un id client", function () {
+      const registre = new RegistreUtilisateur();
+      const depot = new DepotJournal({ ajax (params) { requetes.push(params); } }, registre);
+      registre.idClient = function () { return 'id_client'; };
+
+      expect(depot.cleEvenementsPourLocalStorage()).toBe('evenements_id_client');
     });
   });
 });
