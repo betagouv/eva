17e93b35099b6839307cf4d6e6f1e18320b57a10
Clément Prod'homme <clement@captive.fr>
2021-09-22 17:33:55 +0200

    Restructure le test du synchronisateur

--- tests/situations/commun/infra/synchronisateur.test.js
+++ tests/situations/commun/infra/synchronisateur.test.js
@@ -2,13 +2,21 @@ import Synchronisateur from 'commun/infra/synchronisateur';
 import RegistreUtilisateur from 'commun/infra/registre_utilisateur';
 
 describe('Synchronisateur', function () {
-  it("enregistre une évaluation locale qui n'a j'amais été envoyée au serveur", function () {
-    window.localStorage.setItem('evaluation_1', JSON.stringify({ nom: 'Marcelle', code_campagne: 'CODE' }));
-    window.localStorage.setItem('evaluation_2', JSON.stringify({ nom: 'Clement', code_campagne: 'CODE' }));
+  describe('#recupereReseau()', function () {
     const registreUtilisateur = new RegistreUtilisateur();
-    const creeEvaluation = jest.spyOn(registreUtilisateur, 'creeEvaluation');
-    new Synchronisateur(registreUtilisateur).recupereReseau();
-    expect(creeEvaluation).toHaveBeenCalledTimes(2);
-    expect(creeEvaluation).toHaveBeenLastCalledWith({ nom: 'Clement', code_campagne: 'CODE' });
+
+    describe("quand l'évaluation n'existe pas coté serveur", function () {
+      beforeEach(function () {
+        window.localStorage.setItem('evaluation_1', JSON.stringify({ nom: 'Marcelle', code_campagne: 'CODE' }));
+        window.localStorage.setItem('evaluation_2', JSON.stringify({ nom: 'Clement', code_campagne: 'CODE' }));
+      });
+
+      it('enregistre une évaluation locale', function () {
+        const creeEvaluation = jest.spyOn(registreUtilisateur, 'creeEvaluation');
+        new Synchronisateur(registreUtilisateur).recupereReseau();
+        expect(creeEvaluation).toHaveBeenCalledTimes(2);
+        expect(creeEvaluation).toHaveBeenLastCalledWith({ nom: 'Clement', code_campagne: 'CODE' });
+      });
+    });
   });
 });
