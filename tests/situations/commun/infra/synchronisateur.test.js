6590f1ecdda38062b33c5b9d99e26b1b324b0570
Clément Prod'homme <clement@captive.fr>
2021-09-22 17:57:18 +0200

    Met à jour l'évaluation local si elle existe déjà sur le serveur

--- tests/situations/commun/infra/synchronisateur.test.js
+++ tests/situations/commun/infra/synchronisateur.test.js
@@ -18,5 +18,18 @@ describe('Synchronisateur', function () {
         expect(creeEvaluation).toHaveBeenLastCalledWith({ nom: 'Clement', code_campagne: 'CODE' });
       });
     });
+
+    describe("quand l'évaluation existe coté serveur", function () {
+      beforeEach(function () {
+        window.localStorage.setItem('evaluation_1', JSON.stringify({ id: 1, email: 'Marcelle@paris.fr', telephone: '061234567' }));
+      });
+
+      it("Met à jour les informations de contact au cas où elles n'auraient pas été envoyées", function () {
+        const enregistreContact = jest.spyOn(registreUtilisateur, 'enregistreContact');
+        new Synchronisateur(registreUtilisateur).recupereReseau();
+        expect(enregistreContact).toHaveBeenCalled();
+        expect(enregistreContact).toHaveBeenLastCalledWith('Marcelle@paris.fr', '061234567');
+      });
+    });
   });
 });
