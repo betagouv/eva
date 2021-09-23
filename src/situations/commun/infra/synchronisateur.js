bad5e89458d566b0ae2860acfb7c9065eab19367
Clément Prod'homme <clement@captive.fr>
2021-09-23 11:04:41 +0200

    Évite de lancer en parallère la function de synchronisation

--- src/situations/commun/infra/synchronisateur.js
+++ src/situations/commun/infra/synchronisateur.js
@@ -1,18 +1,36 @@
 export default class Synchronisateur {
   constructor (registreUtilisateur) {
     this.registreUtilisateur = registreUtilisateur;
+    this.enCoursDeSynchronisation = false;
   }
 
   recupereReseau (event) {
+    if (this.enCoursDeSynchronisation) { return; }
+
+    this.enCoursDeSynchronisation = true;
+    Promise.all(this.synchroniseEvaluations())
+      .catch((erreur) => {
+        console.error(erreur);
+      })
+      .finally(() => {
+        this.enCoursDeSynchronisation = false;
+      });
+  }
+
+  synchroniseEvaluations () {
     const evaluations = this.registreUtilisateur.listeEvaluationsLocales();
+    const promesses = [];
 
     Object.entries(evaluations).forEach(([idClient, evaluation]) => {
+      let promesse;
       if (evaluation.id) {
-        this.registreUtilisateur.enregistreContact(evaluation.email, evaluation.telephone);
+        promesse = this.registreUtilisateur.enregistreContact(evaluation.email, evaluation.telephone);
       } else {
         const data = { nom: evaluation.nom, code_campagne: evaluation.code_campagne };
-        this.registreUtilisateur.creeEvaluation(data);
+        promesse = this.registreUtilisateur.creeEvaluation(data);
       }
+      promesses.push(promesse);
     });
+    return promesses;
   }
 }
--- tests/situations/commun/infra/synchronisateur.test.js
+++ tests/situations/commun/infra/synchronisateur.test.js
@@ -3,16 +3,22 @@ import RegistreUtilisateur from 'commun/infra/registre_utilisateur';
 
 describe('Synchronisateur', function () {
   describe('#recupereReseau()', function () {
-    const registreUtilisateur = new RegistreUtilisateur();
+    let registreUtilisateur;
+
+    beforeEach(function () {
+      registreUtilisateur = new RegistreUtilisateur();
+      window.localStorage.clear();
+    });
 
     describe("quand l'évaluation n'existe pas coté serveur", function () {
-      beforeEach(function () {
+      it('enregistre une évaluation locale', function () {
         window.localStorage.setItem('evaluation_1', JSON.stringify({ nom: 'Marcelle', code_campagne: 'CODE' }));
         window.localStorage.setItem('evaluation_2', JSON.stringify({ nom: 'Clement', code_campagne: 'CODE' }));
-      });
 
-      it('enregistre une évaluation locale', function () {
-        const creeEvaluation = jest.spyOn(registreUtilisateur, 'creeEvaluation');
+        const creeEvaluation = jest.spyOn(registreUtilisateur, 'creeEvaluation')
+          .mockImplementation((data) => {
+            return Promise.resolve(data);
+          });
         new Synchronisateur(registreUtilisateur).recupereReseau();
         expect(creeEvaluation).toHaveBeenCalledTimes(2);
         expect(creeEvaluation).toHaveBeenLastCalledWith({ nom: 'Clement', code_campagne: 'CODE' });
@@ -21,14 +27,80 @@ describe('Synchronisateur', function () {
 
     describe("quand l'évaluation existe coté serveur", function () {
       beforeEach(function () {
-        window.localStorage.setItem('evaluation_1', JSON.stringify({ id: 1, email: 'Marcelle@paris.fr', telephone: '061234567' }));
+        window.localStorage.setItem('evaluation_1',
+          JSON.stringify({ id: 1, email: 'Marcelle@paris.fr', telephone: '061234567' }));
       });
 
       it("Met à jour les informations de contact au cas où elles n'auraient pas été envoyées", function () {
-        const enregistreContact = jest.spyOn(registreUtilisateur, 'enregistreContact');
+        const enregistreContact = jest.spyOn(registreUtilisateur, 'enregistreContact')
+          .mockImplementation((data) => {
+            return Promise.resolve(data);
+          });
         new Synchronisateur(registreUtilisateur).recupereReseau();
         expect(enregistreContact).toHaveBeenCalled();
         expect(enregistreContact).toHaveBeenLastCalledWith('Marcelle@paris.fr', '061234567');
+      });
+    });
+
+    describe('ne peut être appelé deux fois en même temps', function () {
+      let resoudLaPremierePromesse;
+      let promesse;
+
+      beforeEach(() => {
+        promesse = new Promise((resolve, reject) => {
+          resoudLaPremierePromesse = resolve;
+        });
+      });
+
+      it('attend la fin de #creeEvaluation', function (done) {
+        window.localStorage.setItem('evaluation_1', JSON.stringify({ nom: 'Marcelle', code_campagne: 'CODE' }));
+
+        const creeEvaluation = jest.spyOn(registreUtilisateur, 'creeEvaluation')
+          .mockImplementation(() => {
+            return promesse;
+          });
+        const synchronisateur = new Synchronisateur(registreUtilisateur);
+
+        synchronisateur.recupereReseau();
+        expect(creeEvaluation).toHaveBeenCalledTimes(1);
+
+        setTimeout(() => {
+          synchronisateur.recupereReseau();
+          expect(creeEvaluation).toHaveBeenCalledTimes(1);
+
+          resoudLaPremierePromesse({ nom: 'Marcelle', code_campagne: 'CODE' });
+          setTimeout(() => {
+            synchronisateur.recupereReseau();
+            expect(creeEvaluation).toHaveBeenCalledTimes(2);
+            done();
+          });
+        });
+      });
+
+      it('attend la fin de #enregistreContact', function (done) {
+        window.localStorage.setItem('evaluation_1',
+          JSON.stringify({ id: 1, email: 'Marcelle@paris.fr', telephone: '061234567' }));
+
+        const spy = jest.spyOn(registreUtilisateur, 'enregistreContact')
+          .mockImplementation(() => {
+            return promesse;
+          });
+        const synchronisateur = new Synchronisateur(registreUtilisateur);
+
+        synchronisateur.recupereReseau();
+        expect(spy).toHaveBeenCalledTimes(1);
+
+        setTimeout(() => {
+          synchronisateur.recupereReseau();
+          expect(spy).toHaveBeenCalledTimes(1);
+
+          resoudLaPremierePromesse({ id: 1 });
+          setTimeout(() => {
+            synchronisateur.recupereReseau();
+            expect(spy).toHaveBeenCalledTimes(2);
+            done();
+          });
+        });
       });
     });
   });
