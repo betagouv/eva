6c61622c5d7b7635a16d72264502ff11c823d81d
Stéphane Hanser <shanser@captive.fr>
2021-10-06 18:15:28 +0200

    Force la déconnexion si un code campagne est dans l'URL
    Co-authored-by: Marine Sourin <marine.sourin@beta.gouv.fr>

--- src/situations/accueil/vues/index.vue
+++ src/situations/accueil/vues/index.vue
@@ -7,11 +7,12 @@
       <overlay-erreur-chargement v-if="erreurChargement" />
       <overlay-attente v-else-if="chargement" raison='chargement' />
     </div>
-    <accueil v-else />
+    <accueil :force-campagne="forceCampagne" :force-nom="forceNom" v-else />
   </div>
 </template>
 
 <script>
+import queryString from 'query-string';
 import OverlayAttente from 'commun/vues/overlay_attente';
 import OverlayErreurChargement from 'commun/vues/overlay_erreur_chargement';
 import Accueil from './accueil';
@@ -20,9 +21,13 @@ export default {
   components: { Accueil, OverlayAttente, OverlayErreurChargement },
 
   data () {
+    const parametresUrl = queryString.parse(location.search);
+
     return {
       chargement: true,
-      erreurChargement: false
+      erreurChargement: false,
+      forceCampagne: parametresUrl.code || '',
+      forceNom: parametresUrl.nom || ''
     };
   },
 
