f0907b8ff2e905b7bddfbd3807b9ed1b648781ee
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2021-10-22 16:46:52 +0200

    Configure node.js sur le port 7700 par défaut

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
 var app = express();
-var directory = '/' + (process.env.STATIC_DIR || 'public')
+var directory = '/' + (process.env.STATIC_DIR || 'public');
 
 app.use(express.static(__dirname + directory));
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 7700;
 app.listen(port, function () {
   console.log('Listening on', port);
 });
