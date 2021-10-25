4b8be6742b7112464617f6810f055e27d5028f0f
Clément Prod'homme <clement@captive.fr>
2021-10-25 11:34:29 +0200

    Configure le router pour rediriger "/" vers "/jeu"
    L'objectif est que l'url https://app.eva.beta.gouv.fr/ continue de fonctionner

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 
 var app = express();
-var directory = '/' + (process.env.STATIC_DIR || 'public');
 
+var directory = '/' + (process.env.STATIC_DIR || 'public');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+  res.redirect('/jeu');
+});
+app.use('/', router);
 app.use(express.static(__dirname + directory));
 
 var port = process.env.PORT || 7700;
