d2c2f595493b672c95276a38e0e4d7ba586c0499
Marie Leuliette <marie.leuliette@gmail.com>
2021-10-26 18:37:12 +0200

    Creation d'un composant question-entete et ajout aux vues qcm et redaction note

--- src/situations/commun/vues/qcm.vue
+++ src/situations/commun/vues/qcm.vue
@@ -10,33 +10,9 @@
       class="question--qcm"
     >
       <slot />
-      <div class="question-entete">
-        <div
-          v-if="afficheLectureQuestion"
-          class="entete-audio"
-        >
-          <div class="question-avec-son">
-            <svg
-              width="32"
-              height="32"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="16" cy="16" r="16" fill="#1E416A"/>
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M10 19.903H8a1 1 0 0 1-1-1v-5.04a1 1 0 0 1 1-1h1.764a1 1 0 0 0 .447-.105l3.621-1.81a1 1 0 0 1 1.447.894v9.083a1 1 0 0 1-1.447.894L10 19.903Z" fill="#FBF9FA"/><path d="M23.48 10c.514.817.92 1.853 1.182 3.023.262 1.17.375 2.44.328 3.709-.047 1.268-.252 2.496-.598 3.586-.345 1.09-.823 2.008-1.392 2.682M19.48 12c.514.566.92 1.283 1.182 2.093.262.81.375 1.69.328 2.567a6.86 6.86 0 0 1-.598 2.483c-.345.754-.823 1.39-1.392 1.857" stroke="#FBF9FA" stroke-width="2" stroke-linecap="round"/>
-            </svg>
-          </div>
-          <bouton-lecture
-            class="bouton-lecture"
-            :nomTechnique="question.nom_technique"
-          />
-        </div>
-        <div class="entete-questions">
-          <p v-if="question.description">{{ question.description }}</p>
-          <p class="question-intitule">{{ question.intitule }}</p>
-          <p v-if="question.consigne">{{ question.consigne }}</p>
-        </div>
-      </div>
+      <question-entete
+      :question="question"
+      />
       <div class="question-contenu">
         <div
           v-if="question.numerique"
@@ -109,11 +85,11 @@ import 'commun/styles/formulaire_qcm.scss';
 
 import LecteurAudio from './lecteur_audio';
 import Question from './question';
-import BoutonLecture from 'commun/vues/bouton_lecture';
+import QuestionEntete from 'commun/vues/question_entete';
 import EvenementAffichageQuestionQCM from 'commun/modeles/evenement_affichage_question_qcm';
 
 export default {
-  components: { LecteurAudio, Question, BoutonLecture },
+  components: { LecteurAudio, Question, QuestionEntete },
 
   props: {
     question: {
@@ -159,9 +135,6 @@ export default {
         donneesReponse = { reponse: choix.id, succes: choix.bonneReponse };
       }
       return donneesReponse;
-    },
-    afficheLectureQuestion () {
-      return this.$depotRessources.existeMessageAudio(this.question.nom_technique);
     }
   },
 
