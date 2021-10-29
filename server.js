d5cb0cf331c5fa364a0d84820b82c583e3c996fd
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2021-10-29 12:54:28 +0200

    Utilise le module path, pour constuire les chemins

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
-var directory = '/' + (process.env.STATIC_DIR || 'public');
+var directory = path.join('/', (process.env.STATIC_DIR || 'public'));
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
   res.redirect('/jeu');
 });
 app.use('/', router);
-app.use(express.static(__dirname + directory));
+app.use(express.static(path.join(__dirname, directory)));
 
 var port = process.env.PORT || 7700;
 app.listen(port, function () {
