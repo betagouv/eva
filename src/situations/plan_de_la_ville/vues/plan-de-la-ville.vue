f7719f13bea0fb48ca5e4fdba5cda35a65fd467d
Clément Prod'homme <clement@captive.fr>
2021-12-20 16:27:02 +0100

    Renomme la vue acte.vue de plan_de_la_ville en plan-de-la-ville.vue
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- src/situations/plan_de_la_ville/vues/plan-de-la-ville.vue
+++ src/situations/plan_de_la_ville/vues/plan-de-la-ville.vue
@@ -1,16 +1,18 @@
 <template>
-    <qcm
-      v-if='questions.length'
-      :key="questionActive.id"
-      :question="questionActive"
-      @reponse="reponseQuestion"
-    />
+  <qcm
+    v-if='question.length'
+    :key="questionActive.id"
+    :question="questionActive"
+    @reponse="reponseQuestion"
+  />
 </template>
 
 <script>
 import { mapState } from 'vuex';
 import Qcm from 'commun/vues/qcm';
 import EvenementReponse from 'questions/modeles/evenement_reponse';
+import ClicMaisonBleue from 'plan_de_la_ville/vues/components/clic-maison-bleue.vue';
+import Vue from 'vue';
 
 export default {
   components: { Qcm },
@@ -19,6 +21,11 @@ export default {
     return {
       indexQuestion: 0
     };
+  },
+
+  mounted () {
+    // enregistre globalement, les composants spécifiques utilisés par certaines questions
+    Vue.component('clic-maison-bleue', ClicMaisonBleue);
   },
 
   computed: {
