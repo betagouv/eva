294a1a357f920786b5bdf24a11fb6b7f5f39d984
Clément Prod'homme <clement@captive.fr>
2021-12-20 16:27:42 +0100

    Permet de cliquer sur la maison bleue dans la situation Plan de la ville
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

---
+++ src/situations/plan_de_la_ville/vues/components/clic-maison-bleue.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id='maison-bleue' v-on:click="clicValide"></div>
+</template>
+
+<script>
+  import 'plan_de_la_ville/styles/clic-maison-bleue.scss';
+
+  export default {
+    methods: {
+      clicValide: function (event) {
+        this.$emit('reponse');
+      }
+    }
+  };
+</script>
