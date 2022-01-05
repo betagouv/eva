5c819cb3e8547574feb7b7dca9f69a4ffa1ce59d
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2022-01-05 12:47:45 +0100

    Corrige l'affichage de la situation Question (le Bureau)
    - Un test s'assure qu'on arrive bien à étendre DepotRessourcesCommmune
    - ajoute l'aide Calculatrice

---
+++ tests/situations/questions/infra/depot_resources_questions.test.js
@@ -0,0 +1,19 @@
+import chargeurs, { chargeurDefaut } from '../../commun/aides/mock_chargeurs';
+import DepotRessourcesQuestions from 'questions/infra/depot_ressources_questions';
+import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
+
+describe('Le dépot ressource de la situation Question', function () {
+  let depot;
+
+  beforeEach(function () {
+    depot = new DepotRessourcesQuestions(chargeurs({ wav: chargeurDefaut }));
+  });
+
+  it('étend DépotRessourcesCommune', function () {
+    expect(depot).toBeInstanceOf(DepotRessourcesCommunes);
+  });
+
+  it('fourni une aide complementaire', function () {
+    expect(depot.imageAideComplementaire()).toEqual(depot.calculatrice());
+  });
+});
