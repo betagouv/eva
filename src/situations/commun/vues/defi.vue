2aa4ea27e7a97811d1ad0e92fa1dc1526c4d3c9f
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2022-01-17 11:27:32 +0100

    Extrait le composant Qcm

--- src/situations/commun/vues/defi.vue
+++ src/situations/commun/vues/defi.vue
@@ -21,43 +21,6 @@
             :question="question"
             v-model="reponse"
         />
-        <div v-else>
-          <div
-            v-for="(element, index) in question.choix"
-            :key="element.id"
-            class="question-reponse question-reponse-multiple"
-          >
-            <bouton-lecture
-              v-if="afficheLectureReponse(element.nom_technique)"
-              class="bouton-lecture"
-              :nomTechnique="element.nom_technique"
-            />
-            <input
-              v-model="reponse"
-              :value="element.id"
-              :id="element.id"
-              name="question"
-              type="radio"
-              class="question-input"
-            />
-            <label
-              :for="element.id"
-              class="question-reponse-intitule"
-            >
-              <reponse-audio-qcm
-                v-if="element.audio"
-                :joue-son="reponse === element.id"
-                :questionnaire="element.audio"
-                :idReponse="index"
-              />
-              <img
-                v-if="element.image"
-                :src="element.image"
-              />
-              {{ element.intitule }}
-            </label>
-          </div>
-        </div>
       </div>
       <button
         v-if="question.type != 'action'"
@@ -76,16 +39,15 @@ import 'commun/styles/boutons.scss';
 import 'commun/styles/bouton.scss';
 import 'commun/styles/formulaire_qcm.scss';
 
-import ReponseAudioQcm from './reponse_audio_qcm';
-import BoutonLecture from 'commun/vues/bouton_lecture';
-import ChampSaisie from 'commun/vues/defi/champ_saisie';
 import Question from './question';
 import QuestionEntete from 'commun/vues/question_entete';
 import Jauge from 'commun/vues/defi/jauge';
+import ChampSaisie from 'commun/vues/defi/champ_saisie';
+import Qcm from 'commun/vues/defi/qcm';
 import EvenementAffichageQuestionQCM from 'commun/modeles/evenement_affichage_question_qcm';
 
 export default {
-  components: { ReponseAudioQcm, BoutonLecture, Question, QuestionEntete, Jauge, ChampSaisie },
+  components: { Question, QuestionEntete, Jauge, ChampSaisie, Qcm },
 
   props: {
     question: {
@@ -120,7 +82,7 @@ export default {
     composantContenu () {
       if (this.question.type === 'action') return undefined;
       if (this.question.type === 'qcm') {
-        return this.question.type_qcm === 'jauge' ? 'jauge' : undefined;
+        return this.question.type_qcm === 'jauge' ? 'jauge' : 'qcm';
       }
       return this.question.type;
     },
@@ -157,9 +119,6 @@ export default {
     envoi () {
       this.envoyer = true;
       this.$emit('reponse', this.donneesReponse);
-    },
-    afficheLectureReponse (nomTechnique) {
-      return this.$depotRessources.existeMessageAudio(nomTechnique);
     }
   }
 };
