92e383927a2bcb1b738da993d284962c4c043fe7
Marine Dominé <marine@captive.fr>
2022-01-26 17:28:24 +0100

    Ajoute les questions et réponses de la situation Café de la place

--- src/situations/cafe_de_la_place/data/defi.js
+++ src/situations/cafe_de_la_place/data/defi.js
@@ -5,7 +5,7 @@ const titre1 = {
   nom_technique: 'titre_1',
   type: 'qcm',
   illustration: listeTitresMusique,
-  description: 'Pour le titre 1, quel est la bonne lecture ? ',
+  description: 'Pour le titre 1, quel est la bonne lecture ?',
   intitule: 'Dès que vous aurez reconnu la bonne lecture, choisissez  une de ces réponses en cliquant sur une des cases. Pour confirmer votre réponse, cliquer sur Valider.',
   choix: [
     {
@@ -26,8 +26,233 @@ const titre1 = {
   ]
 };
 
+const titre2 = {
+  id: 'titre2',
+  nom_technique: 'titre_2',
+  type: 'qcm',
+  illustration: listeTitresMusique,
+  description: 'Titre 2 :',
+  choix: [
+    {
+      id: 'jazzABimoudon',
+      nom_technique: 'jazz_a_bimoudon',
+      bonneReponse: false
+    },
+    {
+      id: 'jazzADimoudon',
+      nom_technique: 'jazz_a_dimoudon',
+      bonneReponse: true
+    },
+    {
+      id: 'jazzADiboudon',
+      nom_technique: 'jazz_a_diboudon',
+      bonneReponse: false
+    }
+  ]
+};
+
+const titre10 = {
+  id: 'titre10',
+  nom_technique: 'titre_10',
+  type: 'qcm',
+  illustration: listeTitresMusique,
+  description: 'Titre 10 :',
+  choix: [
+    {
+      id: 'unAmiEnAmont',
+      nom_technique: 'un_ami_en_amont',
+      bonneReponse: false
+    },
+    {
+      id: 'unAmiAmant',
+      nom_technique: 'un_ami_amant',
+      bonneReponse: false
+    },
+    {
+      id: 'unAmiUnAmant',
+      nom_technique: 'un_ami_un_amant',
+      bonneReponse: true
+    }
+  ]
+};
+
+const titre3 = {
+  id: 'titre3',
+  nom_technique: 'titre_3',
+  type: 'qcm',
+  illustration: listeTitresMusique,
+  description: 'Titre 3 :',
+  choix: [
+    {
+      id: 'balleEtTalle',
+      nom_technique: 'balle_et_talle',
+      bonneReponse: false
+    },
+    {
+      id: 'pailleEtDaille',
+      nom_technique: 'paille_et_daille',
+      bonneReponse: false
+    },
+    {
+      id: 'bailleEtTaille',
+      nom_technique: 'baille_et_taille',
+      bonneReponse: true
+    }
+  ]
+};
+
+const titre11 = {
+  id: 'titre11',
+  nom_technique: 'titre_11',
+  type: 'qcm',
+  illustration: listeTitresMusique,
+  description: 'Titre 11 :',
+  choix: [
+    {
+      id: 'dussoEtMatura',
+      nom_technique: 'dusso_et_matura',
+      bonneReponse: false
+    },
+    {
+      id: 'duxoEtMatura',
+      nom_technique: 'duxo_et_matura',
+      bonneReponse: true
+    },
+    {
+      id: 'duxoEtMadura',
+      nom_technique: 'duxo_et_madura',
+      bonneReponse: false
+    }
+  ]
+};
+
+const titre6 = {
+  id: 'titre6',
+  nom_technique: 'titre_6',
+  type: 'qcm',
+  illustration: listeTitresMusique,
+  description: 'Titre 6 :',
+  choix: [
+    {
+      id: 'amourSugulent',
+      nom_technique: 'amour_sugulent',
+      bonneReponse: false
+    },
+    {
+      id: 'amourSucculent',
+      nom_technique: 'amour_succulent',
+      bonneReponse: true
+    },
+    {
+      id: 'amourSuccule',
+      nom_technique: 'amour_succule',
+      bonneReponse: false
+    }
+  ]
+};
+
+const titre8 = {
+  id: 'titre8',
+  nom_technique: 'titre_8',
+  type: 'qcm',
+  illustration: listeTitresMusique,
+  description: 'Titre 8 :',
+  choix: [
+    {
+      id: 'anguilEtDouceMer',
+      nom_technique: 'anguil_et_douce_mer',
+      bonneReponse: false
+    },
+    {
+      id: 'anquilleEtDouceMer',
+      nom_technique: 'anquille_et_douce_mer',
+      bonneReponse: false
+    },
+    {
+      id: 'anguilleEtDouceMer',
+      nom_technique: 'anguille_et_douce_mer',
+      bonneReponse: true
+    }
+  ]
+};
+
+const titre5 = {
+  id: 'titre5',
+  nom_technique: 'titre_5',
+  type: 'qcm',
+  illustration: listeTitresMusique,
+  description: 'Titre 5 :',
+  choix: [
+    {
+      id: 'exerciceDeStyle',
+      nom_technique: 'exercice_de_style',
+      bonneReponse: true
+    },
+    {
+      id: 'ezerciceDeStyle',
+      nom_technique: 'ezercice_de_style',
+      bonneReponse: false
+    },
+    {
+      id: 'esserciceDeStyle',
+      nom_technique: 'essercice_de_style',
+      bonneReponse: false
+    }
+  ]
+};
+
+const titre4 = {
+  id: 'titre4',
+  nom_technique: 'titre_4',
+  type: 'qcm',
+  illustration: listeTitresMusique,
+  description: 'Titre 4 :',
+  choix: [
+    {
+      id: 'leVilDuTemps',
+      nom_technique: 'le_vil_du_temps',
+      bonneReponse: false
+    },
+    {
+      id: 'leFilsDuTemps',
+      nom_technique: 'le_fils_du_temps',
+      bonneReponse: false
+    },
+    {
+      id: 'leFilDuTemps',
+      nom_technique: 'le_fil_du_temps',
+      bonneReponse: true
+    }
+  ]
+};
+
+const titre7 = {
+  id: 'titre7',
+  nom_technique: 'titre_7',
+  type: 'qcm',
+  illustration: listeTitresMusique,
+  description: 'Titre 7 :',
+  choix: [
+    {
+      id: 'soupconDAmertume',
+      nom_technique: 'soupcon_d_amertume',
+      bonneReponse: true
+    },
+    {
+      id: 'souponDAmertume',
+      nom_technique: 'soupon_d_amertume',
+      bonneReponse: false
+    },
+    {
+      id: 'soupconDAbertube',
+      nom_technique: 'soupcon_d_abertube',
+      bonneReponse: false
+    }
+  ]
+};
+
 const configurationNormale = {
-  questions: [titre1]
+  questions: [titre1, titre2, titre10, titre3, titre11, titre6, titre8, titre5, titre4, titre7]
 };
 
 export { configurationNormale };
--- src/situations/cafe_de_la_place/infra/depot_ressources_cafe_de_la_place.js
+++ src/situations/cafe_de_la_place/infra/depot_ressources_cafe_de_la_place.js
@@ -3,19 +3,91 @@ import sonConsigne from 'commun/assets/consigne_blanche.mp3';
 import fondSituation from 'cafe_de_la_place/assets/liste_titres_musique.png';
 
 import titre1 from 'cafe_de_la_place/assets/audio_questions/titre1.mp3';
+import titre2 from 'cafe_de_la_place/assets/audio_questions/titre2.mp3';
+import titre10 from 'cafe_de_la_place/assets/audio_questions/titre10.mp3';
+import titre3 from 'cafe_de_la_place/assets/audio_questions/titre3.mp3';
+import titre11 from 'cafe_de_la_place/assets/audio_questions/titre11.mp3';
+import titre6 from 'cafe_de_la_place/assets/audio_questions/titre6.mp3';
+import titre8 from 'cafe_de_la_place/assets/audio_questions/titre8.mp3';
+import titre5 from 'cafe_de_la_place/assets/audio_questions/titre5.mp3';
+import titre4 from 'cafe_de_la_place/assets/audio_questions/titre4.mp3';
+import titre7 from 'cafe_de_la_place/assets/audio_questions/titre7.mp3';
 
 import sonChoixBax from 'cafe_de_la_place/assets/audio_reponses/bax.mp3';
 import sonChoixMasse from 'cafe_de_la_place/assets/audio_reponses/masse.mp3';
 import sonChoixMax from 'cafe_de_la_place/assets/audio_reponses/max.mp3';
+import sonChoixJazzDiboudon from 'cafe_de_la_place/assets/audio_reponses/jazz_a_diboudon.mp3';
+import sonChoixJazzBimoudon from 'cafe_de_la_place/assets/audio_reponses/jazz_a_bimoudon.mp3';
+import sonChoixJazzDimoudon from 'cafe_de_la_place/assets/audio_reponses/jazz_a_dimoudon.mp3';
+import sonChoixUnAmiEnAmont from 'cafe_de_la_place/assets/audio_reponses/un_ami_en_amont.mp3';
+import sonChoixUnAmiAmant from 'cafe_de_la_place/assets/audio_reponses/un_ami_amant.mp3';
+import sonChoixUnAmiUnAmant from 'cafe_de_la_place/assets/audio_reponses/un_ami_un_amant.mp3';
+import sonChoixBalleEtTalle from 'cafe_de_la_place/assets/audio_reponses/balle_et_talle.mp3';
+import sonChoixPailleEtDaille from 'cafe_de_la_place/assets/audio_reponses/paille_et_daille.mp3';
+import sonChoixBailleEtTaille from 'cafe_de_la_place/assets/audio_reponses/baille_et_taille.mp3';
+import sonChoixDussoEtMatura from 'cafe_de_la_place/assets/audio_reponses/dusso_et_matura.mp3';
+import sonChoixDuxoEtMatura from 'cafe_de_la_place/assets/audio_reponses/duxo_et_matura.mp3';
+import sonChoixDuxoEtMadura from 'cafe_de_la_place/assets/audio_reponses/duxo_et_madura.mp3';
+import sonChoixAmourSugulent from 'cafe_de_la_place/assets/audio_reponses/amour_sugulent.mp3';
+import sonChoixAmourSucculent from 'cafe_de_la_place/assets/audio_reponses/amour_succulent.mp3';
+import sonChoixAmourSuccule from 'cafe_de_la_place/assets/audio_reponses/amour_succule.mp3';
+import sonChoixAnguilEtDouceMer from 'cafe_de_la_place/assets/audio_reponses/anguil_et_douce_mer.mp3';
+import sonChoixAnquilleEtDouceMer from 'cafe_de_la_place/assets/audio_reponses/anquille_et_douce_mer.mp3';
+import sonChoixAnguilleEtDouceMer from 'cafe_de_la_place/assets/audio_reponses/anguille_et_douce_mer.mp3';
+import sonChoixExerciceDeStyle from 'cafe_de_la_place/assets/audio_reponses/exercice_de_style.mp3';
+import sonChoixEzerciceDeStyle from 'cafe_de_la_place/assets/audio_reponses/ezercice_de_style.mp3';
+import sonChoixEsserciceDeStyle from 'cafe_de_la_place/assets/audio_reponses/essercice_de_style.mp3';
+import sonChoixLeVilDuTemps from 'cafe_de_la_place/assets/audio_reponses/le_vil_du_temps.mp3';
+import sonChoixLeFilsDuTemps from 'cafe_de_la_place/assets/audio_reponses/le_fils_du_temps.mp3';
+import sonChoixLeFilDuTemps from 'cafe_de_la_place/assets/audio_reponses/le_fil_du_temps.mp3';
+import sonChoixSoupconDAmertume from 'cafe_de_la_place/assets/audio_reponses/soupcon_d_amertume.mp3';
+import sonChoixSouponDAmertume from 'cafe_de_la_place/assets/audio_reponses/soupon_d_amertume.mp3';
+import sonChoixSoupconDAbertube from 'cafe_de_la_place/assets/audio_reponses/soupcon_d_abertube.mp3';
 
 const AUDIOS_QUESTIONS = {
-  titre_1: titre1
+  titre_1: titre1,
+  titre_2: titre2,
+  titre_10: titre10,
+  titre_3: titre3,
+  titre_11: titre11,
+  titre_6: titre6,
+  titre_8: titre8,
+  titre_5: titre5,
+  titre_4: titre4,
+  titre_7: titre7
 };
 
 const AUDIOS_REPONSES = {
   bax: sonChoixBax,
   masse: sonChoixMasse,
-  max: sonChoixMax
+  max: sonChoixMax,
+  jazz_a_diboudon: sonChoixJazzDiboudon,
+  jazz_a_bimoudon: sonChoixJazzBimoudon,
+  jazz_a_dimoudon: sonChoixJazzDimoudon,
+  un_ami_en_amont: sonChoixUnAmiEnAmont,
+  un_ami_amant: sonChoixUnAmiAmant,
+  un_ami_un_amant: sonChoixUnAmiUnAmant,
+  balle_et_talle: sonChoixBalleEtTalle,
+  paille_et_daille: sonChoixPailleEtDaille,
+  baille_et_taille: sonChoixBailleEtTaille,
+  dusso_et_matura: sonChoixDussoEtMatura,
+  duxo_et_matura: sonChoixDuxoEtMatura,
+  duxo_et_madura: sonChoixDuxoEtMadura,
+  amour_sugulent: sonChoixAmourSugulent,
+  amour_succulent: sonChoixAmourSucculent,
+  amour_succule: sonChoixAmourSuccule,
+  anguil_et_douce_mer: sonChoixAnguilEtDouceMer,
+  anquille_et_douce_mer: sonChoixAnquilleEtDouceMer,
+  anguille_et_douce_mer: sonChoixAnguilleEtDouceMer,
+  exercice_de_style: sonChoixExerciceDeStyle,
+  ezercice_de_style: sonChoixEzerciceDeStyle,
+  essercice_de_style: sonChoixEsserciceDeStyle,
+  le_vil_du_temps: sonChoixLeVilDuTemps,
+  le_fils_du_temps: sonChoixLeFilsDuTemps,
+  le_fil_du_temps: sonChoixLeFilDuTemps,
+  soupcon_d_amertume: sonChoixSoupconDAmertume,
+  soupon_d_amertume: sonChoixSouponDAmertume,
+  soupcon_d_abertube: sonChoixSoupconDAbertube
 };
 
 const messagesAudios = { ...AUDIOS_QUESTIONS, ...AUDIOS_REPONSES };
