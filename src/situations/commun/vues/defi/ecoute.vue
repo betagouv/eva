2caeaef97298d2bc0ea803ce8ef95a6cddbb4c4d
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2022-02-25 16:44:15 +0100

    Ajoute le composant ecoute.vue
    Ainsi `defi.vue` reste bien générique et n'a pas à passer des parametres
    spécifique à un seul de ses composants

---
+++ src/situations/commun/vues/defi/ecoute.vue
@@ -0,0 +1,22 @@
+<template>
+  <bouton-lecture
+    :nomTechnique="question.reponse"
+    :avecTexte="true"
+  />
+</template>
+
+<script>
+import 'commun/styles/defi/ecoute.scss';
+import BoutonLecture from 'commun/vues/bouton_lecture';
+
+export default {
+  components: { BoutonLecture },
+
+  props: {
+    question: {
+      type: Object,
+      required: true
+    }
+  },
+};
+</script>
