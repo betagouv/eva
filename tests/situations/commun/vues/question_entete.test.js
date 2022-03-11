2c6e2927c9e1b25ccb0df0229f7fbbe20e1da29a
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2022-03-11 21:26:48 +0100

    Ajoute des tests

--- tests/situations/commun/vues/question_entete.test.js
+++ tests/situations/commun/vues/question_entete.test.js
@@ -1,38 +1,106 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import { creeStore } from 'commun/modeles/store';
-import BoutonLecture from 'commun/vues/bouton_lecture';
 import VueQuestionEntete from 'commun/vues/question_entete';
+import { CHARGEMENT, DEMARRE, FINI } from 'commun/modeles/situation';
 
 describe('la vue du composant entête', function () {
   let question;
   let localVue;
   let store;
+  let mockDemareSon;
+  let mockCoupeSon;
+  let BoutonLectureStub;
 
   beforeEach(function () {
+    mockDemareSon = jest.fn();
+    mockCoupeSon = jest.fn();
+    BoutonLectureStub = {
+      name: 'BoutonLecture',
+      template: '<span />',
+      methods: {
+        demarreSon: mockDemareSon,
+        coupeSon: mockCoupeSon
+      }
+    };
     store = creeStore();
     question = { id: 154, choix: [], nom_technique: 'question1' };
     localVue = createLocalVue();
-    localVue.prototype.$journal = { enregistre () {} };
-    localVue.prototype.$traduction = () => {};
     localVue.prototype.$depotRessources = {
-      reponseAudio: () => {
-        return 'chemin de la ressource';
-      },
       existeMessageAudio: () => false
     };
   });
 
-  it("affiche le bouton lecture s'il existe un son", function () {
-    localVue.prototype.$depotRessources.existeMessageAudio = () => true;
-    const vue = shallowMount(VueQuestionEntete, { localVue, store, propsData: { question } });
+  function composant(question) {
+      return shallowMount(VueQuestionEntete, {
+        localVue,
+        store,
+        propsData: { question },
+        stubs: {
+          'bouton-lecture': BoutonLectureStub
+        }
+      });
+  }
 
-    expect(vue.findComponent(BoutonLecture).exists()).toBe(true);
+  describe('quand il existe un son', function() {
+    let vue;
+
+    beforeEach(function() {
+      localVue.prototype.$depotRessources.existeMessageAudio = () => true;
+    });
+
+    it("affiche le bouton lecture", function () {
+      vue = composant(question);
+      expect(vue.findComponent(BoutonLectureStub).exists()).toBe(true);
+    });
+
+    it("joue le son à l'ouverture si l'acte est démarré", function () {
+      store.state.etat = DEMARRE;
+      vue = composant(question);
+      expect(mockDemareSon).toHaveBeenCalled();
+    });
+
+    it("joue le son quand l'acte démarre", function (done) {
+      store.state.etat = CHARGEMENT;
+      vue = composant(question);
+      expect(mockDemareSon).toHaveBeenCalledTimes(0);
+      store.commit('modifieEtat', DEMARRE);
+      vue.vm.$nextTick(() => {
+        expect(mockDemareSon).toHaveBeenCalledTimes(1);
+        done();
+      });
+    });
+
+    it('coupe le son si la situation est terminée', function (done) {
+      store.state.etat = DEMARRE;
+      vue = composant(question);
+      store.commit('modifieEtat', FINI);
+      vue.vm.$nextTick(() => {
+        expect(mockCoupeSon).toHaveBeenCalledTimes(1);
+        done();
+      });
+    });
   });
 
-  it("n'affiche pas le bouton lecture s'il n'existe pas de son", function () {
-    localVue.prototype.$depotRessources.existeMessageAudio = () => false;
-    const vue = shallowMount(VueQuestionEntete, { localVue, store, propsData: { question } });
+  describe("quand il n'existe pas de son", function() {
+    let vue;
 
-    expect(vue.findComponent(BoutonLecture).exists()).toBe(false);
+    beforeEach(function() {
+      localVue.prototype.$depotRessources.existeMessageAudio = () => false;
+    });
+
+    it("n'affiche pas le bouton lecture", function () {
+      vue = composant(question);
+      expect(vue.findComponent(BoutonLectureStub).exists()).toBe(false);
+    });
+
+    it('ne coupe pas le son si la situation est terminée', function (done) {
+      store.state.etat = DEMARRE;
+      vue = composant(question);
+      store.commit('modifieEtat', FINI);
+      vue.vm.$nextTick(() => {
+        expect(mockCoupeSon).toHaveBeenCalledTimes(0);
+        done();
+      });
+    });
   });
 });
