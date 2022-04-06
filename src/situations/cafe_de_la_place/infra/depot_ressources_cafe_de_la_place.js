692d163e40454302a8f8a3c700908cf12bdad9c1
Marie Leuliette <marie.leuliette@gmail.com>
2022-04-06 16:37:15 +0200

    Integration du son pour la sous consigne 1

--- src/situations/cafe_de_la_place/infra/depot_ressources_cafe_de_la_place.js
+++ src/situations/cafe_de_la_place/infra/depot_ressources_cafe_de_la_place.js
@@ -6,6 +6,7 @@ import sousConsigneALrd1 from 'cafe_de_la_place/assets/audio_consignes/sous_cons
 import sousConsigneALrd2 from 'cafe_de_la_place/assets/audio_consignes/sous_consigne_ALrd_2.mp3';
 import sousConsigneACrd1 from 'cafe_de_la_place/assets/audio_consignes/sous_consigne_ACrd_1.mp3';
 import sousConsigneACrd2 from 'cafe_de_la_place/assets/audio_consignes/sous_consigne_ACrd_2.mp3';
+import sousConsigneAPlc1 from 'cafe_de_la_place/assets/audio_consignes/sous_consigne_APlc_1.mp3';
 
 import titre1 from 'cafe_de_la_place/assets/audio_questions/titre1.mp3';
 import titre2 from 'cafe_de_la_place/assets/audio_questions/titre2.mp3';
@@ -83,7 +84,8 @@ const AUDIOS_CONSIGNES = {
   sous_consigne_ALrd_1: sousConsigneALrd1,
   sous_consigne_ALrd_2: sousConsigneALrd2,
   sous_consigne_ACrd_1: sousConsigneACrd1,
-  sous_consigne_ACrd_2: sousConsigneACrd2
+  sous_consigne_ACrd_2: sousConsigneACrd2,
+  sous_consigne_APlc_1: sousConsigneAPlc1
 };
 
 const AUDIOS_QUESTIONS = {
