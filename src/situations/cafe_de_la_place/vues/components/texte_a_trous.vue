a53b4e40aea6d362b81247fb18769f1c51551df0
Clément Prod'homme <clement@captive.fr>
2022-04-13 17:23:35 +0200

    Permet de voir les saisies précédentes lorsque que l'on complète APlc

--- src/situations/cafe_de_la_place/vues/components/texte_a_trous.vue
+++ src/situations/cafe_de_la_place/vues/components/texte_a_trous.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="contenu-telephone">
-    <h1 class="contenu-telephone__titre">Courses pour la <trou-a-completer id="APlc1"/></h1>
+    <h1 class="contenu-telephone__titre">Courses pour la <trou-a-completer idQuestion="APlc1"/></h1>
     <ul class="contenu-telephone__liste">
-      <li>2 <trou-a-completer id="APlc2"/> en <trou-a-completer id="APlc3"/></li>
-      <li>1 pot de <trou-a-completer id="APlc4"/></li>
-      <li>Du <trou-a-completer id="APlc5"/></li>
-      <li>2 kilos de <trou-a-completer id="APlc6"/> de <trou-a-completer id="APlc7"/></li>
-      <li>4 <trou-a-completer id="APlc8"/></li>
-      <li>Des <trou-a-completer id="APlc9"/> qui <trou-a-completer id="APlc10"/> du goût</li>
-      <li>2 <trou-a-completer id="APlc11"/> d'<trou-a-completer id="APlc12"/></li>
-      <li>2 <trou-a-completer id="APlc13"/></li>
-      <li>1 <trou-a-completer id="APlc14"/></li>
-      <li>1 <trou-a-completer id="APlc15"/></li>
-      <li>1 <trou-a-completer id="APlc16"/> de <trou-a-completer id="APlc17"/></li>
-      <li>1 <trou-a-completer id="APlc18"/> qui n'<trou-a-completer id="APlc19"/> pas</li>
-      <li>1 solution <trou-a-completer id="APlc20"/></li>
+      <li>2 <trou-a-completer idQuestion="APlc2"/> en <trou-a-completer idQuestion="APlc3"/></li>
+      <li>1 pot de <trou-a-completer idQuestion="APlc4"/></li>
+      <li>Du <trou-a-completer idQuestion="APlc5"/></li>
+      <li>2 kilos de <trou-a-completer idQuestion="APlc6"/> de <trou-a-completer idQuestion="APlc7"/></li>
+      <li>4 <trou-a-completer idQuestion="APlc8"/></li>
+      <li>Des <trou-a-completer idQuestion="APlc9"/> qui <trou-a-completer idQuestion="APlc10"/> du goût</li>
+      <li>2 <trou-a-completer idQuestion="APlc11"/> d'<trou-a-completer idQuestion="APlc12"/></li>
+      <li>2 <trou-a-completer idQuestion="APlc13"/></li>
+      <li>1 <trou-a-completer idQuestion="APlc14"/></li>
+      <li>1 <trou-a-completer idQuestion="APlc15"/></li>
+      <li>1 <trou-a-completer idQuestion="APlc16"/> de <trou-a-completer idQuestion="APlc17"/></li>
+      <li>1 <trou-a-completer idQuestion="APlc18"/> qui n'<trou-a-completer idQuestion="APlc19"/> pas</li>
+      <li>1 solution <trou-a-completer idQuestion="APlc20"/></li>
     </ul>
   </div>
 </template>
