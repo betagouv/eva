07e3a965a02da154b6e34c021be1ec38e335a46e
Marie Leuliette <marie.leuliette@gmail.com>
2022-05-17 14:56:58 +0200

    Désengorger le fichier defi.js du cafe de la place en répartissant les datas d'orientation, du parcours haut et du parcours bas en sous fichiers

---
+++ src/situations/cafe_de_la_place/data/parcours_haut.js
@@ -0,0 +1,14 @@
+const sousConsigneHPar1 = {
+  id: 'HPar-sous-consigne-1',
+  nom_technique: 'sous_consigne_HPar_1',
+  type: 'sous-consigne',
+  intitule: "Première sous-consigne du parcours Haut à venir",
+};
+
+const parcoursHaut = {
+  series: [
+    { cartes: [ sousConsigneHPar1 ] },
+  ]
+};
+
+export { parcoursHaut };
