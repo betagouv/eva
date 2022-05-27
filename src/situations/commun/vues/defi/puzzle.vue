0f754fe1ea5553ef88b4420669a4c8ef12395001
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2022-05-27 17:48:18 +0200

    HPar1 : Ajout de l'illustration et de la description de la question

---
+++ src/situations/commun/vues/defi/puzzle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="puzzle">
+  </div>
+</template>
+
+<script>
+import 'commun/styles/defi/puzzle.scss';
+
+export default {
+  props: {
+    question: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
