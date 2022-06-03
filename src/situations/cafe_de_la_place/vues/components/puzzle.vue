97b275bb379872ef31a99ce1e088fee5996cb885
Marie Leuliette <marie.leuliette@gmail.com>
2022-06-03 17:36:10 +0200

    Zone de dépôt par défaut à gauche pour les nouvelles du jour

--- src/situations/cafe_de_la_place/vues/components/puzzle.vue
+++ src/situations/cafe_de_la_place/vues/components/puzzle.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="puzzle-container">
-    <draggable class="puzzle-gauche" :list="nouvellesDuJourClassees" group="nouvelles">
+    <draggable class="puzzle-gauche" :list="nouvellesDuJourClassees" group="nouvelles" draggable=".puzzle-item">
       <div
         v-for="nouvelle in nouvellesDuJourClassees"
         :key="nouvelle.id"
@@ -17,6 +17,14 @@
           </svg>
         </div>
         <span>{{nouvelle.contenu}}</span>
+      </div>
+      <div v-if="nouvellesDuJourClassees.length == 0"
+        class="puzzle-item invisible">
+        <!-- cette div permet d'éviter que le premier ghost n'apparaisse en dessous du footer -->
+      </div>
+      <div v-if="affichePuzzleDroite"
+        class="zone-de-depot">
+        {{ $traduction('cafe_de_la_place.puzzle.texte_zone_depot') }}
       </div>
     </draggable>
     <draggable
