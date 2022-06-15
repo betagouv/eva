5ab05b4501c85117ce52dbe1bd9eccb0c92429be
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2022-06-15 18:23:46 +0200

    Affiche un message d'erreur si lecture mp3 impossible

--- tests/situations/accueil/vues/index.test.js
+++ tests/situations/accueil/vues/index.test.js
@@ -2,6 +2,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Index from 'accueil/vues/index';
 import OverlayAttente from 'commun/vues/overlay_attente';
 import OverlayErreurChargement from 'commun/vues/overlay_erreur_chargement';
+import OverlayErreurLectureSon from 'accueil/vues/overlay_erreur_lecture_son';
 import Accueil from 'accueil/vues/accueil';
 import { traduction } from 'commun/infra/internationalisation';
 
@@ -40,19 +41,64 @@ describe('La vue index', function () {
 
   describe('quand elle a fini de charger avec une erreur', function () {
     let wrapper;
+    let consoleError;
 
     beforeEach(function () {
-      depotRessources = new class {
-        chargement () { return Promise.reject(new Error()); }
-      }();
-      localVue.prototype.$depotRessources = depotRessources;
-      wrapper = shallowMount(Index, { localVue });
+      consoleError = jest.spyOn(console, 'error').mockImplementation(() => {});
     });
 
-    it('affiche les composants lorsque le chargement a échoué', function () {
-      expect(wrapper.findComponent(Accueil).exists()).toBe(false);
-      expect(wrapper.findComponent(OverlayAttente).exists()).toBe(false);
-      expect(wrapper.findComponent(OverlayErreurChargement).exists()).toBe(true);
+    afterEach(() => {
+      console.error.mockRestore();
+    });
+
+    describe("quand c'est une erreur innatendue", function() {
+      beforeEach(function () {
+        localVue.prototype.$depotRessources = {
+          chargement: () => { return Promise.reject(new Error('erreur innatendue')); }
+        };
+
+        wrapper = shallowMount(Index, { localVue });
+      });
+
+      it('affiche la vue erreur de chargement', function () {
+        expect(wrapper.findComponent(Accueil).exists()).toBe(false);
+        expect(wrapper.findComponent(OverlayAttente).exists()).toBe(false);
+        expect(wrapper.findComponent(OverlayErreurChargement).exists()).toBe(true);
+        expect(consoleError).toHaveBeenCalled();
+      });
+    });
+
+    describe('quand elle ne peut pas lire les mp3', function () {
+      beforeEach(function () {
+        localVue.prototype.$depotRessources = {
+          chargement: () => { return Promise.reject(new Error('Unable to decode audio data')); }
+        };
+
+        wrapper = shallowMount(Index, { localVue });
+      });
+
+      it('affiche la vue erreur lecture son impossible', function () {
+        expect(wrapper.findComponent(Accueil).exists()).toBe(false);
+        expect(wrapper.findComponent(OverlayAttente).exists()).toBe(false);
+        expect(wrapper.findComponent(OverlayErreurChargement).exists()).toBe(false);
+        expect(wrapper.findComponent(OverlayErreurLectureSon).exists()).toBe(true);
+        expect(consoleError).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('quand elle rencontre une erreur de décodage audio', function () {
+      beforeEach(function () {
+        localVue.prototype.$depotRessources = {
+          chargement: () => { return Promise.reject(new Error('An unknown error occured while processing decodeAudioData')); }
+        };
+
+        wrapper = shallowMount(Index, { localVue });
+      });
+
+      it('affiche la vue erreur lecture son impossible', function () {
+        expect(wrapper.findComponent(OverlayErreurLectureSon).exists()).toBe(true);
+        expect(consoleError).not.toHaveBeenCalled();
+      });
     });
   });
 });
