36f137e7ac5572cde48ff53d42db5aa35e35148c
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2022-06-15 18:23:46 +0200

    Affiche un message d'erreur si lecture mp3 impossible

--- src/situations/accueil/vues/index.vue
+++ src/situations/accueil/vues/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="accueil">
     <div
-      v-if="chargement || erreurChargement"
+      v-if="chargement || erreurChargement || erreurLectureSon"
       class="conteneur"
     >
-      <overlay-erreur-chargement v-if="erreurChargement" />
-      <overlay-attente v-else-if="chargement" raison='chargement' />
+      <overlay-erreur-lecture-son v-if="erreurLectureSon" />
+      <overlay-erreur-chargement v-else-if="erreurChargement" />
+      <overlay-attente v-else raison='chargement' />
     </div>
     <accueil :force-campagne="forceCampagne" :force-nom="forceNom" v-else />
   </div>
@@ -15,10 +16,11 @@
 import queryString from 'query-string';
 import OverlayAttente from 'commun/vues/overlay_attente';
 import OverlayErreurChargement from 'commun/vues/overlay_erreur_chargement';
+import OverlayErreurLectureSon from 'accueil/vues/overlay_erreur_lecture_son';
 import Accueil from './accueil';
 
 export default {
-  components: { Accueil, OverlayAttente, OverlayErreurChargement },
+  components: { Accueil, OverlayAttente, OverlayErreurChargement, OverlayErreurLectureSon },
 
   data () {
     const parametresUrl = queryString.parse(location.search);
@@ -26,6 +28,7 @@ export default {
     return {
       chargement: true,
       erreurChargement: false,
+      erreurLectureSon: false,
       forceCampagne: parametresUrl.code || '',
       forceNom: parametresUrl.nom || ''
     };
@@ -34,8 +37,15 @@ export default {
   mounted () {
     this.$depotRessources.chargement().then(() => {
       this.chargement = false;
-    }).catch(() => {
-      this.erreurChargement = true;
+    }).catch((erreur) => {
+      if(erreur.message.includes('Unable to decode audio data') ||
+         erreur.message.includes('decodeAudioData')) {
+        this.erreurLectureSon = true;
+      }
+      else {
+        this.erreurChargement = true;
+        console.error(erreur);
+      }
     });
   }
 };
