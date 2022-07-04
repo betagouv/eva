30c9b06ab3ec582a2328ad032ca8d6cee88d01fb
Marie Leuliette <marie.leuliette@gmail.com>
2022-07-04 18:46:46 +0200

    Ajoute des barres selectionnables au graphique et envoie la réponse

--- src/situations/cafe_de_la_place/vues/components/graphique.vue
+++ src/situations/cafe_de_la_place/vues/components/graphique.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="zone-cliquable">
+  <div class="graphique">
+    <label class="graphique-barre moyenne">
+      <span>13%</span>
+    </label>
+    <label
+      v-for="pays in pays"
+      v-bind:key="pays.id"
+      class="graphique-barre graphique-barre--selectionnable"
+      :class="{'graphique-barre--selectionnee': paysSelectionnes.includes(pays.id)}"
+      :style="{ height: pays.hauteur }"
+    >
+      <input type="checkbox" class="checkbox" :value="pays.id" v-model="paysSelectionnes" />
+      <span>{{pays.pourcentage}}%</span>
+    </label>
   </div>
 </template>
 
@@ -7,5 +20,57 @@
 import 'cafe_de_la_place/styles/graphique.scss';
 
 export default {
+  data() {
+    return {
+      paysSelectionnes: [],
+      pays: [
+        {
+          id: 'allemagne',
+          pourcentage: 23,
+          hauteur: '17.375rem'
+        },
+        {
+          id: 'pologne',
+          pourcentage: 18,
+          hauteur: '14rem'
+        },
+        {
+          id: 'danemark',
+          pourcentage: 10,
+          hauteur: '7.5rem'
+        },
+        {
+          id: 'grece',
+          pourcentage: 16,
+          hauteur: '12.5rem'
+        },
+        {
+          id: 'italie',
+          pourcentage: 10,
+          hauteur: '7.5rem'
+        },
+        {
+          id: 'roumanie',
+          pourcentage: 6,
+          hauteur: '4.75rem'
+        },
+        {
+          id: 'espagne',
+          pourcentage: 8,
+          hauteur: '6.5rem'
+        },
+      ]
+    };
+  },
+
+  watch: {
+    paysSelectionnes () {
+      if (this.paysSelectionnes.length === 0) {
+        this.$emit('reponse');
+      } else {
+        this.$emit('reponse', { reponse: this.paysSelectionnes } );
+      }
+    }
+  }
 };
 </script>
