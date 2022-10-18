abb439d9cd43c05c3022a96326840e2063593495
Marie Leuliette <marie.leuliette@gmail.com>
2022-10-18 19:16:19 +0200

    integration du v-select à la place du select dans le composant champ formulaire

--- tests/situations/accueil/vues/formulaire_donnees_complementaires.test.js
+++ tests/situations/accueil/vues/formulaire_donnees_complementaires.test.js
@@ -25,13 +25,20 @@ describe("Le formulaire des données complementaires", function () {
     });
   });
 
+  function derouleMenu (donnee) {
+    wrapper
+      .find(`[id='formulaire-donnee_sociodemographique.${donnee}']`)
+      .find('.vs__dropdown-toggle')
+      .trigger('mousedown');
+  }
+
   it("s'affiche", function () {
     expect(wrapper.find('h2').text()).toEqual('accueil.donnee_sociodemographique.titre{"nom":"un nom"}');
     expect(wrapper.exists('form')).toBe(true);
     expect(wrapper.exists('label')).toBe(true);
     expect(wrapper.findAll('input[type=number]').length).toEqual(1);
     expect(wrapper.find('input[type=number]').classes()).toContain('champ-age');
-    expect(wrapper.findAll('select').length).toEqual(3);
+    expect(wrapper.findAll('.v-select').length).toEqual(3);
     expect(wrapper.findAll('.bouton-sociodemographique').length).toEqual(1);
   });
 
@@ -58,49 +65,61 @@ describe("Le formulaire des données complementaires", function () {
     ]);
   });
 
-  it("peut selectionner une valeur dans un select", function () {
-    wrapper
-      .find("[id='formulaire-donnee_sociodemographique.genre']")
-      .findAll('option')
-      .at(1)
-      .setSelected();
-    expect(wrapper
-      .find("[id='formulaire-donnee_sociodemographique.genre']")
-      .find('option:checked').element.value).toBe('Homme');
-    expect(wrapper.vm.genre).toEqual('Homme');
-  });
-
-  it("enregistre les données saisies", function (done) {
-    wrapper.find('.champ-age').setValue(30);
-    wrapper
-      .find("[id='formulaire-donnee_sociodemographique.genre']")
-      .findAll('option')
-      .at(1)
-      .setSelected();
-    wrapper
-      .find("[id='formulaire-donnee_sociodemographique.dernier_niveau_etude']")
-      .findAll('option')
-      .at(1)
-      .setSelected();
-    wrapper
-      .find("[id='formulaire-donnee_sociodemographique.derniere_situation']")
-      .findAll('option')
-      .at(1)
-      .setSelected();
-    wrapper.vm.envoieFormulaire();
-
-    wrapper.vm.$nextTick(() => {
-      wrapper.vm.$nextTick(() => {
-        expect(storeDispatch).toHaveBeenNthCalledWith(1, 'enregistreDonneesComplementaires', {
-          "donnee_sociodemographique_attributes": {
-            "age": "30",
-            "genre": 'Homme',
-            "dernier_niveau_etude": 'Niveau Collège',
-            "derniere_situation": 'Scolarisation'
-          }
-        });
+  it("peut selectionner une valeur dans un select", function (done) {
+    const inputGenre = wrapper.find("[id='formulaire-donnee_sociodemographique.genre']");
+    derouleMenu('genre');
+    wrapper.vm.$nextTick(() => {  
+      inputGenre
+        .findAll('.vs__dropdown-option')
+        .at(0)
+        .trigger('click');
+      wrapper.vm.$nextTick(() => {  
+        expect(inputGenre
+          .find('.vs__selected').text()).toBe('Homme');
+        expect(wrapper.vm.genre).toEqual('Homme');
         done();
       });
+    });
+  });
+
+  it("enregistre les données saisies", async () => {
+    wrapper.find('.champ-age').setValue(30);
+    derouleMenu('dernier_niveau_etude');
+
+    await wrapper.vm.$nextTick();
+
+    wrapper
+      .find("[id='formulaire-donnee_sociodemographique.dernier_niveau_etude']")
+      .findAll('.vs__dropdown-option')
+      .at(0)
+      .trigger('click');
+    derouleMenu('genre');
+
+    await wrapper.vm.$nextTick();
+
+    wrapper
+      .find("[id='formulaire-donnee_sociodemographique.genre']")
+      .findAll('.vs__dropdown-option')
+      .at(0)
+      .trigger('click');
+    derouleMenu('derniere_situation');
+
+    await wrapper.vm.$nextTick();
+
+    wrapper
+      .find("[id='formulaire-donnee_sociodemographique.derniere_situation']")
+      .findAll('.vs__dropdown-option')
+      .at(0)
+      .trigger('click');
+    wrapper.vm.envoieFormulaire();
+    await wrapper.vm.$nextTick();
+    expect(storeDispatch).toHaveBeenNthCalledWith(1, 'enregistreDonneesComplementaires', {
+      "donnee_sociodemographique_attributes": {
+        "age": "30",
+        "genre": 'Homme',
+        "dernier_niveau_etude": 'Niveau Collège',
+        "derniere_situation": 'Scolarisation'
+      }
     });
   });
 });
