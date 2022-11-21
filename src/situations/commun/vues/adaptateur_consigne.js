76005a2dca94678df55f52063086f301034e6b0b
Étienne Charignon <etienne.charignon@gmail.com>
2022-11-21 18:07:34 +0100

    Demarre le jeux après l'entrainement

--- src/situations/commun/vues/adaptateur_consigne.js
+++ src/situations/commun/vues/adaptateur_consigne.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 
-import { ATTENTE_DEMARRAGE, ENTRAINEMENT_DEMARRE, ENTRAINEMENT_FINI, DEMARRE } from 'commun/modeles/situation';
+import { ATTENTE_DEMARRAGE, ENTRAINEMENT_DEMARRE, DEMARRE } from 'commun/modeles/situation';
 import IntroConsigne from 'commun/vues/intro_consigne';
 import Transition from 'commun/vues/transition';
 
@@ -9,30 +9,29 @@ import { traduction } from 'commun/infra/internationalisation';
 export default class AdaptateurConsigne {
   constructor (situation, depotRessources) {
     this.situation = situation;
-    this.depotRessources = depotRessources;
-  }
-
-  affiche (pointInsertion, $) {
-    const div = document.createElement('div');
-    $(pointInsertion).append(div);
-    const vue = this.situation.etat() === ATTENTE_DEMARRAGE ? IntroConsigne : Transition;
+    const vue = situation.etat() === ATTENTE_DEMARRAGE ? IntroConsigne : Transition;
     this.app = createApp(vue, {
       identifiantSituation: this.situation.identifiant,
       onConsigneFini: () => {
         this.situation.modifieEtat(this.prochainEtat());
       }
     });
-    this.app.config.globalProperties.$depotRessources = this.depotRessources;
+    this.app.config.globalProperties.$depotRessources = depotRessources;
     this.app.config.globalProperties.$traduction = traduction;
+  }
+
+  affiche (pointInsertion, $) {
+    const div = document.createElement('div');
+    $(pointInsertion).append(div);
     this.app.mount(div);
   }
 
   prochainEtat () {
-    if (this.situation.etat() === ENTRAINEMENT_FINI ||
-        !this.situation.entrainementDisponible()) {
-      return DEMARRE;
+    if (this.situation.etat() === ATTENTE_DEMARRAGE &&
+        this.situation.entrainementDisponible()) {
+      return ENTRAINEMENT_DEMARRE;
     }
-    return ENTRAINEMENT_DEMARRE;
+    return DEMARRE;
   }
 
   cache () {
