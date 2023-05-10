0a18de5f216733b2ff6e0a03c8abd4c7dd195f35
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2023-05-10 19:05:53 +0200

    Rends ses illustrations de questions à Livraison

--- src/situations/livraison/infra/depot_ressources_livraison.js
+++ src/situations/livraison/infra/depot_ressources_livraison.js
@@ -1,10 +1,25 @@
-import DepotRessourcesQuestion from 'questions/infra/depot_ressources_questions';
+import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
 
 import sonConsigne from 'livraison/assets/consigne_demarrage_livraison.mp3';
 import sonConsigneTransition from 'livraison/assets/consigne_transition_livraison.mp3';
 
-export default class DepotRessourcesLivraison extends DepotRessourcesQuestion {
+import { illustrationsQuestions } from '../data/illustrations';
+
+export default class DepotRessourcesLivraison extends DepotRessourcesCommunes {
   constructor (chargeurs) {
     super(chargeurs, {}, {}, null, sonConsigne, sonConsigneTransition);
+    this.charge([...new Set(Object.values(illustrationsQuestions))]);
+  }
+
+  imageAideComplementaire () {
+    return this.calculatrice();
+  }
+
+  illustrationQuestion (question) {
+    if (question.nom_technique && illustrationsQuestions[question.nom_technique]) {
+      return this.ressource(illustrationsQuestions[question.nom_technique]);
+    } else {
+      throw new Error(`La question ${question.id} avec le nom technique "${question.nom_technique}" ne possède pas d'illustration`);
+    }
   }
 }
--- src/situations/questions/infra/depot_ressources_questions.js
+++ src/situations/questions/infra/depot_ressources_questions.js
@@ -2,23 +2,9 @@ import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
 
 import sonConsigne from 'questions/assets/consigne_demarrage_questions.mp3';
 import sonConsigneBlanche from 'commun/assets/consigne_blanche.mp3';
-import { illustrationsQuestions } from '../data/illustrations';
 
 export default class DepotRessourcesQuestions extends DepotRessourcesCommunes {
   constructor (chargeurs, messagesVideos = {}, messagesAudio = {}, fondConsigne = null, sonConsigneDemarrage = sonConsigne, sonConsigneTransition = sonConsigneBlanche ) {
     super(chargeurs, messagesVideos, messagesAudio, fondConsigne, sonConsigneDemarrage, sonConsigneTransition);
-    this.charge([...new Set(Object.values(illustrationsQuestions))]);
-  }
-
-  imageAideComplementaire () {
-    return this.calculatrice();
-  }
-
-  illustrationQuestion (question) {
-    if (question.nom_technique && illustrationsQuestions[question.nom_technique]) {
-      return this.ressource(illustrationsQuestions[question.nom_technique]);
-    } else {
-      throw new Error(`La question ${question.id} avec le nom technique "${question.nom_technique}" ne possède pas d'illustration`);
-    }
   }
 }
