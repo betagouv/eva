4f4a376c1b2522063b59f11662a4f88f7b7b1ce4
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2023-05-10 19:23:36 +0200

    Simplifie dépotResourceQuestion qui n'est de toute façon pas utilisée (situation Bureau)

--- src/situations/questions/infra/depot_ressources_questions.js
+++ src/situations/questions/infra/depot_ressources_questions.js
@@ -1,10 +1,13 @@
 import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
 
-import sonConsigne from 'questions/assets/consigne_demarrage_questions.mp3';
 import sonConsigneBlanche from 'commun/assets/consigne_blanche.mp3';
 
 export default class DepotRessourcesQuestions extends DepotRessourcesCommunes {
-  constructor (chargeurs, messagesVideos = {}, messagesAudio = {}, fondConsigne = null, sonConsigneDemarrage = sonConsigne, sonConsigneTransition = sonConsigneBlanche ) {
-    super(chargeurs, messagesVideos, messagesAudio, fondConsigne, sonConsigneDemarrage, sonConsigneTransition);
+  constructor (chargeurs) {
+    super(chargeurs, {}, {}, null, sonConsigneBlanche);
+  }
+
+  imageAideComplementaire () {
+    return this.calculatrice();
   }
 }
