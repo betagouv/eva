524970faa11acfd32de79090efbc8f076987e8ac
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2023-05-10 19:05:53 +0200

    Rends ses illustrations de questions à Livraison

---
+++ tests/situations/livraison/infra/depot_resources_livraison.test.js
@@ -0,0 +1,41 @@
+import chargeurs from '../../commun/aides/mock_chargeurs';
+import DepotRessourcesLivraison from 'livraison/infra/depot_ressources_livraison';
+import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
+import CetC1 from 'livraison/assets/illustration_questions/CetC1.jpg';
+
+describe('Le dépot ressource de la situation Livraison', function () {
+  let depot;
+
+  beforeEach(function () {
+    depot = new DepotRessourcesLivraison(chargeurs());
+  });
+
+  it('étend DépotRessourcesCommune', function () {
+    expect(depot).toBeInstanceOf(DepotRessourcesCommunes);
+  });
+
+  it('fourni une aide complementaire', function () {
+    expect(depot.imageAideComplementaire()).toEqual(depot.calculatrice());
+  });
+
+  describe("#illustrationQuestion", function() {
+    it("Retourne l'illustration d'une question", function () {
+      expect(depot.illustrationQuestion({ nom_technique: 'connaissance_et_comprehension_1' }))
+        .toEqual(CetC1);
+    });
+
+    it("Retourne une erreur si une question n'a pas d'illustration", function () {
+      expect(() => {
+        depot.illustrationQuestion({ id: 'id1', nom_technique: 'inconnu' });
+      })
+        .toThrow('La question id1 avec le nom technique "inconnu" ne possède pas d\'illustration');
+    });
+
+    it("Retourne une erreur si une question n'a pas de nom technique", function () {
+      expect(() => {
+        depot.illustrationQuestion({ id: 'id1' });
+      })
+        .toThrow('La question id1 avec le nom technique "undefined" ne possède pas d\'illustration');
+    });
+  });
+});
--- tests/situations/questions/infra/depot_resources_questions.test.js
+++ tests/situations/questions/infra/depot_resources_questions.test.js
@@ -1,7 +1,6 @@
 import chargeurs from '../../commun/aides/mock_chargeurs';
 import DepotRessourcesQuestions from 'questions/infra/depot_ressources_questions';
 import DepotRessourcesCommunes from 'commun/infra/depot_ressources_communes';
-import CetC1 from 'questions/assets/illustration_questions/CetC1.jpg';
 
 describe('Le dépot ressource de la situation Question', function () {
   let depot;
@@ -12,30 +11,5 @@ describe('Le dépot ressource de la situation Question', function () {
 
   it('étend DépotRessourcesCommune', function () {
     expect(depot).toBeInstanceOf(DepotRessourcesCommunes);
-  });
-
-  it('fourni une aide complementaire', function () {
-    expect(depot.imageAideComplementaire()).toEqual(depot.calculatrice());
-  });
-
-  describe("#illustrationQuestion", function() {
-    it("Retourne l'illustration d'une question", function () {
-      expect(depot.illustrationQuestion({ nom_technique: 'connaissance_et_comprehension_1' }))
-        .toEqual(CetC1);
-    });
-
-    it("Retourne une erreur si une question n'a pas d'illustration", function () {
-      expect(() => {
-        depot.illustrationQuestion({ id: 'id1', nom_technique: 'inconnu' });
-      })
-        .toThrow('La question id1 avec le nom technique "inconnu" ne possède pas d\'illustration');
-    });
-
-    it("Retourne une erreur si une question n'a pas de nom technique", function () {
-      expect(() => {
-        depot.illustrationQuestion({ id: 'id1' });
-      })
-        .toThrow('La question id1 avec le nom technique "undefined" ne possède pas d\'illustration');
-    });
   });
 });
