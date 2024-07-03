3a741b0605510f77c01f3d3b752da6c9767939c6
Marie Leuliette <marie.leuliette@captive.fr>
2024-07-03 13:02:11 +0200

    Ajouter toutes les questions du niveau 1

--- src/situations/place_du_marche/data/defis.js
+++ src/situations/place_du_marche/data/defis.js
@@ -1,11 +1,16 @@
 import { niveau1, niveau2, niveau3 } from './numeratie';
-import { N1Rrn } from './rattrapage';
+import { N1Rrn, N1Rde, N1Res, N1Ron, N1Roa, N1Ros } from './rattrapage';
 
 const configurationNormale = {
   'niveau1': niveau1,
   'niveau2': niveau2,
   'niveau3': niveau3,
   'N1Prn': N1Rrn,
+  'N1Pde': N1Rde,
+  'N1Pes': N1Res,
+  'N1Pon': N1Ron,
+  'N1Poa': N1Roa,
+  'N1Pos': N1Ros,
 };
 
 export { configurationNormale };
