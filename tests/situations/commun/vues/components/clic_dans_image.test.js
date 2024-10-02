e503571e69b634e099cdb0bd2560f726fb5b1f8d
Marie Leuliette <marie.leuliette@captive.fr>
2024-10-02 14:01:18 +0900

    ✨ Configure la selection multiple dans clic dans image

--- tests/situations/commun/vues/components/clic_dans_image.test.js
+++ tests/situations/commun/vues/components/clic_dans_image.test.js
@@ -2,6 +2,9 @@ import { shallowMount } from '@vue/test-utils';
 
 import ClicDansImage from 'commun/vues/components/clic_dans_image.vue';
 
+const svgUneReponse = `<svg><circle class="bonne-reponse"></circle><path></path></svg">`;
+const svgDeuxReponses = `<svg><path class="bonne-reponse"></path><path class="bonne-reponse"></path><path></path></svg>`;
+
 describe('Le composant Clic Dans Image', function () {
   let question;
   let wrapper;
@@ -9,12 +12,9 @@ describe('Le composant Clic Dans Image', function () {
   beforeEach(function () {
     question = {
       id: 1,
-      zone_cliquable: "data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgNTEyIDUxMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48Y2lyY2xlIGNsYXNzPSJib25uZS1yZXBvbnNlIiBjeD0iMjU2IiBjeT0iNTYiIHI9IjQwIi8+PHBhdGggZD0ibTIwNC4yMyAyNzQuNDRjMi45LTE4LjA2IDQuMi0zNS41Mi0uNS00Ny41OS00LTEwLjM4LTEyLjctMTYuMTktMjMuMi0yMC4xNWwtOTIuNTMtMjkuOTRjLTEyLTQtMjMuMjEtMTAuNy0yNC0yMy45NC0xLTE3IDE0LTI4IDI5LTI0IDAgMCA4OCAzMS4xNCAxNjMgMzEuMTRzMTYyLTMxIDE2Mi0zMWMxOC01IDMwIDkgMzAgMjMuNzkgMCAxNC4yMS0xMSAxOS4yMS0yNCAyMy45NGwtODggMzEuOTFjLTggMy0yMSA5LTI2IDE4LjE4LTYgMTAuNzUtNSAyOS41My0yLjEgNDcuNTlsNS45IDI5LjYzIDM3LjQxIDE2My45YzIuOCAxMy4xNS02LjMgMjUuNDQtMTkuNCAyNy43NHMtMjMuODEtNi42NC0yNy42OS0xOS4zNmwtMzcuNTYtMTE1LjkzcS0yLjcxLTguMzQtNC44LTE2Ljg3bC01Ljc2LTIzLjQ4LTUuMyAyMS42NXEtMi41MiAxMC4zNS01LjggMjAuNDhsLTM2LjkgMTE0LjA1Yy00IDEyLjg1LTE0LjUgMjEuNzUtMjcuNiAxOS40NnMtMjIuNC0xNS41OS0xOS40Ni0yNy43NGwzNy4zOS0xNjMuODN6Ii8+PC9zdmc+Cg==",
-      reponse: {
-        score: 1
-      }
+      score: 1,
+      zone_cliquable: svgToBase64(svgUneReponse)
     };
-    wrapper = composant(question);
   });
 
   function composant (question) {
@@ -23,14 +23,30 @@ describe('Le composant Clic Dans Image', function () {
     });
   }
 
+  function svgToBase64(svgString) {
+    const base64Svg = btoa(svgString);
+    const dataUri = `data:image/svg+xml;base64,${base64Svg}`;
+    return dataUri;
+  }
+
   it("Affiche le svg", function () {
     const wrapper = composant(question);
 
     expect(wrapper.find('svg').exists()).toBe(true);
   });
 
+  describe('quand je clique sur le svg directement', function () {
+    it("n'ajoute pas la classe reponse--selectionnee", function () {
+      const wrapper = composant(question);
+
+      const svg = wrapper.find('svg');
+      svg.trigger('click');
+      expect(svg.classes()).not.toContain('reponse--selectionnee');
+    });
+  });
+
   describe('quand je clique sur un element du svg', function () {
-    it("ajoute la classe reponse--selectionnee", function () {
+    it("selectionne l'élément", function () {
       const wrapper = composant(question);
 
       const reponse = wrapper.find('circle');
@@ -39,22 +55,98 @@ describe('Le composant Clic Dans Image', function () {
     });
   });
 
-  describe("l'envoi de la réponse", function() {
+  describe('pour une selection simple', function () {
+    beforeEach(function () {
+      wrapper = composant(question);
+    });
+
+    describe("#selectionMultiple", function () {
+      it("est faux", function () {
+        expect(wrapper.vm.selectionMultiple).toBe(false);
+      });
+    });
+
+    describe('quand je clique sur un second element du svg', function () {
+      it("désélectionne l'ancien élement et sélectionne le nouveau", function (done) {
+        const circle = wrapper.find('circle');
+        circle.trigger('click');
+
+        wrapper.vm.$nextTick(() => {
+          expect(circle.classes()).toContain('reponse--selectionnee');
+          const path = wrapper.find('path');
+          path.trigger('click');
+
+          wrapper.vm.$nextTick(() => {
+            expect(path.classes()).toContain('reponse--selectionnee');
+            done();
+          });
+        });
+      });
+    });
+
     describe('quand je sélectionne une bonne réponse', function () {
-      it('émet la bonne réponse', function () {
+      it('émet la bonne réponse et le score', function () {
         const reponse = wrapper.find('.bonne-reponse');
         reponse.trigger('click');
         expect(wrapper.emitted().reponse.length).toEqual(1);
         expect(wrapper.emitted().reponse[0][0].succes).toEqual(true);
+        expect(wrapper.emitted().reponse[0][0].score).toEqual(1);
       });
     });
 
     describe('quand je sélectionne une mauvaise réponse', function () {
-      it("émet la mauvaise réponse", function () {
+      it("émet la mauvaise réponse et le score", function () {
         const reponse = wrapper.find('path');
         reponse.trigger('click');
         expect(wrapper.emitted().reponse.length).toEqual(1);
         expect(wrapper.emitted().reponse[0][0].succes).toEqual(false);
+        expect(wrapper.emitted().reponse[0][0].score).toEqual(0);
+      });
+    });
+  });
+
+  describe('pour une selection multiple', function () {
+    beforeEach(function(){
+      question.zone_cliquable = svgToBase64(svgDeuxReponses);
+      wrapper = composant(question);
+    });
+
+    describe("#selectionMultiple", function () {
+      it("est vrai", function () {
+        expect(wrapper.vm.selectionMultiple).toBe(true);
+      });
+    });
+
+    describe('quand je clique sur un second element du svg', function () {
+      it("sélectionne le nouveau et laisse l'ancien élément sélectionné", function (done) {
+        const path = wrapper.findAll('path');
+        path[0].trigger('click');
+
+        wrapper.vm.$nextTick(() => {
+          expect(path[0].classes()).toContain('reponse--selectionnee');
+          path[1].trigger('click');
+
+          wrapper.vm.$nextTick(() => {
+            expect(path[1].classes()).toContain('reponse--selectionnee');
+            expect(path[0].classes()).toContain('reponse--selectionnee');
+            done();
+          });
+        });
+      });
+    });
+
+    describe('envoie des réponses', function () {
+      it('émet une partie ou la totalité du score en fonction du nombre de bonnes réponses sélectionnées', function () {
+        const reponses = wrapper.findAll('.bonne-reponse');
+        expect(reponses.length).toBe(2);
+        reponses[0].trigger('click');
+        expect(wrapper.emitted().reponse.length).toEqual(1);
+        expect(wrapper.emitted().reponse[0][0].succes).toEqual(false);
+        expect(wrapper.emitted().reponse[0][0].score).toEqual(0.5);
+
+        reponses[1].trigger('click');
+        expect(wrapper.emitted().reponse[1][0].succes).toEqual(true);
+        expect(wrapper.emitted().reponse[1][0].score).toEqual(1);
       });
     });
   });
