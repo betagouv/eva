8c0c84e7d28ec65432bd52a039723c4afd6c8a79
Marie Leuliette <marie.leuliette@captive.fr>
2024-12-11 15:31:18 +0900

    ✅ Ajoute test pour le nouveau barème de calcul du score de glisser déposer place du marché et refacto des test

--- tests/situations/place_du_marche/vues/components/glisser_deposer.test.js
+++ tests/situations/place_du_marche/vues/components/glisser_deposer.test.js
@@ -12,7 +12,7 @@ describe('Le composant Glisser Déposer de place du marché', function () {
     config.global.renderStubDefaultSlot = false;
   });
 
-  function genereVue(reponsesNonClassees, zone_depot_url = null) {
+  function genereVue(reponsesNonClassees = []) {
     wrapper = mount(glisserDeposer, {
       global: {
         mocks: {
@@ -22,21 +22,27 @@ describe('Le composant Glisser Déposer de place du marché', function () {
       props: {
         question: {
           reponsesNonClassees,
-          score: 1,
-          zone_depot_url
+          score: 1
         }
       }
     });
   }
 
+  function mockNombreZonesDeDepot(nombre) {
+    jest.spyOn(wrapper.vm, 'nombreZonesDeDepot', 'get').mockReturnValue(nombre);
+  }
+
   it("affiche le composant glisser déposer", function () {
-    genereVue([]);
+    genereVue();
     expect(wrapper.find('.glisser-deposer').exists()).toBe(true);
   });
 
   describe('#envoiReponsesOrdonnees', function () {
-    it('envoi la réponse avec son ordre, son score et son succès en cas de succès', function () {
+    beforeEach(function () {
       genereVue([1, 4]);
+    });
+
+    it('envoi la réponse avec son ordre, son score et son succès en cas de succès', function () {
       wrapper.vm.envoiReponsesOrdonnees( { scoreMax: 1, succes: true, reponse: [1, 2] });
       expect(wrapper.emitted('reponse').length).toEqual(1);
       expect(wrapper.emitted('reponse')[0][0]).toEqual({
@@ -48,7 +54,6 @@ describe('Le composant Glisser Déposer de place du marché', function () {
     });
 
     it("envoi la réponse avec son ordre, son score et son succès en cas d'échec", function () {
-      genereVue([1, 4]);
       wrapper.vm.envoiReponsesOrdonnees( { scoreMax: 1, succes: false, reponse: [2, 1] });
       expect(wrapper.emitted('reponse').length).toEqual(1);
       expect(wrapper.emitted('reponse')[0][0]).toEqual({
@@ -65,37 +70,76 @@ describe('Le composant Glisser Déposer de place du marché', function () {
       wrapper.vm.envoiReponsesPlacees({ reponse, succes });
     }
 
-    beforeEach(function () {
-      jest.restoreAllMocks();
-    });
+    describe("quand il y a autant d'éléments à placer que de zones de dépôt", function () {
+      beforeEach(function () {
+        genereVue([1, 2, 3, 4]);
+        mockNombreZonesDeDepot(4);
+      });
 
-    it('émet un événement avec au moins une réponse placée', function () {
-      const zone = "data:image/svg+xml;base64,PHN2ZyBjbGFzcz0iYm9ubmUtcmVwb25zZSIgd2lkdGg9IjEwODgiIGhlaWdodD0iNTY2IiB2aWV3Qm94PSIwIDAgMTA4OCA1NjYiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxyZWN0IGNsYXNzPSJ6b25lLWRlcG90IHpvbmUtZGVwb3QtLU4yUFExUjEiIHg9IjI0MCIgeT0iNzQiIHdpZHRoPSIyNTEiIGhlaWdodD0iMTYxIiBmaWxsPSIjMjAwMDlGIiBmaWxsLW9wYWNpdHk9IjAuMjUiLz4KPHJlY3QgY2xhc3M9InpvbmUtZGVwb3Qgem9uZS1kZXBvdC0tTjJQUTFSMiIgeD0iNjMiIHk9IjMyMCIgd2lkdGg9IjE1MCIgaGVpZ2h0PSIxMjMiIGZpbGw9IiNGRjAwMDAiIGZpbGwtb3BhY2l0eT0iMC4zNCIvPgo8L3N2Zz4K";
-      genereVue([1, 2, 3, 4], zone);
-      expect(wrapper.vm.zoneDeDepots.length > 0).toEqual(true);
+      it('émet un événement avec au moins une réponse placée', function () {
+        envoyerReponse(true, 'N1Pse1');
 
-      envoyerReponse(true, 'N1Pse1');
+        expect(wrapper.vm.auMoinsUneReponsePlacee).toBe(true);
+        expect(wrapper.emitted('reponse')[0][0]).toEqual({
+          scoreMax: 1,
+          succes: false,
+          score: 0.25,
+          reponse: ['N1Pse1']
+        });
+      });
 
-      expect(wrapper.emitted('reponse')[0][0]).toEqual({
-        scoreMax: 1,
-        succes: false,
-        score: 0.25,         reponse: ['N1Pse1']
+      it('émet un événement avec toutes les réponses placées', function () {
+        envoyerReponse(true, 'N1Pse1');
+        envoyerReponse(true, 'N1Pse2');
+        envoyerReponse(true, 'N1Pse3');
+        envoyerReponse(true, 'N1Pse4');
+
+        expect(wrapper.emitted('reponse')).toEqual(
+          expect.arrayContaining([[{ "reponse": ["N1Pse1", "N1Pse2", "N1Pse3", "N1Pse4"], "score": 1, "scoreMax": 1, "succes": true }]])
+        );
       });
     });
 
-    it('émet un événement avec toutes les réponses placées', function () {
-      const zone = "data:image/svg+xml;base64,PHN2ZyBjbGFzcz0iYm9ubmUtcmVwb25zZSIgd2lkdGg9IjEwODgiIGhlaWdodD0iNTY2IiB2aWV3Qm94PSIwIDAgMTA4OCA1NjYiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxyZWN0IGNsYXNzPSJ6b25lLWRlcG90IHpvbmUtZGVwb3QtLU4yUFExUjEiIHg9IjI0MCIgeT0iNzQiIHdpZHRoPSIyNTEiIGhlaWdodD0iMTYxIiBmaWxsPSIjMjAwMDlGIiBmaWxsLW9wYWNpdHk9IjAuMjUiLz4KPHJlY3QgY2xhc3M9InpvbmUtZGVwb3Qgem9uZS1kZXBvdC0tTjJQUTFSMiIgeD0iNjMiIHk9IjMyMCIgd2lkdGg9IjE1MCIgaGVpZ2h0PSIxMjMiIGZpbGw9IiNGRjAwMDAiIGZpbGwtb3BhY2l0eT0iMC4zNCIvPgo8L3N2Zz4K";
-      genereVue([1, 2, 3, 4], zone);
-      expect(wrapper.vm.zoneDeDepots.length > 0).toEqual(true);
+    describe("quand il y a plus d'éléments à placer que de zones de dépôt", function () {
+      beforeEach(function () {
+        genereVue([1, 2, 3, 4]);
+        mockNombreZonesDeDepot(3);
+      });
 
-      envoyerReponse(true, 'N1Pse1');
-      envoyerReponse(true, 'N1Pse2');
-      envoyerReponse(true, 'N1Pse3');
-      envoyerReponse(true, 'N1Pse4');
+      it('émet un événement avec la moitié du score si une seule réponse est mal placée', function () {
+        envoyerReponse(true, 'N1Pse1');
+        envoyerReponse(false, 'N1Pse2');
+        envoyerReponse(true, 'N1Pse3');
 
-      expect(wrapper.emitted('reponse')).toEqual(
-        expect.arrayContaining([[{ "reponse": ["N1Pse1", "N1Pse2", "N1Pse3", "N1Pse4"], "score": 1, "scoreMax": 1, "succes": true }]])
-      );
+        expect(wrapper.emitted('reponse')).toEqual(
+          expect.arrayContaining([[{ "reponse": ["N1Pse1", "N1Pse2", 'N1Pse3'], "score": 0.5, "scoreMax": 1, "succes": false }]])
+        );
+      });
+    });
+  });
+
+  describe('#calculeScoreUneFauteAutorisee', function () {
+    beforeEach(function () {
+      genereVue([1, 2, 3, 4]);
+      mockNombreZonesDeDepot(3);
+    });
+
+    it('calcule le score si toutes les réponses sont bien placées', function () {
+      wrapper.vm.reponsesPlacees = [{succes: true}, {succes: true}, {succes: true}];
+      const score = wrapper.vm.calculeScoreUneFauteAutorisee();
+      expect(score).toEqual(1);
+    });
+
+    it('calcule le score si seulement une réponse est mal placée', function () {
+      wrapper.vm.reponsesPlacees = [{succes: true}, {succes: false}, {succes: true}];
+      const score = wrapper.vm.calculeScoreUneFauteAutorisee();
+      expect(score).toEqual(0.5);
+    });
+
+    it("calcule le score si plus d'une réponse est mal placées", function () {
+      wrapper.vm.reponsesPlacees = [{succes: false}, {succes: false}, {succes: true}];
+      const score = wrapper.vm.calculeScoreUneFauteAutorisee();
+      expect(score).toEqual(0);
     });
   });
 });
