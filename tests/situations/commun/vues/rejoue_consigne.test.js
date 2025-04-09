9609e9cb3402fece2830135564eacaf3b43db3e7
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-04-09 11:24:19 +0200

    ✅ Corrige les tests (partie 1)

--- tests/situations/commun/vues/rejoue_consigne.test.js
+++ tests/situations/commun/vues/rejoue_consigne.test.js
@@ -1,74 +1,53 @@
-import $ from 'jquery';
-import VueRejoueConsigne from 'commun/vues/rejoue_consigne';
+import { mount } from '@vue/test-utils';
+import VueRejoueConsigne from 'commun/vues/rejoue_consigne.vue';
 import EvenementRejoueConsigne from 'commun/modeles/evenement_rejoue_consigne';
-import SituationCommune from 'commun/modeles/situation';
+import { traduction } from 'commun/infra/internationalisation';
+import { creeStore } from 'commun/modeles/store';
 
-describe('vue Rejoue Consigne', function () {
-  let vue;
-  let journal;
-  let mockJoueurConsigne;
-  let situation;
+describe('Le composant VueRejoueConsigne', () => {
+  let mockJournal;
+  let wrapper;
 
-  beforeEach(function () {
-    $('body').append('<div id="pointInsertion"></div>');
-    journal = { enregistre () {} };
-    situation = new SituationCommune();
-    mockJoueurConsigne = new class {
-      joue (cbFin) {
-        this.cbFin = cbFin;
+  let store;
+
+  const consigneDemarrage = function consigneDemarrage() {
+    return new ArrayBuffer(10);
+  };
+
+  beforeEach(() => {
+    store = creeStore();
+    mockJournal = { enregistre: jest.fn() };
+    
+    wrapper = mount(VueRejoueConsigne, {
+      props: {
+        journal: mockJournal
+      },
+      global: {
+        plugins: [store],
+        mocks: {
+          $depotRessources: {
+            consigneDemarrage: consigneDemarrage
+          },
+          $traduction: traduction
+        }
       }
-
-      finConsigne () {
-        this.cbFin();
-      }
-    }();
-
-    vue = new VueRejoueConsigne(situation, mockJoueurConsigne, journal);
+    });
   });
 
-  it("sait s'insérer dans une page web", function () {
-    vue.affiche('#pointInsertion', $);
-    expect($('#pointInsertion .bouton-lire-consigne').length).toEqual(1);
+  it("s'intègre correctement dans une page web", () => {
+    expect(wrapper.findComponent({ name: 'BoutonLecture' }).exists()).toBe(true);
   });
 
-  it('sait se cacher', function () {
-    vue.affiche('#pointInsertion', $);
-    vue.cache();
-    expect($('#pointInsertion .bouton-lire-consigne').length).toEqual(0);
-    expect($('#pointInsertion .bouton-en-pause').length).toEqual(0);
+  it("démarre le son approprié", () => {
+    const audioBuffer = wrapper.vm.sonAJouer();
+
+    expect(audioBuffer).toBeInstanceOf(ArrayBuffer);
+    expect(audioBuffer.byteLength).toBe(10);
   });
 
-  it('passe en état lecture en cours', function () {
-    vue.affiche('#pointInsertion', $);
-    vue.litConsigne($);
-    expect($('#pointInsertion .bouton-lire-consigne').length).toEqual(0);
-    expect($('#pointInsertion .bouton-en-pause').length).toEqual(1);
-  });
+  it("enregistre un événement RejoueConsigne", () => {
+    wrapper.vm.enregistrerEvenement();
 
-  it("à la fin de la lecture, repasse à l'état initial", function () {
-    vue.affiche('#pointInsertion', $);
-    vue.litConsigne($);
-    mockJoueurConsigne.finConsigne();
-    expect($('#pointInsertion .bouton-lire-consigne').length).toEqual(1);
-    expect($('#pointInsertion .bouton-en-pause').length).toEqual(0);
-  });
-
-  it('on peut lire la consigne plusieurs fois', function () {
-    vue.affiche('#pointInsertion', $);
-    vue.litConsigne($);
-    mockJoueurConsigne.finConsigne();
-    vue.litConsigne($);
-    mockJoueurConsigne.finConsigne();
-    expect($('#pointInsertion .bouton-lire-consigne').length).toEqual(1);
-    expect($('#pointInsertion .bouton-en-pause').length).toEqual(0);
-  });
-
-  it('journalise un événement RejoueConsigne', function (done) {
-    journal.enregistre = (evenement) => {
-      expect(evenement).toBeInstanceOf(EvenementRejoueConsigne);
-      done();
-    };
-    vue.affiche('#pointInsertion', $);
-    vue.litConsigne($);
+    expect(mockJournal.enregistre).toHaveBeenCalledWith(expect.any(EvenementRejoueConsigne));
   });
 });
