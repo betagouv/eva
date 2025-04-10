5fc75c2303d70e01f6bbab034fbe82dc7dfe54be
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-04-10 10:58:30 +0200

    🐛 Corrige la situation Inventaire

--- src/situations/inventaire/vues/situation.js
+++ src/situations/inventaire/vues/situation.js
@@ -2,12 +2,14 @@ import 'inventaire/styles/situation.scss';
 
 import VueEtageres from 'inventaire/vues/etageres';
 import { initialiseFormulaireSaisieInventaire } from 'inventaire/vues/formulaire_saisie_inventaire';
+import { creeStore } from 'commun/modeles/store';
 
 export default class VueSituation {
   constructor (situation, journal, depotRessources) {
     this.journal = journal;
     this.situation = situation;
     this.depotRessources = depotRessources;
+    this.store = creeStore();
   }
 
   affiche (pointInsertion, $) {
