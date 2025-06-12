440f4ea51cce8404f50bbc27052f75689651dd4e
guillaume imary <guillaume.imary@gmail.com>
2025-06-12 13:16:12 +0200

    🐛 Corrige le param pour code_personnel

--- src/situations/accueil/vues/index.vue
+++ src/situations/accueil/vues/index.vue
@@ -57,7 +57,7 @@ export default {
       erreurLectureSon: false,
       forceCampagne: parametresUrl.code || '',
       forceNom: parametresUrl.nom || '',
-      forceCodePersonnel: parametresUrl.codePersonnel || '',
+      forceCodePersonnel: parametresUrl.code_personnel || '',
       afficheErreurSmartphone: false
     };
   },
