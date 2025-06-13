2b0046357f9725cd7a7cc31953447a48016444f4
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2025-06-13 12:09:03 +0200

    🔧 Ne perd pas les paramètres lors de la redirection de /jeu

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ var app = express();
 var directory = path.join('/', (process.env.STATIC_DIR || 'public'));
 var router = express.Router();
 
-router.get('/jeu', function (req, res, next) {
-  res.redirect(process.env.URL_JEUX);
-});
+function gereRedirectionJeu(req, res) {
+  var cheminRedirection = req.path.replace('/jeu', '');
+  var urlRedirection = process.env.URL_JEUX + cheminRedirection;
+  if (req.query && Object.keys(req.query).length > 0) {
+    var params = new URLSearchParams(req.query).toString();
+    urlRedirection += '?' + params;
+  }
+  res.redirect(urlRedirection);
+}
+
+router.get('/jeu', gereRedirectionJeu);
+router.get('/jeu/*', gereRedirectionJeu);
+
 app.use('/', router);
 app.use(express.static(path.join(__dirname, directory)));
 
